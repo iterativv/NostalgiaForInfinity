d15a3f44434ade3d0363a497142b049d4296bc68
iterativ <vangelis.dimou@tutanota.com>
2021-07-18 00:14:56 +0300

    Second %R based sell logic path

--- NostalgiaForInfinityNext.py
+++ NostalgiaForInfinityNext.py
@@ -2060,6 +2060,46 @@ class NostalgiaForInfinityNext(IStrategy):
 
         return False, None
 
+    def sell_r_2(self, current_profit: float, last_candle) -> tuple:
+        if (0.02 > current_profit > 0.012):
+            if (last_candle['r_480'] > -4.0) & (last_candle['rsi'] > 78.0) & (last_candle['stochrsi_fastk_96'] > 99.0) & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_1'
+        elif (0.03 > current_profit > 0.02):
+            if (last_candle['r_480'] > -4.5) & (last_candle['rsi'] > 78.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_2'
+        elif (0.04 > current_profit > 0.03):
+            if (last_candle['r_480'] > -5.0) & (last_candle['rsi'] > 78.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_3'
+        elif (0.05 > current_profit > 0.04):
+            if (last_candle['r_480'] > -5.5) & (last_candle['rsi'] > 78.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_4'
+        elif (0.06 > current_profit > 0.05):
+            if (last_candle['r_480'] > -6.0) & (last_candle['rsi'] > 78.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_5'
+        elif (0.07 > current_profit > 0.06):
+            if (last_candle['r_480'] > -30.0) & (last_candle['rsi'] > 79.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_6'
+        elif (0.08 > current_profit > 0.07):
+            if (last_candle['r_480'] > -34.0) & (last_candle['rsi'] > 80.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_7'
+        elif (0.09 > current_profit > 0.08):
+            if (last_candle['r_480'] > -7.0) & (last_candle['rsi'] > 80.5) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_8'
+        elif (0.1 > current_profit > 0.09):
+            if (last_candle['r_480'] > -6.0) & (last_candle['rsi'] > 80.5) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_9'
+        elif (0.12 > current_profit > 0.1):
+            if (last_candle['r_480'] > -5.0) & (last_candle['rsi'] > 80.5) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_10'
+        elif (0.2 > current_profit > 0.12):
+            if (last_candle['r_480'] > -4.0) & (last_candle['rsi'] > 81.0) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_11'
+        elif (current_profit > 0.2):
+            if (last_candle['r_480'] > -3.0) & (last_candle['rsi'] > 81.5) & (last_candle['stochrsi_fastk_96'] > 99.0)  & (last_candle['stochrsi_fastd_96'] > 99.0):
+                return True, 'signal_profit_w_2_12'
+
+        return False, None
+
     def custom_sell(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
                     current_profit: float, **kwargs):
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
@@ -2131,6 +2171,11 @@ class NostalgiaForInfinityNext(IStrategy):
 
             # Williams %R based sell 1
             sell, signal_name = self.sell_r_1(current_profit, last_candle)
+            if (sell) and (signal_name is not None):
+                return signal_name
+
+            # Williams %R based sell 2
+            sell, signal_name = self.sell_r_2(current_profit, last_candle)
             if (sell) and (signal_name is not None):
                 return signal_name
 
@@ -2431,6 +2476,11 @@ class NostalgiaForInfinityNext(IStrategy):
 
         # Williams %R
         dataframe['r_480'] = williams_r(dataframe, period=480)
+
+        # Stochastic RSI
+        stochrsi = ta.STOCHRSI(dataframe, timeperiod=96, fastk_period=3, fastd_period=3, fastd_matype=0)
+        dataframe['stochrsi_fastk_96'] = stochrsi['fastk']
+        dataframe['stochrsi_fastd_96'] = stochrsi['fastd']
 
         # For sell checks
         dataframe['crossed_below_ema_12_26'] = qtpylib.crossed_below(dataframe['ema_12'], dataframe['ema_26'])
