1896087966f52496bab1732f307b8c71e3e6a23e
Pedro Algarvio <pedro@algarvio.me>
2021-07-27 10:38:57 +0100

    The rest of the integration tests

--- tests/backtests/binance/test_binance.py
+++ tests/backtests/binance/test_binance.py
@@ -1,3 +1,23 @@
+def test_20210101_20220701(backtest):
+    ret = backtest(start_date="20210101", end_date="20210201")
+    assert ret.results.max_drawdown < 0.475
+
+
+def test_20210201_20210301(backtest):
+    ret = backtest(start_date="20210201", end_date="20210301")
+    assert ret.results.max_drawdown < 0.7524
+
+
+def test_20210301_20210401(backtest):
+    ret = backtest(start_date="20210401", end_date="20210401")
+    assert ret.results.max_drawdown < 0.5029
+
+
+def test_20210401_20210501(backtest):
+    ret = backtest(start_date="20210401", end_date="20210501")
+    assert ret.results.max_drawdown < 0.1265
+
+
 def test_20210601_20210701(backtest):
     ret = backtest(start_date="20210601", end_date="20210701")
     assert ret.results.max_drawdown < 0.17
