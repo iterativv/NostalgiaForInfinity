827bf053978c17274d6ff145747f6250e2724ced
Pedro Algarvio <pedro@algarvio.me>
2021-07-28 17:54:04 +0100

    Add kucoin backtest tests

---
+++ tests/backtests/kucoin/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from tests.backtests.helpers import Backtest
+from tests.conftest import REPO_ROOT
+
+
+@pytest.fixture(scope="session")
+def check_exchange_data_presence():
+    kucoin_exchange_data_dir = REPO_ROOT / "user_data" / "data" / "kucoin"
+    if not kucoin_exchange_data_dir.is_dir():
+        pytest.fail(
+            "There's no exchange data. Make sure the repository submodule is init/update. "
+            "Check the repository README.md for more information."
+        )
+    if not list(kucoin_exchange_data_dir.rglob("*.json")):
+        pytest.fail(
+            "There's no exchange data. Make sure the repository submodule is init/update. "
+            "Check the repository README.md for more information."
+        )
+
+
+@pytest.fixture
+def backtest(request, check_exchange_data_presence):
+    return Backtest(request, "kucoin")
