d42ef338fb3849b50d0860b5e69a294eec124774
Pedro Algarvio <pedro@algarvio.me>
2021-07-29 12:51:33 +0100

    Data format changed

--- tests/backtests/kucoin/conftest.py
+++ tests/backtests/kucoin/conftest.py
@@ -12,7 +12,7 @@ def check_exchange_data_presence():
             "There's no exchange data. Make sure the repository submodule is init/update. "
             "Check the repository README.md for more information."
         )
-    if not list(kucoin_exchange_data_dir.rglob("*.json")):
+    if not list(kucoin_exchange_data_dir.rglob("*.json.gz")):
         pytest.fail(
             "There's no exchange data. Make sure the repository submodule is init/update. "
             "Check the repository README.md for more information."
