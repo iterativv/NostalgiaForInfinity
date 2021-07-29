a6e4f65cffb0fbc785cb2bccb9586030b8a408c2
Pedro Algarvio <pedro@algarvio.me>
2021-07-29 12:37:33 +0100

    Stricter tests

--- tests/backtests/binance/test_date_ranges.py
+++ tests/backtests/binance/test_date_ranges.py
@@ -1,23 +1,68 @@
-def test_20210101_20220701(backtest):
+def test_20210101_20210201(backtest, subtests):
     ret = backtest(start_date="20210101", end_date="20210201")
-    assert ret.results.max_drawdown < 0.475
+    with subtests.test("max_drawdown"):
+        assert round(ret.results.max_drawdown, 5) == 0.47411
+    with subtests.test("trades"):
+        assert ret.stats.trades == 187
+    with subtests.test("wins"):
+        assert ret.stats.wins == 169
+    with subtests.test("draws"):
+        assert ret.stats.draws == 0
+    with subtests.test("losses"):
+        assert ret.stats.losses == 18
 
 
-def test_20210201_20210301(backtest):
+def test_20210201_20210301(backtest, subtests):
     ret = backtest(start_date="20210201", end_date="20210301")
-    assert ret.results.max_drawdown < 0.7524
+    with subtests.test("max_drawdown"):
+        assert round(ret.results.max_drawdown, 5) == 0.75234
+    with subtests.test("trades"):
+        assert ret.stats.trades == 223
+    with subtests.test("wins"):
+        assert ret.stats.wins == 201
+    with subtests.test("draws"):
+        assert ret.stats.draws == 0
+    with subtests.test("losses"):
+        assert ret.stats.losses == 22
 
 
-def test_20210301_20210401(backtest):
+def test_20210301_20210401(backtest, subtests):
     ret = backtest(start_date="20210401", end_date="20210401")
-    assert ret.results.max_drawdown < 0.5029
+    with subtests.test("max_drawdown"):
+        assert round(ret.results.max_drawdown, 5) == 0.0
+    with subtests.test("trades"):
+        assert ret.stats.trades == 0
+    with subtests.test("wins"):
+        assert ret.stats.wins == 0
+    with subtests.test("draws"):
+        assert ret.stats.draws == 0
+    with subtests.test("losses"):
+        assert ret.stats.losses == 0
 
 
-def test_20210401_20210501(backtest):
+def test_20210401_20210501(backtest, subtests):
     ret = backtest(start_date="20210401", end_date="20210501")
-    assert ret.results.max_drawdown < 0.1265
+    with subtests.test("max_drawdown"):
+        assert round(ret.results.max_drawdown, 5) == 0.12640
+    with subtests.test("trades"):
+        assert ret.stats.trades == 254
+    with subtests.test("wins"):
+        assert ret.stats.wins == 248
+    with subtests.test("draws"):
+        assert ret.stats.draws == 0
+    with subtests.test("losses"):
+        assert ret.stats.losses == 6
 
 
-def test_20210601_20210701(backtest):
+def test_20210601_20210701(backtest, subtests):
     ret = backtest(start_date="20210601", end_date="20210701")
-    assert ret.results.max_drawdown < 0.17
+    with subtests.test("max_drawdown"):
+        assert round(ret.results.max_drawdown, 5) == 0.16666
+    with subtests.test("trades"):
+        assert ret.stats.trades == 128
+    with subtests.test("wins"):
+        assert ret.stats.wins == 120
+    with subtests.test("draws"):
+        assert ret.stats.draws == 0
+    with subtests.test("losses"):
+        assert ret.stats.losses == 8
