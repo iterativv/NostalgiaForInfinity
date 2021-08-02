47f3a6d0a03fa1ed8700d1e3284c428aa3a9c912
anasyusef <anas.y0807@gmail.com>
2021-08-02 23:09:35 +0100

    test: ignore formatter

--- tests/unit/test_data/input/Strategy1.py
+++ tests/unit/test_data/input/Strategy1.py
@@ -1,5 +1,6 @@
+# fmt: off
+from freqtrade.strategy import DecimalParameter
 from freqtrade.strategy.interface import IStrategy
-from freqtrade.strategy import DecimalParameter 
 from pandas.core.frame import DataFrame
 
 
@@ -7,7 +8,7 @@ class Strategy1(IStrategy):
     INTERFACE_VERSION = 2
 
     buy_32_ma_offset = DecimalParameter(0.90, 0.99, default=0.946, space='buy', optimize=False, load=True)
-    
+
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         if self.buy_32_ma_offset.value > 0.90:
             pass
--- tests/unit/test_data/output/Strategy1.py
+++ tests/unit/test_data/output/Strategy1.py
@@ -1,5 +1,6 @@
+# fmt: off
+from freqtrade.strategy import DecimalParameter
 from freqtrade.strategy.interface import IStrategy
-from freqtrade.strategy import DecimalParameter 
 from pandas.core.frame import DataFrame
 
 
@@ -7,7 +8,7 @@ class Strategy1(IStrategy):
     INTERFACE_VERSION = 2
 
     buy_32_ma_offset = 0.946
-    
+
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         if self.buy_32_ma_offset > 0.90:
             pass
