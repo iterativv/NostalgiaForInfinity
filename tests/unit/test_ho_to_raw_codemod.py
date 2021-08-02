65386ba7ac8daebdc3ef6b2e3258555af1f405a0
anasyusef <anas.y0807@gmail.com>
2021-08-02 23:06:39 +0100

    test: add replace references test case

--- tests/unit/test_ho_to_raw_codemod.py
+++ tests/unit/test_ho_to_raw_codemod.py
@@ -1,5 +1,11 @@
 import ast
+from pathlib import Path
 
+import pytest
+from freqtrade.configuration import Configuration
+from freqtrade.resolvers import StrategyResolver
+
+from codemods.ho_to_raw_codemod import replace_all_references
 from codemods.ho_to_raw_codemod import transform_code
 from tests.unit.conftest import REPO_ROOT
 
@@ -112,3 +118,24 @@ def test_transform_code_syntax():
         is_valid = False
 
     assert is_valid is True
+
+
+def test_transform_and_replace_references():
+    test_data_dir = REPO_ROOT.joinpath("tests/unit/test_data")
+    config = Configuration.from_files([])
+    config["strategy"] = "Strategy1"
+    config["strategy_path"] = test_data_dir.joinpath("input")
+    strategy = StrategyResolver.load_strategy(config)
+    source_input = ""
+    with open(config["strategy_path"].joinpath('Strategy1.py')) as f:
+        source_input = f.read()
+    transformed_input = transform_code(
+        replace_all_references(strategy, source_input))
+
+    expected_source = ""
+    with open(test_data_dir.joinpath("output/Strategy1.py")) as f:
+        expected_source = f.read()
+
+    assert transformed_input.strip() == expected_source.strip()
+
+    # assert transformed_input.strip() == expected_output.strip()
