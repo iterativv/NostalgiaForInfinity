df181128c9d7e783cb8a72f0ba44693d2302d41f
iterativ <vangelis.dimou@tutanota.com>
2021-08-08 08:19:55 +0300

    Buy 23: minor rework.

--- NostalgiaForInfinityNext.py
+++ NostalgiaForInfinityNext.py
@@ -2116,7 +2116,7 @@ class NostalgiaForInfinityNext(IStrategy):
     buy_rsi_22 = DecimalParameter(26.0, 56.0, default=37.0, space='buy', decimals=1, optimize=False, load=True)
 
     buy_bb_offset_23 = DecimalParameter(0.97, 1.0, default=0.983, space='buy', decimals=3, optimize=False, load=True)
-    buy_ewo_23 = DecimalParameter(2.0, 10.0, default=7.0, space='buy', decimals=1, optimize=False, load=True)
+    buy_ewo_23 = DecimalParameter(2.0, 10.0, default=6.2, space='buy', decimals=1, optimize=False, load=True)
     buy_rsi_23 = DecimalParameter(20.0, 40.0, default=30.0, space='buy', decimals=1, optimize=False, load=True)
     buy_rsi_1h_23 = DecimalParameter(60.0, 80.0, default=70.0, space='buy', decimals=1, optimize=False, load=True)
 
