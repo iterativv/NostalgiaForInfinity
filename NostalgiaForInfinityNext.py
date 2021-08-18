55bedbee752cc5ca5193fec25c165472b05b4b28
iterativ <vangelis.dimou@tutanota.com>
2021-08-18 16:07:35 +0300

    sell_quick_mode: fine tune pmax bull/bear sells.

--- NostalgiaForInfinityNext.py
+++ NostalgiaForInfinityNext.py
@@ -2305,8 +2305,9 @@ class NostalgiaForInfinityNext(IStrategy):
                 return True, 'signal_stoploss_q_atr'
 
         if (0.04 > current_profit > 0.02) and (last_candle['pm'] <= last_candle['pmax_thresh']) and (last_candle['close'] > last_candle['sma_21'] * 1.1):
-                return True, 'signal_profit_q_pmax_bull'
-        if (0.045 > current_profit > 0.003) and (last_candle['pm'] > last_candle['pmax_thresh']) and (last_candle['close'] > last_candle['sma_21'] * 1.016):         return True, 'signal_profit_q_pmax_bear'
+            return True, 'signal_profit_q_pmax_bull'
+        if (0.045 > current_profit > 0.003) and (last_candle['pm'] > last_candle['pmax_thresh']) and (last_candle['close'] > last_candle['sma_21'] * 1.016):
+            return True, 'signal_profit_q_pmax_bear'
 
         return False, None
 
