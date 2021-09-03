78dbf1ac0da22b8db34232296163e7995780925c
iterativ <vangelis.dimou@tutanota.com>
2021-09-03 17:40:48 +0300

    Add Kucoin test deviation for 20210801-20210901.

--- tests/backtests/test_winrate_and_drawdown.py
+++ tests/backtests/test_winrate_and_drawdown.py
@@ -78,6 +78,7 @@ def deviations():
             ("20210501", "20210601"): {"max_drawdown": 141, "winrate": 90},
             ("20210601", "20210701"): {"max_drawdown": 51, "winrate": 76},
             ("20210701", "20210801"): {"max_drawdown": 82, "winrate": 90},
+            ("20210801", "20210901"): {"max_drawdown": 60, "winrate": 90},
         },
     }
 
