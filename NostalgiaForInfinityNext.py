d3885ff6254705c7ad9baad9a3fbb004b8267e7c
iterativ <vangelis.dimou@tutanota.com>
2021-09-16 18:34:55 +0300

    sell_long_mode: rework.

--- NostalgiaForInfinityNext.py
+++ NostalgiaForInfinityNext.py
@@ -3696,10 +3696,7 @@ class NostalgiaForInfinityNext(IStrategy):
         elif (0.03 < current_profit <= 0.06) and (current_time - timedelta(minutes=720) > trade.open_date_utc) and (last_candle['r_480'] > -20.0):
             return True, 'sell_long_l_1'
 
-        elif (current_profit < -0.1) and (last_candle['close'] < last_candle['atr_high_thresh_l']) and (previous_candle_1['close'] > previous_candle_1['atr_high_thresh_l']) and (last_candle['sma_200_dec_24']) and (last_candle['ema_25'] < last_candle['ema_50']):
-                return True, 'sell_long_stoploss_1'
-
-        return self.sell_stoploss_atr(current_profit, last_candle, previous_candle_1, trade, current_time)
+        return self.sell_stoploss_extra(current_profit, max_profit, max_loss, last_candle, previous_candle_1, trade, current_time)
 
         return False, None
 
