4002d49f95904c61651465be6ad1c201c70cb9ba
iterativ <vangelis.dimou@tutanota.com>
2021-09-18 12:05:07 +0300

    Buy 28: rework.

--- NostalgiaForInfinityNext.py
+++ NostalgiaForInfinityNext.py
@@ -1816,7 +1816,7 @@ class NostalgiaForInfinityNext(IStrategy):
 
     buy_28_ma_offset = 0.928
     buy_28_ewo_min = 2.0
-    buy_28_rsi_max = 33.4
+    buy_28_rsi_14_max = 33.4
     buy_28_cti_max = -0.84
     buy_28_r_14_max = -97.0
     buy_28_cti_1h_max = 0.95
@@ -3922,12 +3922,12 @@ class NostalgiaForInfinityNext(IStrategy):
             return f"{signal_name} ( {buy_tag})"
 
         # Stoplosses
-        if any(c in ['empty', '28', '29', '30', '31'] for c in buy_tags):
+        if any(c in ['empty', '29', '30', '31'] for c in buy_tags):
             sell, signal_name = self.sell_stoploss_atr(current_profit, last_candle, previous_candle_1, trade, current_time)
             if sell and (signal_name is not None):
                 return f"{signal_name} ( {buy_tag})"
 
-        if any(c in ['empty', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '32', '33', '34', '35', '36', '37', '38', '39','40', '41', '42', '43', '44', '45', '46', '47', '48'] for c in buy_tags):
+        if any(c in ['empty', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '32', '33', '34', '35', '36', '37', '38', '39','40', '41', '42', '43', '44', '45', '46', '47', '48'] for c in buy_tags):
             sell, signal_name = self.sell_stoploss_extra(current_profit, max_profit, max_loss, last_candle, previous_candle_1, trade, current_time)
             if sell and (signal_name is not None):
                 return f"{signal_name} ( {buy_tag})"
@@ -5048,7 +5048,7 @@ class NostalgiaForInfinityNext(IStrategy):
                     item_buy_logic.append(dataframe['moderi_64'] == True)
                     item_buy_logic.append(dataframe['close'] < dataframe['hull_75'] * self.buy_28_ma_offset)
                     item_buy_logic.append(dataframe['ewo_sma'] > self.buy_28_ewo_min)
-                    item_buy_logic.append(dataframe['rsi_14'] < self.buy_28_rsi_max)
+                    item_buy_logic.append(dataframe['rsi_14'] < self.buy_28_rsi_14_max)
                     item_buy_logic.append(dataframe['cti'] < self.buy_28_cti_max)
                     item_buy_logic.append(dataframe['cti'].shift(1) < self.buy_28_cti_max)
                     item_buy_logic.append(dataframe['r_14'] < self.buy_28_r_14_max)
