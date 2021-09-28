48c0454dfd3591c96aff7957a27ea6aa70cf3b20
iterativ <vangelis.dimou@tutanota.com>
2021-09-28 21:16:36 +0300

    sell_stoploss: rework.

--- NostalgiaForInfinityNextGen.py
+++ NostalgiaForInfinityNextGen.py
@@ -1031,9 +1031,9 @@ class NostalgiaForInfinityNextGen(IStrategy):
 
         # Under EMA200, pair negative, low max rate
         if (
-                (-0.04 > current_profit > -0.08)
+                (-0.04 > current_profit > -0.07)
                 and (max_profit < 0.005)
-                and (max_loss < 0.08)
+                and (max_loss < 0.07)
                 and (last_candle['close'] < last_candle['ema_200'])
                 and (last_candle['ema_25'] < last_candle['ema_50'])
                 and (last_candle['ema_vwma_osc_32'] < 0.0 )
@@ -1045,8 +1045,8 @@ class NostalgiaForInfinityNextGen(IStrategy):
 
         # Under EMA200, pair negative, long duration, max rate over a specific value
         if (
-                (-0.04 > current_profit > -0.08)
-                and (max_loss < 0.08)
+                (-0.04 > current_profit > -0.07)
+                and (max_loss < 0.07)
                 and (max_profit > 0.06)
                 and (max_profit > (current_profit + 0.1))
                 and (last_candle['close'] < last_candle['ema_200'])
@@ -1058,6 +1058,23 @@ class NostalgiaForInfinityNextGen(IStrategy):
                 and (current_time - timedelta(minutes=720) > trade.open_date_utc)
         ):
                 return True, 'sell_stoploss_u_e_4'
+
+                # Under EMA200, pair negative, low max rate
+        if (
+                (-0.04 > current_profit > -0.07)
+                and (max_profit < 0.015)
+                and (max_loss < 0.07)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['sma_200_dec_24'])
+                and (last_candle['sma_200_dec_20_1h'])
+                and (last_candle['ema_vwma_osc_32'] < 0.0 )
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (last_candle['ema_vwma_osc_96'] < 0.0)
+                and (last_candle['cmf'] < -0.0)
+                and (current_time - timedelta(minutes=120) < trade.open_date_utc)
+        ):
+            return True, 'sell_stoploss_u_e_5'
 
         # Under EMA200, pair and BTC negative, low max rate
         if (
