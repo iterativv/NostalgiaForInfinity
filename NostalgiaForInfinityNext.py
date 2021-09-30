284c9639376035bbe1ed986e4e976530af932f88
iterativ <vangelis.dimou@tutanota.com>
2021-09-30 15:36:17 +0300

    sell_stoploss: rework.

--- NostalgiaForInfinityNext.py
+++ NostalgiaForInfinityNext.py
@@ -3386,96 +3386,154 @@ class NostalgiaForInfinityNext(IStrategy):
     def sell_stoploss(self, current_profit: float, max_profit: float, max_loss: float, last_candle, previous_candle_1, trade: 'Trade', current_time: 'datetime') -> tuple:
         # Under & near EMA200, local uptrend move
         if (
-            current_profit < -0.05
-            and last_candle['close'] < last_candle['ema_200']
-            and last_candle['cmf'] < 0.0
-            and (last_candle['ema_200'] - last_candle['close']) / last_candle['close'] < 0.004
-            and last_candle['rsi_14'] > previous_candle_1['rsi_14']
-            and last_candle['rsi_14'] > last_candle['rsi_14_1h'] + 10.0
-            and last_candle['sma_200_dec_24']
-            and current_time - timedelta(minutes=2880) > trade.open_date_utc
+                (current_profit < -0.05)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['cmf'] < 0.0)
+                and (((last_candle['ema_200'] - last_candle['close']) / last_candle['close']) < 0.004)
+                and last_candle['rsi_14'] > previous_candle_1['rsi_14']
+                and (last_candle['rsi_14'] > (last_candle['rsi_14_1h'] + 10.0))
+                and (last_candle['sma_200_dec_24'])
+                and (current_time - timedelta(minutes=2880) > trade.open_date_utc)
         ):
             return True, 'signal_stoploss_u_e_1'
 
         # Under EMA200, local strong uptrend move
         if (
-            current_profit < -0.08
-            and last_candle['close'] < last_candle['ema_200']
-            and last_candle['cmf'] < 0.0
-            and last_candle['rsi_14'] > previous_candle_1['rsi_14']
-            and last_candle['rsi_14'] > last_candle['rsi_14_1h'] + 24.0
-            and last_candle['sma_200_dec_20']
-            and last_candle['sma_200_dec_24']
-            and current_time - timedelta(minutes=2880) > trade.open_date_utc
+                (current_profit < -0.08)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['cmf'] < 0.0)
+                and last_candle['rsi_14'] > previous_candle_1['rsi_14']
+                and (last_candle['rsi_14'] > (last_candle['rsi_14_1h'] + 24.0))
+                and (last_candle['sma_200_dec_20'])
+                and (last_candle['sma_200_dec_24'])
+                and (current_time - timedelta(minutes=2880) > trade.open_date_utc)
         ):
             return True, 'signal_stoploss_u_e_2'
 
         # Under EMA200, pair negative, low max rate
         if (
-            -0.04 > current_profit > -0.08
-            and max_profit < 0.005
-            and max_loss < 0.08
-            and last_candle['close'] < last_candle['ema_200']
-            and last_candle['ema_25'] < last_candle['ema_50']
-            and last_candle['ema_vwma_osc_32'] < 0
-            and last_candle['ema_vwma_osc_64'] < 0
-            and last_candle['ema_vwma_osc_96'] < 0
-            and last_candle['cmf'] < -0.0
-            and current_time - timedelta(minutes=30) > trade.open_date_utc
+                (current_profit < -0.08)
+                and (max_profit < 0.04)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['sma_200_dec_20'])
+                and (last_candle['sma_200_dec_24'])
+                and (last_candle['sma_200_dec_20_1h'])
+                and (last_candle['ema_vwma_osc_32'] < 0.0)
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (last_candle['ema_vwma_osc_96'] < 0.0)
+                and (last_candle['cmf'] < -0.0)
+                and (last_candle['cmf_1h'] < -0.0)
+                and (last_candle['close'] < last_candle['sup_level_1h'])
+                and (last_candle['btc_not_downtrend_1h'] == False)
+                and (current_time - timedelta(minutes=1440) > trade.open_date_utc)
         ):
-            return True, 'signal_stoploss_u_e_3'
+            return True, 'signal_stoploss_u_e_doom'
 
         # Under EMA200, pair and BTC negative, low max rate
         if (
-            -0.05 > current_profit > -0.09
-            and not last_candle['btc_not_downtrend_1h']
-            and last_candle['ema_vwma_osc_32'] < 0
-            and last_candle['ema_vwma_osc_64'] < 0
-            and max_profit < 0.005
-            and max_loss < 0.09
-            and last_candle['sma_200_dec_24']
-            and last_candle['cmf'] < -0.0
-            and last_candle['close'] < last_candle['ema_200']
-            and last_candle['ema_25'] < last_candle['ema_50']
-            and last_candle['cti'] < -0.8
-            and last_candle['r_480'] < -50.0
+                (-0.05 > current_profit > -0.09)
+                and (last_candle['btc_not_downtrend_1h'] == False)
+                and (last_candle['ema_vwma_osc_32'] < 0.0)
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (max_profit < 0.005)
+                and (max_loss < 0.09)
+                and (last_candle['sma_200_dec_24'])
+                and (last_candle['cmf'] < -0.0)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['cti'] < -0.8)
+                and (last_candle['r_480'] < -50.0)
         ):
             return True, 'signal_stoploss_u_e_b_1'
 
-        # Under EMA200, pair & BTC negative, CTI, downtrend, normal max rate
-        if (
-            -0.1 > current_profit > -0.2
-            and not last_candle['btc_not_downtrend_1h']
-            and last_candle['ema_vwma_osc_32'] < 0
-            and last_candle['ema_vwma_osc_64'] < 0
-            and last_candle['ema_vwma_osc_96'] < 0
-            and max_profit < 0.05
-            and max_loss < 0.2
-            and last_candle['sma_200_dec_24']
-            and last_candle['sma_200_dec_20_1h']
-            and last_candle['cmf'] < -0.45
-            and last_candle['close'] < last_candle['ema_200']
-            and last_candle['ema_25'] < last_candle['ema_50']
-            and last_candle['cti'] < -0.8
-            and last_candle['r_480'] < -97.0
+        # Under EMA200, pair and BTC negative, CTI, Elder Ray Index negative, normal max rate
+        elif (
+                (-0.1 > current_profit > -0.2)
+                and (last_candle['btc_not_downtrend_1h'] == False)
+                and (last_candle['ema_vwma_osc_32'] < 0.0)
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (last_candle['ema_vwma_osc_96'] < 0.0)
+                and (max_profit < 0.05)
+                and (max_loss < 0.2)
+                and (last_candle['sma_200_dec_24'])
+                and (last_candle['sma_200_dec_20_1h'])
+                and (last_candle['cmf'] < -0.45)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['cti'] < -0.8)
+                and (last_candle['r_480'] < -97.0)
         ):
             return True, 'signal_stoploss_u_e_b_2'
 
         # Under EMA200, pair and BTC negative, low max rate
         if (
-            -0.04 > current_profit > -0.07
-            and last_candle['ema_vwma_osc_32'] < 0
-            and last_candle['ema_vwma_osc_64'] < 0
-            and max_profit < 0.005
-            and max_loss < 0.07
-            and last_candle['sma_200_dec_24']
-            and last_candle['close'] < last_candle['ema_200']
-            and last_candle['ema_25'] < last_candle['ema_50']
-            and last_candle['cmf'] < -0.0
-            and last_candle['r_480'] < -50.0
-            and not last_candle['btc_not_downtrend_1h']
+                (-0.04 > current_profit > -0.07)
+                and (last_candle['ema_vwma_osc_32'] < 0.0)
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (max_profit < 0.005)
+                and (max_loss < 0.07)
+                and (last_candle['sma_200_dec_24'])
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['cmf'] < -0.0)
+                and (last_candle['r_480'] < -50.0)
+                and (last_candle['btc_not_downtrend_1h'] == False)
         ):
             return True, 'signal_stoploss_u_e_b_3'
+
+
+        # Under EMA200, pair & BTC negative, low max rate, short trade duration
+        if (
+                (-0.04 > current_profit > -0.09)
+                and (max_profit < 0.005)
+                and (max_loss < 0.09)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['ema_vwma_osc_32'] < 0.0 )
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (last_candle['ema_vwma_osc_96'] < 0.0)
+                and (last_candle['cmf'] < -0.1)
+                and (last_candle['r_480'] < -75.0)
+                and (last_candle['close'] < last_candle['sup_level_1h'])
+                and (current_time - timedelta(minutes=60) < trade.open_date_utc)
+                and (last_candle['btc_not_downtrend_1h'] == False)
+        ):
+            return True, 'signal_stoploss_u_e_b_4'
+
+        # Under EMA200, pair & BTC negative, low max rate, short trade duration
+        if (
+                (-0.04 > current_profit > -0.09)
+                and (max_profit < 0.015)
+                and (max_loss < 0.09)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['ema_vwma_osc_32'] < 0.0 )
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (last_candle['ema_vwma_osc_96'] < 0.0)
+                and (last_candle['cmf'] < -0.2)
+                and (last_candle['close'] < last_candle['sup_level_1h'])
+                and (current_time - timedelta(minutes=180) < trade.open_date_utc)
+                and (last_candle['btc_not_downtrend_1h'] == False)
+        ):
+            return True, 'signal_stoploss_u_e_b_5'
+
+        # Under EMA200, pair & BTC negative, low max rate, short trade duration
+        if (
+                (-0.04 > current_profit > -0.1)
+                and (max_profit < 0.015)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['ema_25'] < last_candle['ema_50'])
+                and (last_candle['ema_vwma_osc_32'] < 0.0 )
+                and (last_candle['ema_vwma_osc_64'] < 0.0)
+                and (last_candle['ema_vwma_osc_96'] < 0.0)
+                and (last_candle['cmf'] < -0.25)
+                and (last_candle['r_480'] < -75.0)
+                and (last_candle['close'] < last_candle['sup_level_1h'])
+                and (current_time - timedelta(minutes=60) < trade.open_date_utc)
+                and (last_candle['btc_not_downtrend_1h'] == False)
+        ):
+            return True, 'signal_stoploss_u_e_b_6'
 
         return False, None
 
