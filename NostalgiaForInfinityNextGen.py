6bb23bdbf28e76adf619c040d0f4c2fe4a74a7ab
iterativ <vangelis.dimou@tutanota.com>
2021-10-01 15:15:29 +0300

    Fix minor typo.

--- NostalgiaForInfinityNextGen.py
+++ NostalgiaForInfinityNextGen.py
@@ -2524,7 +2524,7 @@ class NostalgiaForInfinityNextGen(IStrategy):
                         return True, 'sell_profit_p_bull_48_2_2_1'
                 elif 0.02 > current_profit >= 0.01:
                     if (last_candle['rsi_14'] < 39.0) and (last_candle['cmf'] < -0.25):
-                        return True, 'sell_profit_p_bull_48_2_2_1'
+                        return True, 'sell_profit_p_bull_48_2_1_1'
             else:
                 if current_profit >= 0.2:
                     if (last_candle['rsi_14'] < 33.0) and (last_candle['cmf'] < -0.35):
