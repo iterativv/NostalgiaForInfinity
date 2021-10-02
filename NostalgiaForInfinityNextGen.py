b758be381a88ea6df85f15914b5ac794f4b3173c
iterativ <vangelis.dimou@tutanota.com>
2021-10-02 08:39:48 +0300

    Buy 14: rework.

--- NostalgiaForInfinityNextGen.py
+++ NostalgiaForInfinityNextGen.py
@@ -584,12 +584,12 @@ class NostalgiaForInfinityNextGen(IStrategy):
             "sma200_rising"             : False,
             "sma200_rising_val"         : "24",
             "sma200_1h_rising"          : False,
-            "sma200_1h_rising_val"      : "50",
+            "sma200_1h_rising_val"      : "24",
             "safe_dips_threshold_0"     : 0.06,
             "safe_dips_threshold_2"     : 0.09,
-            "safe_dips_threshold_12"    : 0.24,
-            "safe_dips_threshold_144"   : 0.38,
-            "safe_pump_6h_threshold"    : 0.4,
+            "safe_dips_threshold_12"    : None,
+            "safe_dips_threshold_144"   : None,
+            "safe_pump_6h_threshold"    : 0.42,
             "safe_pump_12h_threshold"   : None,
             "safe_pump_24h_threshold"   : None,
             "safe_pump_36h_threshold"   : 0.9,
@@ -3736,18 +3736,16 @@ class NostalgiaForInfinityNextGen(IStrategy):
                     item_buy_logic.append((dataframe['ema_26'].shift() - dataframe['ema_12'].shift()) > (dataframe['open'] / 100))
                     item_buy_logic.append(dataframe['close'] < (dataframe['bb20_2_low'] * 0.998))
 
-                # Condition #14 - Semi swing. Mild uptrend. 1h uptrend. Local dip.
+                # Condition #14 - Semi swing. Uptrend. Local dip.
                 elif index == 14:
                     # Non-Standard protections (add below)
 
                     # Logic
-                    item_buy_logic.append(dataframe['ewo_ema'] > 2.0)
-                    item_buy_logic.append(dataframe['close'].shift(1) < (dataframe['ema_20'].shift(1) * 0.992))
+                    item_buy_logic.append(dataframe['ewo_ema'] > 7.5)
                     item_buy_logic.append(dataframe['close'] > (dataframe['close'].shift(1)))
-                    item_buy_logic.append(dataframe['cmf'].shift(1) < -0.48)
+                    item_buy_logic.append(dataframe['close'].shift(1) < (dataframe['bb20_2_low'].shift(1) * 0.985))
                     item_buy_logic.append(dataframe['r_14'].shift(1) < -97.0)
-                    item_buy_logic.append(dataframe['ewo_ema_1h'] > 7.5)
-                    item_buy_logic.append(dataframe['cti_1h'] < 0.0)
+                    item_buy_logic.append(dataframe['r_96'].shift(1) < -85.0)
 
                 # Condition #15 - Semi swing. Downtrend.Local dip.
                 elif index == 15:
