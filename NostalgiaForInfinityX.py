ac5343640e66bdec2d5138a9c01e7a9f48b9e2ef
iterativ <vangelis.dimou@tutanota.com>
2021-10-27 11:52:26 +0300

    Buy 19: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -741,7 +741,7 @@ class NostalgiaForInfinityX(IStrategy):
             "sma200_rising_val"         : "24",
             "sma200_1h_rising"          : False,
             "sma200_1h_rising_val"      : "36",
-            "safe_dips_threshold_0"     : 0.028,
+            "safe_dips_threshold_0"     : 0.026,
             "safe_dips_threshold_2"     : 0.09,
             "safe_dips_threshold_12"    : 0.26,
             "safe_dips_threshold_144"   : 0.44,
