35286cac210c182fdf5023b7a2b9b437c2b4a31f
iterativ <vangelis.dimou@tutanota.com>
2021-10-29 16:36:36 +0300

    sell_long_mode: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -3343,7 +3343,7 @@ class NostalgiaForInfinityX(IStrategy):
         if sell and (signal_name is not None):
             return True, signal_name
 
-        if (0.0 < current_profit <= 0.02) and (max_profit - current_profit > 0.03) and (last_candle['cmf'] < 0.0):
+        if (0.0 < current_profit <= 0.02) and (max_profit - current_profit > 0.025) and (last_candle['cmf'] < 0.0):
             return True, 'sell_long_t_0'
         elif (0.02 < current_profit <= 0.04) and (max_profit - current_profit > 0.03) and (last_candle['cmf'] < 0.0):
             return True, 'sell_long_t_1'
