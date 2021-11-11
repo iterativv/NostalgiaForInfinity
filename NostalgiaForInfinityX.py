81530c1dbbf5d4c8cb5572f59352d654ec7e0d63
iterativ <vangelis.dimou@tutanota.com>
2021-11-11 20:25:59 +0200

    Buy 51: 15m. Semi swing. Deep local dip. Mild 15m uptrend.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -230,6 +230,7 @@ class NostalgiaForInfinityX(IStrategy):
         "buy_condition_48_enable": True,
         "buy_condition_49_enable": True,
         "buy_condition_50_enable": True,
+        "buy_condition_51_enable": True,
         #############
     }
 
@@ -1636,6 +1637,34 @@ class NostalgiaForInfinityX(IStrategy):
             "safe_pump_24h_threshold"   : 0.56,
             "safe_pump_36h_threshold"   : 1.0,
             "safe_pump_48h_threshold"   : None,
+            "btc_1h_not_downtrend"      : False,
+            "close_over_pivot_type"     : "none", # pivot, sup1, sup2, sup3, res1, res2, res3
+            "close_over_pivot_offset"   : 1.0,
+            "close_under_pivot_type"    : "none", # pivot, sup1, sup2, sup3, res1, res2, res3
+            "close_under_pivot_offset"  : 1.0
+        },
+        51: {
+            "ema_fast"                  : False,
+            "ema_fast_len"              : "50",
+            "ema_slow"                  : False,
+            "ema_slow_len"              : "50",
+            "close_above_ema_fast"      : False,
+            "close_above_ema_fast_len"  : "200",
+            "close_above_ema_slow"      : False,
+            "close_above_ema_slow_len"  : "200",
+            "sma200_rising"             : False,
+            "sma200_rising_val"         : "42",
+            "sma200_1h_rising"          : False,
+            "sma200_1h_rising_val"      : "50",
+            "safe_dips_threshold_0"     : 0.03,
+            "safe_dips_threshold_2"     : 0.09,
+            "safe_dips_threshold_12"    : None,
+            "safe_dips_threshold_144"   : None,
+            "safe_pump_6h_threshold"    : 0.5,
+            "safe_pump_12h_threshold"   : 0.58,
+            "safe_pump_24h_threshold"   : None,
+            "safe_pump_36h_threshold"   : None,
+            "safe_pump_48h_threshold"   : 1.1,
             "btc_1h_not_downtrend"      : False,
             "close_over_pivot_type"     : "none", # pivot, sup1, sup2, sup3, res1, res2, res3
             "close_over_pivot_offset"   : 1.0,
@@ -4748,6 +4777,7 @@ class NostalgiaForInfinityX(IStrategy):
 
         # EMAs
         informative_15m['ema_12'] = ta.EMA(informative_15m, timeperiod=12)
+        informative_15m['ema_16'] = ta.EMA(informative_15m, timeperiod=16)
         informative_15m['ema_20'] = ta.EMA(informative_15m, timeperiod=20)
         informative_15m['ema_26'] = ta.EMA(informative_15m, timeperiod=25)
         informative_15m['ema_50'] = ta.EMA(informative_15m, timeperiod=50)
@@ -5716,6 +5746,19 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append(dataframe['r_96_15m'] < -75.0)
                     item_buy_logic.append(dataframe['rsi_14'] < 32.0)
                     item_buy_logic.append(dataframe['r_14_15m'] < -97.0)
+
+                # Condition #51 - 15m. Semi swing. Downtrend. Dip.
+                elif index == 51:
+                    # Non-Standard protections
+
+                    # Logic
+                    item_buy_logic.append(dataframe['close_15m'] < (dataframe['ema_16_15m'] * 0.944))
+                    item_buy_logic.append(dataframe['ewo_15m'] < -1.0)
+                    item_buy_logic.append(dataframe['rsi_14_15m'] > 28.0)
+                    item_buy_logic.append(dataframe['cti_15m'] < -0.84)
+                    item_buy_logic.append(dataframe['r_14_15m'] < -94.0)
+                    item_buy_logic.append(dataframe['rsi_14'] > 30.0)
+                    item_buy_logic.append(dataframe['crsi_1h'] > 1.0)
 
                 item_buy_logic.append(dataframe['volume'] > 0)
                 item_buy = reduce(lambda x, y: x & y, item_buy_logic)
