9a471740b411fc2727f46a38b950e8b2ba97576f
iterativ <vangelis.dimou@tutanota.com>
2021-12-20 22:04:26 +0200

    sell_stoploss: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v10.8.150"
+        return "v10.8.151"
 
     # ROI table:
     minimal_roi = {
@@ -2299,7 +2299,7 @@ class NostalgiaForInfinityX(IStrategy):
                 (current_profit < -0.025)
                 and (last_candle['close'] < last_candle['ema_200'])
                 and (last_candle['cmf'] < -0.0)
-                and (((last_candle['ema_200'] - last_candle['close']) / last_candle['close']) < 0.02)
+                and (((last_candle['ema_200'] - last_candle['close']) / last_candle['close']) < 0.004)
                 and last_candle['rsi_14'] > previous_candle_1['rsi_14']
                 and (last_candle['rsi_14'] > (last_candle['rsi_14_1h'] + 16.0))
                 and (last_candle['sma_200_dec_20'])
@@ -2309,6 +2309,22 @@ class NostalgiaForInfinityX(IStrategy):
                 and (trade.open_date_utc + timedelta(minutes=3000) > current_time )
         ):
             return True, 'sell_stoploss_u_e_1'
+
+        # Under & near EMA200, local uptrend move
+        if (
+                (current_profit < -0.025)
+                and (last_candle['close'] < last_candle['ema_200'])
+                and (last_candle['cmf'] < -0.0)
+                and (((last_candle['ema_200'] - last_candle['close']) / last_candle['close']) < 0.02)
+                and last_candle['rsi_14'] > previous_candle_1['rsi_14']
+                and (last_candle['rsi_14'] > (last_candle['rsi_14_1h'] + 16.0))
+                and (last_candle['sma_200_dec_20'])
+                and (last_candle['sma_200_dec_24'])
+                and (current_time - timedelta(minutes=2880) > trade.open_date_utc)
+                # temporary
+                and (trade.open_date_utc + timedelta(minutes=3000) > current_time )
+        ):
+            return True, 'sell_stoploss_u_e_2'
 
         return False, None
 
