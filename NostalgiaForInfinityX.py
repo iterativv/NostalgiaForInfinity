d60518df6c257bd33741e651388050607ebad1c3
iterativ <vangelis.dimou@tutanota.com>
2021-12-22 02:01:13 +0200

    Buy 25: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v10.8.163"
+        return "v10.8.164"
 
     # ROI table:
     minimal_roi = {
@@ -8909,8 +8909,8 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append(dataframe['ema_12_1h'] > dataframe['ema_35_1h'])
                     item_buy_logic.append(dataframe['cmf_1h'].shift(12) < 0.0)
                     item_buy_logic.append(dataframe['cmf_1h'] > 0.0)
-                    item_buy_logic.append(dataframe['rsi_14'] < 48.0)
-                    item_buy_logic.append(dataframe['rsi_14_1h'] > 67.6)
+                    item_buy_logic.append(dataframe['rsi_14'] < 33.0)
+                    item_buy_logic.append(dataframe['cti_1h'] < 0.0)
 
                 # Condition #26 - Semi swing. Local deep dip.
                 elif index == 26:
