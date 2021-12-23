f3c1410b28164e629c02738e801bc1c17f0148fa
iterativ <vangelis.dimou@tutanota.com>
2021-12-23 18:14:15 +0200

    Buy 32: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v10.8.189"
+        return "v10.8.190"
 
     # ROI table:
     minimal_roi = {
@@ -1143,7 +1143,7 @@ class NostalgiaForInfinityX(IStrategy):
             "safe_dips_threshold_0"     : 0.028,
             "safe_dips_threshold_2"     : 0.09,
             "safe_dips_threshold_12"    : 0.26,
-            "safe_dips_threshold_144"   : 0.44,
+            "safe_dips_threshold_144"   : 0.3,
             "safe_pump_6h_threshold"    : 0.5,
             "safe_pump_12h_threshold"   : None,
             "safe_pump_24h_threshold"   : 0.78,
