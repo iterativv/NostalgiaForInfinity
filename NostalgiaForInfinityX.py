3f51f1385f81bfc1316ebf6ce6e5b700ab5a4be2
iterativ <vangelis.dimou@tutanota.com>
2021-12-27 12:58:32 +0200

    sell_pivot: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v10.9.22"
+        return "v10.9.23"
 
     # ROI table:
     minimal_roi = {
@@ -7958,40 +7958,40 @@ class NostalgiaForInfinityX(IStrategy):
 
         if (last_candle['close'] > (last_candle['res3_1d'] * 1.8)):
             if (0.02 > current_profit >= 0.01):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 44.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 44.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_1_1'
             elif (0.03 > current_profit >= 0.02):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 45.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 45.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_2_1'
             elif (0.04 > current_profit >= 0.03):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 46.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 46.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_3_1'
             elif (0.05 > current_profit >= 0.04):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 47.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 47.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_4_1'
             elif (0.06 > current_profit >= 0.05):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 48.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 48.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_5_1'
             elif (0.07 > current_profit >= 0.06):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 47.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 47.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_6_1'
             elif (0.08 > current_profit >= 0.07):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 46.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 46.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_7_1'
             elif (0.09 > current_profit >= 0.08):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 45.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 45.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_8_1'
             elif (0.1 > current_profit >= 0.09):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 44.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 44.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_9_1'
             elif (0.12 > current_profit >= 0.1):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 43.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 43.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_10_1'
             elif (0.2 > current_profit >= 0.12):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 42.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 42.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_11_1'
             elif (current_profit >= 0.2):
-                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 41.0) and (last_candle['rsi_14_1h'] > 60.0) and (last_candle['r_480_1h'] > -35.0):
+                if (last_candle['r_480'] > -35.0) and (last_candle['rsi_14'] < 41.0) and (last_candle['rsi_14_1h'] > 50.0) and (last_candle['r_480_1h'] > -35.0):
                     return True, 'sell_profit_pv_4_12_1'
 
         return False, None
