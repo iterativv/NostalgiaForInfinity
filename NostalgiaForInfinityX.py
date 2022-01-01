b8719d731f8651cc745a0cdf900fa4f6153836a0
Charlie <wieger.bontekoe@gmail.com>
2022-01-01 15:15:47 +0100

    Update NostalgiaForInfinityX.py

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -163,7 +163,7 @@ class NostalgiaForInfinityX(IStrategy):
 
     # These values can be overridden in the "ask_strategy" section in the config.
     use_sell_signal = True
-    sell_profit_only = False
+    sell_profit_only = True
     ignore_roi_if_buy_signal = True
 
     # Number of candles the strategy requires before producing valid signals
