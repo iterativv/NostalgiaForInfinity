7204fa7e716611e8e9cb2e1cd74eefbb89c64374
iterativ <vangelis.dimou@tutanota.com>
2022-01-05 09:47:38 +0200

    Buy 30: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v10.9.118"
+        return "v10.9.119"
 
     # ROI table:
     minimal_roi = {
@@ -9278,7 +9278,6 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.018))
                     item_buy_logic.append((dataframe['ema_26'].shift() - dataframe['ema_12'].shift()) > (dataframe['open'] / 100))
                     item_buy_logic.append(dataframe['close'] < (dataframe['bb20_2_low'] * 0.98))
-                    item_buy_logic.append(dataframe['r_480_1h'] < -22.0)
 
                 # Condition #31 - Long mode. Local dip.
                 elif index == 31:
