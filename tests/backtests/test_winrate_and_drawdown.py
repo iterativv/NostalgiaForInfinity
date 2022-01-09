96a0032922cf133225d98f82931df6bc5aba7a43
eSeR1805 <costianuv@gmail.com>
2022-01-09 17:39:10 +0200

    Add DD deviation for Binance December

--- tests/backtests/test_winrate_and_drawdown.py
+++ tests/backtests/test_winrate_and_drawdown.py
@@ -92,6 +92,7 @@ def deviations():
             ("20210801", "20210901"): {"max_drawdown": 40, "winrate": 85},
             ("20210901", "20211001"): {"max_drawdown": 40, "winrate": 80},
             ("20211001", "20211101"): {"max_drawdown": 60, "winrate": 85},
+            ("20211201", "20220101"): {"max_drawdown": 40, "winrate": 90},
         },
     }
 
