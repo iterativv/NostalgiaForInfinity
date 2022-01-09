8e6e33905d0cfaef155774cf98449884432c6567
eSeR1805 <costianuv@gmail.com>
2022-01-09 17:29:47 +0200

    Update backtests. December added

--- tests/backtests/test_winrate_and_drawdown.py
+++ tests/backtests/test_winrate_and_drawdown.py
@@ -59,6 +59,7 @@ def timerange_fmt(value):
         Timerange("20210901", "20211001"),
         Timerange("20211001", "20211101"),
         Timerange("20211101", "20211201"),
+        Timerange("20211201", "20220101"),
     ),
     ids=timerange_fmt,
 )
