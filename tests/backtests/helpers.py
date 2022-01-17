21692dff42296e3a9f627d84fc95da1a84e4a3c7
iterativ <vangelis.dimou@tutanota.com>
2022-01-17 11:59:57 +0200

    Fix filename for CI backtests.

--- tests/backtests/helpers.py
+++ tests/backtests/helpers.py
@@ -112,7 +112,7 @@ class Backtest:
             log.debug("Command Result:\n%s", ret)
         assert ret.exitcode == 0
         generated_results_file = list(
-            f for f in tmp_path.rglob("backtest-result-*.json") if "meta" not in f
+            f for f in tmp_path.rglob("backtest-results-*.json") if "meta" not in f
         )[0]
         generated_json_ci_results_artifact_path = None
         if self.request.config.option.artifacts_path:
