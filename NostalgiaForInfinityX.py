c8ec5870ef357ec487d2ac11204e936259c743da
iterativ <vangelis.dimou@tutanota.com>
2022-01-19 10:06:26 +0200

    OKX links to march the recent domain name change.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -98,7 +98,7 @@ else:
 ##  Binance: https://accounts.binance.com/en/register?ref=EAZC47FM (5% discount on trading fees)         ##
 ##  Kucoin: https://www.kucoin.com/r/QBSSSPYV (5% discount on trading fees)                              ##
 ##  Gate.io: https://www.gate.io/signup/8054544 (10% discount on trading fees)                           ##
-##  OKEx: https://www.okex.com/join/11749725760 (5% discount on trading fees)                            ##
+##  OKX: https://www.okx.com/join/11749725760 (5% discount on trading fees)                              ##
 ##  Huobi: https://www.huobi.com/en-us/topic/double-reward/?invite_code=ubpt2223                         ##
 ###########################################################################################################
 
