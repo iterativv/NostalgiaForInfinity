6cfa8f15522abf1167eb2e6d0edaddc6f6d537a3
iterativ <vangelis.dimou@tutanota.com>
2022-01-22 16:26:02 +0200

    Buy 3: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.45"
+        return "v11.0.46"
 
     # ROI table:
     minimal_roi = {
@@ -343,7 +343,7 @@ class NostalgiaForInfinityX(IStrategy):
             "safe_pump_12h_threshold"   : None,
             "safe_pump_24h_threshold"   : 0.5,
             "safe_pump_36h_threshold"   : None,
-            "safe_pump_48h_threshold"   : 0.9,
+            "safe_pump_48h_threshold"   : 0.76,
             "btc_1h_not_downtrend"      : False,
             "close_over_pivot_type"     : "none", # pivot, sup1, sup2, sup3, res1, res2, res3
             "close_over_pivot_offset"   : 1.0,
