c719c3263d35d9b340c455b1f7fd58f12eb57497
iterativ <vangelis.dimou@tutanota.com>
2022-01-23 09:11:43 +0200

    sell_stoploss: increase the trade duration threshold check.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.56"
+        return "v11.0.57"
 
     # ROI table:
     minimal_roi = {
@@ -2416,7 +2416,7 @@ class NostalgiaForInfinityX(IStrategy):
 
         if (
                 (current_time - timedelta(minutes=30) > trade.open_date_utc)
-                and (trade.open_date_utc + timedelta(minutes=5760) > current_time)
+                and (trade.open_date_utc + timedelta(minutes=7000) > current_time)
         ):
             if (-0.12 <= current_profit < -0.08):
                 if (last_candle['close'] < last_candle['atr_high_thresh_1']) and (previous_candle_1['close'] > previous_candle_1['atr_high_thresh_1']) and (last_candle['cmf'] < -0.0):
