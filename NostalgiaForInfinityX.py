ff4a0a1f206ab01987e7a408b74e54d9ff1ebdae
iterativ <vangelis.dimou@tutanota.com>
2022-01-27 19:53:05 +0200

    Buy 38: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.95"
+        return "v11.0.96"
 
     # ROI table:
     minimal_roi = {
@@ -9630,14 +9630,12 @@ class NostalgiaForInfinityX(IStrategy):
                 # Condition #38 - Semi swing. Uptrend. Local dip.
                 elif index == 38:
                     # Non-Standard protections
-                    item_buy_logic.append(dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.01))
+                    item_buy_logic.append(dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.0118))
 
                     # Logic
                     item_buy_logic.append(dataframe['ema_26'] > dataframe['ema_12'])
-                    item_buy_logic.append((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.024))
+                    item_buy_logic.append((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.02))
                     item_buy_logic.append((dataframe['ema_26'].shift() - dataframe['ema_12'].shift()) > (dataframe['open'] / 100))
-                    item_buy_logic.append(dataframe['r_64'] < -66.0)
-                    item_buy_logic.append(dataframe['r_96'] < -50.0)
                     item_buy_logic.append(dataframe['r_480_1h'] < -1.0)
 
                 # Condition #39 - Semi swing. Uptrend. Local dip.
