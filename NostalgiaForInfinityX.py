73960c1866a60a12b5e618eb0a2b5783b2778253
iterativ <vangelis.dimou@tutanota.com>
2022-01-31 14:42:00 +0200

    sell_stoploss: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -107,7 +107,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.124"
+        return "v11.0.125"
 
     # ROI table:
     minimal_roi = {
@@ -2452,15 +2452,15 @@ class NostalgiaForInfinityX(IStrategy):
         ):
             return True, 'sell_stoploss_u_e_1'
 
-        if (
-                (current_profit < -0.12)
-                and (last_candle['close'] < last_candle['ema_200'])
-                and (last_candle['close'] < (last_candle['ema_200'] - last_candle['atr']))
-                and (last_candle['sma_200_dec_20'])
-                # temporary
-                and (trade.open_date_utc + timedelta(minutes=3000) > current_time)
-        ):
-            return True, 'sell_stoploss_doom'
+        # if (
+        #         (current_profit < -0.12)
+        #         and (last_candle['close'] < last_candle['ema_200'])
+        #         and (last_candle['close'] < (last_candle['ema_200'] - last_candle['atr']))
+        #         and (last_candle['sma_200_dec_20'])
+        #         # temporary
+        #         and (trade.open_date_utc + timedelta(minutes=3000) > current_time)
+        # ):
+        #     return True, 'sell_stoploss_doom'
 
         if (
                 (current_time - timedelta(minutes=30) > trade.open_date_utc)
