7d64286454797b3226f5e8684b0891a1ba50a47a
iterativ <vangelis.dimou@tutanota.com>
2022-02-02 17:24:08 +0200

    Buy 56: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -110,7 +110,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.157"
+        return "v11.0.158"
 
     # ROI table:
     minimal_roi = {
@@ -9920,6 +9920,7 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append(dataframe['cti_15m'].shift(3).rolling(15).max() < -0.9)
                     item_buy_logic.append(dataframe['r_14_15m'].shift(3) < -90.0)
                     item_buy_logic.append(dataframe['r_14'] < -65.0)
+                    item_buy_logic.append(dataframe['crsi_1h'] > 0.0)
 
                 # Condition #57 - 15m. Semi swing. Strong uptrend. Local dip. BTC not downtrend.
                 elif index == 57:
