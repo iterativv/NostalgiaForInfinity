8e045c87403672c9720ea4c397c8119bf12e166a
iterativ <vangelis.dimou@tutanota.com>
2022-02-07 13:40:11 +0200

    Rebuy: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -110,7 +110,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.186"
+        return "v11.0.187"
 
     # ROI table:
     minimal_roi = {
@@ -164,7 +164,7 @@ class NostalgiaForInfinityX(IStrategy):
     # Rebuy feature
     position_adjustment_enable = True
     max_rebuy_orders = 2
-    max_rebuy_multiplier = 1.0
+    max_rebuy_multiplier = 1.33
 
     # Run "populate_indicators()" only for new candle.
     process_only_new_candles = True
@@ -2356,7 +2356,7 @@ class NostalgiaForInfinityX(IStrategy):
                 # This returns first order stake size
                 stake_amount = filled_buys[0].cost
                 # This then calculates current safety order size
-                stake_amount = stake_amount * (0.25 + (count_of_buys * 0.005))
+                stake_amount = stake_amount * (0.5 + (count_of_buys * 0.005))
                 return stake_amount
             except Exception as exception:
                 return None
