af2ea70da72cf4dcc3b15425c421a70c315cf674
iterativ <vangelis.dimou@tutanota.com>
2022-02-12 16:02:08 +0200

    sell_stoploss: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -110,7 +110,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.228"
+        return "v11.0.229"
 
     # ROI table:
     minimal_roi = {
@@ -2467,12 +2467,15 @@ class NostalgiaForInfinityX(IStrategy):
                 (current_profit < -0.025)
                 and (last_candle['close'] < last_candle['ema_200'])
                 and (last_candle['cmf'] < -0.0)
-                and (((last_candle['ema_200'] - last_candle['close']) / last_candle['close']) < 0.024)
+                and (last_candle['ema_vwma_osc_96'] < -0.0)
+                and (((last_candle['ema_200'] - last_candle['close']) / last_candle['close']) < 0.004)
                 and last_candle['rsi_14'] > previous_candle_1['rsi_14']
-                and (last_candle['rsi_14'] > (last_candle['rsi_14_1h'] + 16.0))
-                and (current_time - timedelta(minutes=7200) > trade.open_date_utc)
+                and (last_candle['rsi_14'] > (last_candle['rsi_14_1h'] + 20.0))
+                and (last_candle['sma_200_dec_20'])
+                and (last_candle['sma_200_dec_24'])
+                and (current_time - timedelta(minutes=9200) > trade.open_date_utc)
                 # temporary
-                and (trade.open_date_utc + timedelta(minutes=22000) > current_time)
+                and (trade.open_date_utc + timedelta(minutes=24000) > current_time)
         ):
             return True, 'sell_stoploss_u_e_2'
 
