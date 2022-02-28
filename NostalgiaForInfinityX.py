155b591f04017c62646944071479c671cb25f852
iterativ <vangelis.dimou@tutanota.com>
2022-02-28 09:35:26 +0200

    Buy 8: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -112,7 +112,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.313"
+        return "v11.0.314"
 
     # ROI table:
     minimal_roi = {
@@ -495,7 +495,7 @@ class NostalgiaForInfinityX(IStrategy):
             "close_over_pivot_type"     : "none", # pivot, sup1, sup2, sup3, res1, res2, res3
             "close_over_pivot_offset"   : 0.98,
             "close_under_pivot_type"    : "res3", # pivot, sup1, sup2, sup3, res1, res2, res3
-            "close_under_pivot_offset"  : 1.6
+            "close_under_pivot_offset"  : 1.2
         },
         9: {
             "ema_fast"                  : False,
