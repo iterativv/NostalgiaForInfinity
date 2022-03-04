d60ea787a4edb2ad52c8ccb880e8eee2630ac3be
iterativ <vangelis.dimou@tutanota.com>
2022-03-04 12:49:17 +0200

    Buy 24: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -112,7 +112,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.334"
+        return "v11.0.335"
 
     # ROI table:
     minimal_roi = {
@@ -10026,7 +10026,7 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append(dataframe['ewo_1h'] > 2.8)
                     item_buy_logic.append(dataframe['cti_1h'] < 0.9)
                     item_buy_logic.append(dataframe['r_480_1h'] < -25.0)
-                    item_buy_logic.append(dataframe['volume_mean_12'] > (dataframe['volume_mean_24'] * 0.7))
+                    item_buy_logic.append(dataframe['volume_mean_12'] > (dataframe['volume_mean_24'] * 0.86))
 
                 # Condition #25 - Semi swing. CMF 1h cross.
                 elif index == 25:
