90c769905c31fe126aef4821f51a778c91205447
iterativ <vangelis.dimou@tutanota.com>
2022-04-03 17:57:56 +0300

    Buy 24: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -114,7 +114,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.495"
+        return "v11.0.496"
 
     # ROI table:
     minimal_roi = {
@@ -934,7 +934,7 @@ class NostalgiaForInfinityX(IStrategy):
             "sma200_1h_rising_val"      : "24",
             "safe_dips_threshold_0"     : 0.032,
             "safe_dips_threshold_2"     : 0.09,
-            "safe_dips_threshold_12"    : 0.16,
+            "safe_dips_threshold_12"    : 0.12,
             "safe_dips_threshold_144"   : 0.44,
             "safe_pump_6h_threshold"    : 0.4,
             "safe_pump_12h_threshold"   : None,
@@ -10122,7 +10122,7 @@ class NostalgiaForInfinityX(IStrategy):
                     # Logic
                     item_buy_logic.append(dataframe['ewo'] > 3.4)
                     item_buy_logic.append(dataframe['r_14'] < -97.0)
-                    item_buy_logic.append(dataframe['r_96'] < -90.0)
+                    item_buy_logic.append(dataframe['r_96'] < -80.0)
                     item_buy_logic.append(dataframe['ewo_1h'] > 2.7)
                     item_buy_logic.append(dataframe['cti_1h'] < 0.9)
                     item_buy_logic.append(dataframe['r_480_1h'] < -25.0)
