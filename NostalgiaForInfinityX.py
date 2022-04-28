4ae26081c1d73b2c2326cee8ab6cbc54c98cf8bb
iterativ <vangelis.dimou@tutanota.com>
2022-04-28 09:20:13 +0300

    Buy 3: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -115,7 +115,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.611
+        return "v11.0.611"
 
     # ROI table:
     minimal_roi = {
