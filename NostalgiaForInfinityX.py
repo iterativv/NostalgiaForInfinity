758a8ce46645e040ad1c7008e3d1e1974e7fabd2
iterativ <vangelis.dimou@tutanota.com>
2022-05-12 16:17:47 +0300

    Buy 63: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -115,7 +115,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.738"
+        return "v11.0.739"
 
     # ROI table:
     minimal_roi = {
@@ -10801,6 +10801,7 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['volume_mean_12'] > (dataframe['volume_mean_24'] * 0.95))
                     )
                     item_buy_logic.append(dataframe['volume'] < (dataframe['volume_mean_12'] * 1.4))
+                    item_buy_logic.append(dataframe['volume_mean_12'] > (dataframe['volume_mean_24'] * 0.8))
 
                 # Condition #64 - Semi swing. Squeeze momentum.
                 elif index == 64:
