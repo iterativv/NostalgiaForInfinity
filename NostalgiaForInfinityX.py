5cdab8b26a1c3819bdb13cb0d978c5424519b6fd
iterativ <vangelis.dimou@tutanota.com>
2022-05-23 14:29:32 +0300

    Buy 21: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -115,7 +115,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.865"
+        return "v11.0.866"
 
     # ROI table:
     minimal_roi = {
@@ -10666,6 +10666,15 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['close'] > dataframe['ema_26'])
                         | (dataframe['close'] < dataframe['sma_30'] * 0.912)
                         | (dataframe['close'] < (dataframe['res3_1d'] * 1.4))
+                    )
+                    item_buy_logic.append(
+                        (dataframe['btc_not_downtrend_1h'] == True)
+                        | (dataframe['cmf'] > -0.1)
+                        | (dataframe['mfi'] > 6.0)
+                        | (dataframe['cti_1h'] < 0.5)
+                        | (dataframe['tpct_change_144'] < 0.18)
+                        | (dataframe['hl_pct_change_48_1h'] < 0.75)
+                        | (dataframe['close'] < dataframe['ema_20'] * 0.936)
                     )
 
                 # Condition #22 - Swing. Uptrend. Bounce from daily support level
