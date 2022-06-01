7362a5fd2aff2f05518abdb490eac92e292d44eb
iterativ <vangelis.dimou@tutanota.com>
2022-06-01 09:40:41 +0300

    Add TRY to informative stake.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -9364,7 +9364,7 @@ class NostalgiaForInfinityX(IStrategy):
         informative_pairs.extend([(pair, self.info_timeframe_1d) for pair in pairs])
         informative_pairs.extend([(pair, self.info_timeframe_15m) for pair in pairs])
 
-        if self.config['stake_currency'] in ['USDT','BUSD','USDC','DAI','TUSD','PAX','USD','EUR','GBP','BRL']:
+        if self.config['stake_currency'] in ['USDT','BUSD','USDC','DAI','TUSD','PAX','USD','EUR','GBP', 'TRY', 'BRL']:
             btc_info_pair = f"BTC/{self.config['stake_currency']}"
         else:
             btc_info_pair = "BTC/USDT"
