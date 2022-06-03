9678dbe44bbd44990fafc2fdc8636ee2df5971ff
iterativ <vangelis.dimou@tutanota.com>
2022-06-03 09:19:56 +0300

    sell_stoploss: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -115,7 +115,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 2
 
     def version(self) -> str:
-        return "v11.0.993"
+        return "v11.0.994"
 
     # ROI table:
     minimal_roi = {
@@ -2667,6 +2667,8 @@ class NostalgiaForInfinityX(IStrategy):
                 and (last_candle['rsi_14'] > last_candle['rsi_14_1h'] + 20.0)
                 and (last_candle['cmf'] < -0.0)
                 and (last_candle['sma_200_dec_24'])
+                and (last_candle['sma_200_dec_20'])
+                and (last_candle['sma_200_dec_20_1h'])
                 and (last_candle['btc_not_downtrend_1h'] == False)
                 and (current_time - timedelta(minutes=30) > trade.open_date_utc)
                 # temporary
@@ -2677,15 +2679,15 @@ class NostalgiaForInfinityX(IStrategy):
 
         # For times with strongly negative sentiment
         if (
-                (current_profit < [-0.25, -0.25, -0.25][stop_index])
+                (current_profit < [-0.16, -0.16, -0.2][stop_index])
                 and (last_candle['close'] < last_candle['ema_200'])
                 and (last_candle['close'] < (last_candle['ema_200'] - last_candle['atr']))
                 and (last_candle['sma_200_dec_20'])
                 and (last_candle['cmf'] < -0.0)
                 and (last_candle['btc_not_downtrend_1h'] == False)
-                and (last_candle['btc_tpct_change_144_5m'] > 0.05)
+                and (last_candle['btc_tpct_change_144_5m'] > 0.06)
                 and (last_candle['hl_pct_change_24_1h'] > 0.5)
-                and (last_candle['tpct_change_144'] > 0.2)
+                and (last_candle['tpct_change_144'] > 0.25)
                 # temporary
                 and (trade.open_date_utc.replace(tzinfo=None) > datetime(2022, 5, 12) or is_backtest)
         ):
@@ -2693,7 +2695,7 @@ class NostalgiaForInfinityX(IStrategy):
 
         # Absolute limit, just in case...
         if (
-                (current_profit < [-0.25, -0.25, -0.25][stop_index])
+                (current_profit < [-0.2, -0.2, -0.25][stop_index])
                 # temporary
                 and (trade.open_date_utc.replace(tzinfo=None) > datetime(2022, 5, 25) or is_backtest)
         ):
