1b94d1d1c2271539182a82dfe91dc2d2229db6f9
iterativ <vangelis.dimou@tutanota.com>
2022-07-12 14:59:30 +0300

    Rebuys: fine tune the thresholds for mode 5 (half mode).

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -115,7 +115,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.0.1274"
+        return "v11.0.1275"
 
 
     # ROI table:
@@ -189,7 +189,7 @@ class NostalgiaForInfinityX(IStrategy):
     rebuy_pcts_p_2 = (0.02, 0.025, 0.025, 0.03, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095)
     rebuy_pcts_n_3 = (-0.02, -0.04, -0.06, -0.08, -0.1, -0.12, -0.14, -0.16)
     rebuy_pcts_n_4 = (-0.02, -0.06, -0.1)
-    rebuy_pcts_n_5 = (-0.1, -0.14)
+    rebuy_pcts_n_5 = (-0.05, -0.08)
     rebuy_multi_0 = 0.15
     rebuy_multi_1 = 0.35
     rebuy_multi_2 = 1.0
