19d07b06c060957e7d7a312cad5f195a5ccbab29
iterativ <vangelis.dimou@tutanota.com>
2022-09-05 10:49:53 +0300

    Buy 10: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -116,7 +116,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.23"
+        return "v11.2.24"
 
 
     # ROI table:
@@ -12246,11 +12246,18 @@ class NostalgiaForInfinityX(IStrategy):
                     )
                     item_buy_logic.append(
                         (dataframe['btc_not_downtrend_1h'] == True)
+                        | (dataframe['r_480'] > -30.0)
                         | (dataframe['cti_1h'] < -0.9)
                         | (dataframe['tpct_change_144'] < 0.1)
+                        | (dataframe['close_max_48'] < (dataframe['close'] * 1.08))
+                        | (dataframe['btc_pct_close_max_72_5m'] < 1.01)
+                        | (dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.01))
                         | (dataframe['close'] > dataframe['ema_26'])
+                        | (dataframe['close'] < dataframe['bb20_2_low'] * 0.97)
                         | (dataframe['close'] < dataframe['ema_20'] * 0.91)
                         | ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.04))
+                        | (dataframe['close_15m'] < (dataframe['bb20_2_low_15m'] * 0.95))
+                        | ((dataframe['ema_26_15m'] - dataframe['ema_12_15m']) > (dataframe['open_15m'] * 0.03))
                     )
                     item_buy_logic.append(
                         (dataframe['cmf'] > -0.2)
