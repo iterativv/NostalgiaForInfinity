2517733fc19ab7790fba97e53ab7c602e87d312a
iterativ <vangelis.dimou@tutanota.com>
2022-09-06 19:06:28 +0300

    Rebuys: fine tune the thresholds for mode 0.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.27"
+        return "v11.2.28"
 
 
     # ROI table:
@@ -2672,7 +2672,7 @@ class NostalgiaForInfinityX(IStrategy):
             if (1 <= count_of_entries <= 1):
                 if (
                         (current_profit < self.rebuy_pcts_n_0[count_of_entries - 1])
-                        and (last_candle['ha_close'] > last_candle['ha_open'])
+                        and (last_candle['close_max_48'] < (last_candle['close'] * 1.05))
                         and (last_candle['btc_pct_close_max_72_5m'] < 1.02)
                 ):
                     is_rebuy = True
