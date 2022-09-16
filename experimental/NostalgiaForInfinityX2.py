f2d7c2a135d441b5a513944234f99df71bad2db7
iterativ <vangelis.dimou@tutanota.com>
2022-09-16 19:09:30 +0300

    X2: update the ref links.

--- experimental/NostalgiaForInfinityX2.py
+++ experimental/NostalgiaForInfinityX2.py
@@ -48,7 +48,9 @@ log = logging.getLogger(__name__)
 ##   Gate.io: https://www.gate.io/signup/8054544 (10% discount on trading fees)                          ##
 ##   FTX: https://ftx.com/eu/profile#a=100178030 (5% discount on trading fees)                           ##
 ##   OKX: https://www.okx.com/join/11749725760 (5% discount on trading fees)                             ##
+##   ByBit: https://partner.bybit.com/b/nfi                                                              ##
 ##   Huobi: https://www.huobi.com/en-us/topic/double-reward/?invite_code=ubpt2223                        ##
+##   Bitvavo: https://account.bitvavo.com/create?a=D22103A4BC (no fees for the first € 1000)             ##
 ###########################################################################################################
 
 class NostalgiaForInfinityX2(IStrategy):
