6deb0a33f2d9da4d759444455c37f9ba68ae33db
iterativ <vangelis.dimou@tutanota.com>
2022-09-23 15:57:46 +0300

    Buy 30: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.164"
+        return "v11.2.165"
 
 
     # ROI table:
@@ -14336,19 +14336,27 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['mfi'] > 20.0)
                         | (dataframe['rsi_14'] < 10.0)
                         | (dataframe['cti'] < -0.9)
-                        | (dataframe['crsi'] > 10.0)
-                        | (dataframe['cti_1h'] < 0.8)
+                        | (dataframe['crsi'] > 16.0)
+                        | (dataframe['cti_1h'] < -0.5)
                         | (dataframe['rsi_14_1h'] < 50.0)
                         | (dataframe['r_14_1h'] < -75.0)
-                        | (dataframe['tpct_change_144'] < 0.2)
-                        | (dataframe['close_max_48'] < (dataframe['close'] * 1.16))
-                        | (dataframe['hl_pct_change_36'] < 0.2)
+                        | (dataframe['tpct_change_144'] < 0.16)
+                        | (dataframe['close_max_48'] < (dataframe['close'] * 1.12))
+                        | (dataframe['hl_pct_change_36'] < 0.12)
                         | (dataframe['hl_pct_change_48_1h'] < 0.5)
-                        | (dataframe['btc_pct_close_max_72_5m'] < 1.01)
+                        |
+                        (
+                            (dataframe['btc_pct_close_max_72_5m'] < 1.01)
+                            & (dataframe['close'] < (dataframe['res3_1d'] * 1.0))
+                        )
                         | (dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.01))
                         | (dataframe['close'] < (dataframe['res1_1d'] * 1.0))
                         | (dataframe['close'] > (dataframe['sma_200'] * 0.99))
-                        | (dataframe['close'] > (dataframe['sup_level_1h'] * 0.95))
+                        |
+                        (
+                            (dataframe['close'] > (dataframe['sup_level_1h'] * 0.95))
+                            & (dataframe['close'] < (dataframe['res3_1d'] * 1.0))
+                        )
                         | (dataframe['close'] < dataframe['sma_30'] * 0.86)
                         | (dataframe['close'] < dataframe['ema_20'] * 0.88)
                         | (dataframe['close'] < dataframe['bb20_2_low'] * 0.95)
