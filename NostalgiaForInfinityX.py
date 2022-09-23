d72e8d94e668bbad6234de3497ac6679a351c024
iterativ <vangelis.dimou@tutanota.com>
2022-09-23 22:42:23 +0300

    Buy 3: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.176"
+        return "v11.2.177"
 
 
     # ROI table:
@@ -10741,19 +10741,22 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['rsi_14_1h'] < 20.0)
                         | (dataframe['r_14_1h'] < -90.0)
                         | (dataframe['crsi_1h'] > 10.0)
-                        | (dataframe['tpct_change_144'] < 0.18)
+                        | (dataframe['tpct_change_144'] < 0.16)
                         | (dataframe['btc_pct_close_max_72_5m'] < 1.03)
-                        | (dataframe['sma_200'] > dataframe['sma_200'].shift(24))
                         | (dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.01))
                         | (dataframe['ema_20'] > dataframe['ema_50'])
                         | (dataframe['close'] > dataframe['ema_26'])
                         | (dataframe['close'] > (dataframe['sma_200_1h'] * 0.8))
                         | (dataframe['close'] < dataframe['bb20_2_low'] * 0.99)
                         | (dataframe['close'] < dataframe['ema_20'] * 0.946)
-                        | (dataframe['close'] > (dataframe['sup2_1d'] * 1.0))
+                        |
+                        (
+                            (dataframe['close'] > (dataframe['sup2_1d'] * 1.0))
+                            & (dataframe['close'] > (dataframe['sma_200_1h'] * 0.7))
+                        )
                         | (dataframe['close_15m'] < (dataframe['bb20_2_low_15m'] * 0.999))
-                        | ((dataframe['ema_26_15m'] - dataframe['ema_12_15m']) > (dataframe['open_15m'] * 0.02))
-                        | (dataframe['bb40_2_delta'].gt(dataframe['close'] * 0.068))
+                        | ((dataframe['ema_26_15m'] - dataframe['ema_12_15m']) > (dataframe['open_15m'] * 0.03))
+                        | (dataframe['bb40_2_delta'].gt(dataframe['close'] * 0.074))
                         | (dataframe['closedelta'].gt(dataframe['close'] * 0.032))
                     )
                     item_buy_logic.append(
