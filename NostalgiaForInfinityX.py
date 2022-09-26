4ae73a9073320342c4f753210395178c8fb00620
iterativ <vangelis.dimou@tutanota.com>
2022-09-26 10:11:47 +0300

    Buy 4: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.246"
+        return "v11.2.247"
 
 
     # ROI table:
@@ -11380,7 +11380,6 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['mfi'] > 46.0)
                         | (dataframe['rsi_14'] < 12.0)
                         | (dataframe['cti'] < -0.95)
-                        | (dataframe['crsi'] > 10.0)
                         | (dataframe['cti_1h'] < -0.8)
                         | (dataframe['rsi_14_1h'] < 20.0)
                         | (dataframe['r_14_1h'] < -97.0)
@@ -11395,13 +11394,13 @@ class NostalgiaForInfinityX(IStrategy):
                         |
                         (
                             (dataframe['btc_pct_close_max_72_5m'] < 1.01)
-                            & (dataframe['tpct_change_144'] < 0.3)
+                            & (dataframe['tpct_change_144'] < 0.25)
                         )
                         | (dataframe['close'] > (dataframe['sma_200'] * 0.9))
                         | (dataframe['close'] < dataframe['ema_20'] * 0.88)
                         | (dataframe['close'] < dataframe['bb20_2_low'] * 0.955)
                         | ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.05))
-                        | (dataframe['close_15m'] < (dataframe['bb20_2_low_15m'] * 0.93))
+                        | (dataframe['close_15m'] < (dataframe['bb20_2_low_15m'] * 0.89))
                         | ((dataframe['ema_26_15m'] - dataframe['ema_12_15m']) > (dataframe['open_15m'] * 0.04))
                         | (dataframe['rsi_14_15m'] < 15.0)
                     )
