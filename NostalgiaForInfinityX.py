bbc8ddbaffcd04bcb45b76de965a6685184f14f4
iterativ <vangelis.dimou@tutanota.com>
2022-09-26 12:49:59 +0300

    Buy 24: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.252"
+        return "v11.2.253"
 
 
     # ROI table:
@@ -13971,11 +13971,7 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.01))
                     )
                     item_buy_logic.append(
-                        (
-
-                            (dataframe['ewo'] > 4.2)
-                            & (dataframe['btc_pct_close_max_72_5m'] < 1.03)
-                        )
+                       (dataframe['ewo'] > 4.2)
                         | (dataframe['cti_1h'] < 0.5)
                         | (dataframe['rsi_14_1h'] < 50.0)
                         | (dataframe['hl_pct_change_48_1h'] < 0.4)
@@ -14047,13 +14043,20 @@ class NostalgiaForInfinityX(IStrategy):
                         | ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.028))
                     )
                     item_buy_logic.append(
-                        (dataframe['ewo'] > 4.0)
+                        (
+                            (dataframe['ewo'] > 4.0)
+                            & (dataframe['btc_pct_close_max_72_5m'] < 1.03)
+                        )
                         | (dataframe['cmf'] > -0.3)
                         | (dataframe['mfi'] > 20.0)
                         | (dataframe['rsi_14'] < 25.0)
                         | (dataframe['cti_1h'] < 0.5)
                         | (dataframe['rsi_14_1h'] < 50.0)
-                        | (dataframe['crsi_1h'] > 30.0)
+                        |
+                        (
+                            (dataframe['crsi_1h'] > 30.0)
+                            & (dataframe['btc_pct_close_max_72_5m'] < 1.03)
+                        )
                         | (dataframe['tpct_change_144'] < 0.08)
                         | (dataframe['close_max_48'] < (dataframe['close'] * 1.06))
                         | (dataframe['hl_pct_change_48_1h'] < 0.3)
