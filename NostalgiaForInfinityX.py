1300973b2e64b7d99273704cfea1e4b65540d32d
iterativ <vangelis.dimou@tutanota.com>
2022-09-27 15:01:27 +0300

    sell_stoploss: BTC stake to -16%.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.286"
+        return "v11.2.287"
 
 
     # ROI table:
@@ -2962,7 +2962,7 @@ class NostalgiaForInfinityX(IStrategy):
                 return True, 'sell_stoploss_stop_2'
 
             if (
-                (current_profit < [-0.1, -0.1, -0.18][stop_index])
+                (current_profit < [-0.16, -0.16, -0.18][stop_index])
                 and (current_time - timedelta(hours=1) > trade.open_date_utc)
                 # temporary
                 and (trade.open_date_utc.replace(tzinfo=None) > datetime(2022, 9, 21) or is_backtest)
