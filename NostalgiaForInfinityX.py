b3d86df477028727f65cefe0806e039388685e5d
iterativ <vangelis.dimou@tutanota.com>
2022-10-05 00:49:49 +0300

    Buy 14: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.424"
+        return "v11.2.425"
 
 
     # ROI table:
@@ -13374,15 +13374,27 @@ class NostalgiaForInfinityX(IStrategy):
                             (dataframe['btc_not_downtrend_1h'] == True)
                             & (dataframe['close'] < (dataframe['res3_1d'] * 1.0))
                         )
-                        | (dataframe['cmf'] > 0.0)
-                        | (dataframe['mfi'] > 40.0)
+                        |
+                        (
+                            (dataframe['cmf'] > 0.0)
+                            & (dataframe['cti_1h'] < 0.75)
+                        )
+                        |
+                        (
+                            (dataframe['mfi'] > 40.0)
+                            & (dataframe['cti_1h'] < 0.75)
+                        )
                         | (dataframe['rsi_14'] < 30.0)
                         | (dataframe['cti'] < -0.9)
                         | (dataframe['cti_1h'] < 0.5)
                         | (dataframe['rsi_14_1h'] < 50.0)
                         | (dataframe['tpct_change_144'] < 0.1)
                         | (dataframe['close_max_48'] < (dataframe['close'] * 1.1))
-                        | (dataframe['btc_pct_close_max_72_5m'] < 1.01)
+                        |
+                        (
+                            (dataframe['btc_pct_close_max_72_5m'] < 1.01)
+                            & (dataframe['btc_not_downtrend_1h'] == True)
+                        )
                         | (dataframe['close'] < dataframe['ema_20'] * 0.95)
                         | (dataframe['close'] < dataframe['bb20_2_low'] * 0.98)
                         | ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.02))
