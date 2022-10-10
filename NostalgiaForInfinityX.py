8c0137b3f59c504403c26700563a10f03cd963bb
iterativ <vangelis.dimou@tutanota.com>
2022-10-10 13:29:59 +0300

    Buy 39: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.466"
+        return "v11.2.467"
 
 
     # ROI table:
@@ -15475,6 +15475,7 @@ class NostalgiaForInfinityX(IStrategy):
                         (
                             (dataframe['cmf'] > -0.1)
                             & (dataframe['btc_pct_close_max_72_5m'] < 1.02)
+                            & (dataframe['tpct_change_144'] < 0.2)
                         )
                         | (dataframe['cti'] < -0.95)
                         | (dataframe['cti_1h'] < 0.5)
@@ -15482,7 +15483,11 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['tpct_change_144'] < 0.1)
                         | (dataframe['close'] < dataframe['sma_30'] * 0.89)
                         | (dataframe['ema_200'] > (dataframe['ema_200'].shift(12) * 1.015))
-                        | (dataframe['close'] < (dataframe['bb20_2_low'] * 0.99))
+                        |
+                        (
+                            (dataframe['close'] < (dataframe['bb20_2_low'] * 0.99))
+                            & (dataframe['rsi_14'] < 36.0)
+                        )
                         | (dataframe['bb40_2_delta'].gt(dataframe['close'] * 0.12))
                         | (dataframe['closedelta'].gt(dataframe['close'] * 0.02))
                     )
