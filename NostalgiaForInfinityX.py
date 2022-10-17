0fd18ec3bb2aeb54a8de0b04652fa5ead103f481
iterativ <vangelis.dimou@tutanota.com>
2022-10-17 06:15:40 +0300

    Buy 74: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.513"
+        return "v11.2.514"
 
 
     # ROI table:
@@ -19522,6 +19522,7 @@ class NostalgiaForInfinityX(IStrategy):
                     item_buy_logic.append(dataframe['btc_pct_close_max_24_5m'] < 1.03)
                     item_buy_logic.append(dataframe['btc_pct_close_max_72_5m'] < 1.03)
                     item_buy_logic.append(dataframe['hl_pct_change_36'] < 0.5)
+                    item_buy_logic.append(dataframe['ema_200_pct_change_144'] < 0.12)
 
                     # Logic
                     item_buy_logic.append(dataframe['rsi_14'] < dataframe['rsi_14'].shift(1))
