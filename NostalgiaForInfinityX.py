f15e570b75bab6ae9506f9c69db6bad9866028a1
iterativ <vangelis.dimou@tutanota.com>
2022-10-19 11:15:29 +0300

    Buy 16: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.530"
+        return "v11.2.531"
 
 
     # ROI table:
@@ -13723,6 +13723,7 @@ class NostalgiaForInfinityX(IStrategy):
                 # Condition #16 - Semi swing. Cross above.
                 elif index == 16:
                     # Non-Standard protections
+                    item_buy_logic.append(dataframe['hl_pct_change_36'] < 0.5)
 
                     # Logic
                     item_buy_logic.append(dataframe['ema_12_1h'].shift(12) < dataframe['ema_35_1h'].shift(12))
@@ -13763,6 +13764,7 @@ class NostalgiaForInfinityX(IStrategy):
                         (
                             (dataframe['close'] < (dataframe['res1_1d'] * 1.0))
                             & (dataframe['btc_pct_close_max_72_5m'] < 1.01)
+                            & (dataframe['rsi_14'] < 36.0)
                         )
                         | (dataframe['close'] < dataframe['sma_30'] * 0.99)
                         | (dataframe['close'] < dataframe['bb20_2_low'] * 0.999)
