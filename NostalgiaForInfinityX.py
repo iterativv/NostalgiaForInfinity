88613519255acd931836e45ac17c86b26bf1d417
iterativ <vangelis.dimou@tutanota.com>
2022-10-23 17:12:25 +0300

    Buy 57: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.578"
+        return "v11.2.579"
 
 
     # ROI table:
@@ -17645,28 +17645,31 @@ class NostalgiaForInfinityX(IStrategy):
                             (dataframe['cmf'] > 0.0)
                             & (dataframe['close'] < (dataframe['res3_1d'] * 1.1))
                         )
-                        | (dataframe['mfi'] > 46.0)
+                        | (dataframe['mfi'] > 50.0)
                         | (dataframe['rsi_14'] < 30.0)
                         |
                         (
                             (dataframe['cti'] < -0.8)
                             & (dataframe['ema_200_pct_change_144'] < 0.05)
+                            & (dataframe['rsi_14'] < 36.0)
                         )
                         | (dataframe['cti_1h'] < -0.5)
                         | (dataframe['rsi_14_1h'] < 40.0)
                         | (dataframe['r_14_1h'] < -90.0)
-                        | (dataframe['tpct_change_144'] < 0.16)
+                        | (dataframe['tpct_change_144'] < 0.12)
                         |
                         (
                             (dataframe['close_max_48'] < (dataframe['close'] * 1.1))
                             & (dataframe['close'] < (dataframe['res3_1d'] * 1.1))
                             & (dataframe['ema_200_pct_change_144'] < 0.05)
+                            & (dataframe['rsi_14'] < 36.0)
                         )
                         | (dataframe['hl_pct_change_48_1h'] < 0.36)
                         |
                         (
                             (dataframe['sma_200'] > dataframe['sma_200'].shift(48))
                             & (dataframe['ema_200_pct_change_144'] < 0.05)
+                            & (dataframe['rsi_14'] < 36.0)
                         )
                         | (dataframe['close'] < dataframe['ema_20'] * 0.96)
                         | (dataframe['close'] < dataframe['bb20_2_low'] * 0.999)
