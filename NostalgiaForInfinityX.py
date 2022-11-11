5799ebc10dd9d517cd62578bb2c4a70d2f7c56e8
iterativ <vangelis.dimou@tutanota.com>
2022-11-12 01:18:13 +0200

    Buy 1: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.767"
+        return "v11.2.768"
 
 
     # ROI table:
@@ -10474,6 +10474,7 @@ class NostalgiaForInfinityX(IStrategy):
                         (
                             (dataframe['mfi'] > 30.0)
                             & (dataframe['hl_pct_change_36'] < 0.25)
+                            & (dataframe['hl_pct_change_48_1h'] < 0.5)
                         )
                         | (dataframe['rsi_14'] < 22.0)
                         | (dataframe['cti'] < -0.93)
@@ -10499,7 +10500,7 @@ class NostalgiaForInfinityX(IStrategy):
                         (
                             (dataframe['hl_pct_change_36'] < 0.2)
                             & (dataframe['btc_pct_close_max_72_5m'] < 1.03)
-                            & (dataframe['hl_pct_change_48_1h'] < 0.6)
+                            & (dataframe['hl_pct_change_48_1h'] < 0.5)
                             & (dataframe['close_max_48'] > (dataframe['close'] * 1.05))
                         )
                         |
@@ -10520,7 +10521,11 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['close'] > (dataframe['sma_200'] * 0.99))
                         | (dataframe['close'] < dataframe['sma_30'] * 0.86)
                         | (dataframe['close'] < dataframe['ema_20'] * 0.89)
-                        | (dataframe['close'] < dataframe['bb20_2_low'] * 0.988)
+                        |
+                        (
+                            (dataframe['close'] < dataframe['bb20_2_low'] * 0.988)
+                            & (dataframe['hl_pct_change_48_1h'] < 0.5)
+                        )
                         | ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.05))
                         |
                         (
