47d7c6c55f1188e7a734771e069b848b5c3bd39c
iterativ <vangelis.dimou@tutanota.com>
2022-11-17 15:30:14 +0200

    Buy 1: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -116,7 +116,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.840"
+        return "v11.2.841"
 
 
     # ROI table:
@@ -10488,7 +10488,10 @@ class NostalgiaForInfinityX(IStrategy):
                         | ((dataframe['ema_26_15m'] - dataframe['ema_12_15m']) > (dataframe['open_15m'] * 0.01))
                     )
                     item_buy_logic.append(
-                        (dataframe['cmf'] > 0.2)
+                        (
+                            (dataframe['cmf'] > 0.2)
+                            & (dataframe['hl_pct_change_36'] < 0.22)
+                        )
                         |
                         (
                             (dataframe['mfi'] > 30.0)
@@ -10502,6 +10505,7 @@ class NostalgiaForInfinityX(IStrategy):
                             (dataframe['cti_1h'] < -0.5)
                             & (dataframe['btc_pct_close_max_72_5m'] < 1.04)
                             & (dataframe['hl_pct_change_48_1h'] < 0.6)
+                            & (dataframe['hl_pct_change_36'] < 0.22)
                         )
                         | (dataframe['rsi_14_1h'] < 30.0)
                         |
@@ -10514,6 +10518,7 @@ class NostalgiaForInfinityX(IStrategy):
                             (dataframe['close_max_48'] < (dataframe['close'] * 1.1))
                             & (dataframe['hl_pct_change_48_1h'] < 0.6)
                             & (dataframe['close_max_48'] > (dataframe['close'] * 1.05))
+                            & (dataframe['hl_pct_change_36'] < 0.22)
                         )
                         |
                         (
