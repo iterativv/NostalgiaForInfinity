7607145a1926ec882aff610ce1add988684a8381
iterativ <vangelis.dimou@tutanota.com>
2022-11-19 14:38:17 +0200

    Buy 68: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -116,7 +116,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.2.889"
+        return "v11.2.890"
 
 
     # ROI table:
@@ -20847,6 +20847,7 @@ class NostalgiaForInfinityX(IStrategy):
                             (dataframe['btc_not_downtrend_1h'] == True)
                             & (dataframe['cmf'] > -0.1)
                             & (dataframe['mfi'] > 10.0)
+                            & (dataframe['ema_200_pct_change_144'] < 0.22)
                         )
                         |
                         (
