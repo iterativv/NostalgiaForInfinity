0f96801b721a5460815bbeff408623a30316439e
iterativ <vangelis.dimou@tutanota.com>
2022-12-07 15:19:48 +0200

    Buy 45: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.3.77"
+        return "v11.3.78"
 
 
     # ROI table:
@@ -18015,9 +18015,23 @@ class NostalgiaForInfinityX(IStrategy):
                         | (dataframe['ewo_15m'] > 4.0)
                     )
                     item_buy_logic.append(
-                        (dataframe['cmf'] > -0.2)
-                        | (dataframe['mfi'] > 20.0)
-                        | (dataframe['crsi'] > 25.0)
+                        (
+                            (dataframe['cmf'] > -0.2)
+                            & (dataframe['tpct_change_144'] < 0.2)
+                            & (dataframe['not_downtrend_1h'])
+                        )
+                        |
+                        (
+                            (dataframe['mfi'] > 20.0)
+                            & (dataframe['tpct_change_144'] < 0.2)
+                            & (dataframe['not_downtrend_1h'])
+                        )
+                        |
+                        (
+                            (dataframe['crsi'] > 25.0)
+                            & (dataframe['tpct_change_144'] < 0.2)
+                            & (dataframe['not_downtrend_1h'])
+                        )
                         | (dataframe['cti_1h'] < -0.9)
                         | (dataframe['rsi_14_1h'] < 40.0)
                         | (dataframe['crsi_1h'] > 30.0)
@@ -18032,7 +18046,12 @@ class NostalgiaForInfinityX(IStrategy):
                             & (dataframe['cmf'] > -0.4)
                         )
                         | (dataframe['close'] < dataframe['ema_20'] * 0.95)
-                        | (dataframe['close'] < dataframe['bb20_2_low'] * 0.999)
+                        |
+                        (
+                            (dataframe['close'] < dataframe['bb20_2_low'] * 0.999)
+                            & (dataframe['tpct_change_144'] < 0.2)
+                            & (dataframe['not_downtrend_1h'])
+                        )
                         |
                         (
                             ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.02))
