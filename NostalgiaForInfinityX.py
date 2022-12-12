40299b5064161aa00a3ff2cd1787180371ab7455
iterativ <vangelis.dimou@tutanota.com>
2022-12-12 13:44:51 +0200

    Buy 74: rework.

--- NostalgiaForInfinityX.py
+++ NostalgiaForInfinityX.py
@@ -117,7 +117,7 @@ class NostalgiaForInfinityX(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v11.3.84"
+        return "v11.3.85"
 
 
     # ROI table:
@@ -22416,6 +22416,7 @@ class NostalgiaForInfinityX(IStrategy):
                             (dataframe['rsi_14'] < 20.0)
                             & (dataframe['crsi_1h'] > 6.0)
                             & (dataframe['hl_pct_change_36'] < 0.16)
+                            & (dataframe['ema_200_pct_change_288'] < 0.12)
                         )
                         | (dataframe['crsi'] > 20.0)
                         |
@@ -22442,14 +22443,20 @@ class NostalgiaForInfinityX(IStrategy):
                         (
                             (dataframe['tpct_change_144'] < 0.1)
                             & (dataframe['sma_200_dec_20_1h'] == False)
+                            & (dataframe['ema_200_pct_change_288'] < 0.12)
                         )
                         |
                         (
                             (dataframe['close_max_48'] < (dataframe['close'] * 1.1))
                             & (dataframe['tpct_change_144'] < 0.3)
                             & (dataframe['sma_200_dec_20_1h'] == False)
+                            & (dataframe['ema_200_pct_change_288'] < 0.12)
                         )
-                        | (dataframe['hl_pct_change_36'] < 0.1)
+                        |
+                        (
+                            (dataframe['hl_pct_change_36'] < 0.1)
+                            & (dataframe['ema_200_pct_change_288'] < 0.12)
+                        )
                         | (dataframe['hl_pct_change_48_1h'] < 0.16)
                         |
                         (
@@ -22475,7 +22482,7 @@ class NostalgiaForInfinityX(IStrategy):
                         |
                         (
                             (dataframe['close'] < dataframe['bb20_2_low'] * 0.99)
-                            & (dataframe['ema_200_pct_change_288'] < 0.16)
+                            & (dataframe['ema_200_pct_change_288'] < 0.12)
                             & (dataframe['ema_200_pct_change_288'] > -0.2)
                         )
                         |
