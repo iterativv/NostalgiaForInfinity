632afa0988fe923a7f2cdd75141b794efa45c810
iterativ <vangelis.dimou@tutanota.com>
2022-12-12 21:13:38 +0200

    X2: exit_pump_bull: rework.

--- experimental/NostalgiaForInfinityX2.py
+++ experimental/NostalgiaForInfinityX2.py
@@ -911,7 +911,7 @@ class NostalgiaForInfinityX2(IStrategy):
                                                                             previous_time_profit_reached, enter_tags)
             if sell_max and signal_name_max is not None:
                 return True, f"{signal_name_max}_m"
-            if (current_profit > (previous_profit + 0.03)):
+            if (current_profit > (previous_profit + 0.03)) and (previous_sell_reason not in ["exit_pump_bull_stoploss_doom"]):
                 # Update the target, raise it.
                 mark_pair, mark_signal = self.pump_bull_mark_profit_target(pair, True, previous_sell_reason, trade, current_time, current_rate, current_profit, last_candle, previous_candle_1)
                 if mark_pair:
