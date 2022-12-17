1137299867732ffe24385476c9c451b24d7cded1
iterativ <vangelis.dimou@tutanota.com>
2022-12-17 15:53:20 +0200

    X2: buy 4: rework.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -3159,6 +3159,10 @@ class NostalgiaForInfinityX2(IStrategy):
                                           | (dataframe['top_wick_pct_4h'] < 0.04)
                                           | (dataframe['cti_20_4h'] < 0.8)
                                           | (dataframe['pct_change_high_max_3_12_4h'] > -0.05))
+                    # current 4h red
+                    item_buy_logic.append((dataframe['change_pct_4h'] > -0.04)
+                                          | (dataframe['rsi_14_max_6_4h'] < 80.0)
+                                          | (dataframe['pct_change_high_max_1_12_4h'] > -0.05))
 
                     # Logic
                     item_buy_logic.append(dataframe['ema_26'] > dataframe['ema_12'])
