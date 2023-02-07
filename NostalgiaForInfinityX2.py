dff42e9f14fc55a25a06654b908d185f6f95e6f4
iterativ <vangelis.dimou@tutanota.com>
2023-02-07 12:22:39 +0200

    X2: pump_bull_exit_profit_target: rework.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -64,7 +64,7 @@ class NostalgiaForInfinityX2(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v12.0.127"
+        return "v12.0.128"
 
     # ROI table:
     minimal_roi = {
@@ -1077,7 +1077,7 @@ class NostalgiaForInfinityX2(IStrategy):
                 # profit is over the threshold, don't exit
                 self._remove_profit_target(pair)
                 return False, None
-            if (current_profit < (previous_profit - 0.08)):
+            if (current_profit < (previous_profit - 0.1)):
                     return True, previous_sell_reason
         elif (previous_sell_reason in ["exit_profit_pump_bull_max"]):
             if (current_profit < -0.08):
