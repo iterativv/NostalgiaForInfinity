55a2955f0e4cb1d7449839c74545ac4b3d4274b2
iterativ <vangelis.dimou@tutanota.com>
2023-02-22 16:21:13 +0200

    X2: add exception for Kraken startup candle data number.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -64,7 +64,7 @@ class NostalgiaForInfinityX2(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v12.0.154"
+        return "v12.0.155"
 
     # ROI table:
     minimal_roi = {
@@ -219,8 +219,8 @@ class NostalgiaForInfinityX2(IStrategy):
                 self.config["user_data_dir"] / ("nfix2-profit_max-" + bot_name  + self.config["exchange"]["name"] + "-" + self.config["stake_currency"] +  ("-(backtest)" if (self.config['runmode'].value == 'backtest') else "") + ".json")
             )
 
-        # OKX provides a lower number of candle data per API call
-        if self.config["exchange"]["name"] in ["okx"]:
+            # OKX, Kraken provides a lower number of candle data per API call
+        if self.config["exchange"]["name"] in ["okx", "kraken"]:
             self.startup_candle_count = 480
 
         # If the cached data hasn't changed, it's a no-op
