e09a6a53d49266d6c869ee015c3aefd67dd1d059
iterativ <vangelis.dimou@tutanota.com>
2023-03-04 16:40:37 +0200

    X2: add global protections rule.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -64,7 +64,7 @@ class NostalgiaForInfinityX2(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v12.0.226"
+        return "v12.0.227"
 
     # ROI table:
     minimal_roi = {
@@ -6083,6 +6083,16 @@ class NostalgiaForInfinityX2(IStrategy):
                 | (dataframe['cti_20_1h'] < -0.5)
                 | (dataframe['cti_20_1d'] < -0.0)
                 | (dataframe['ema_200_1h'] > dataframe['ema_200_1h'].shift(576))
+                | (dataframe['ema_200_1d'] > dataframe['ema_200_1d'].shift(1152))
+            )
+            &
+            (
+                (dataframe['is_downtrend_3_1h'] == False)
+                | (dataframe['cti_20_15m'] < -0.9)
+                | (dataframe['rsi_3_15m'] > 25.0)
+                | (dataframe['rsi_14_15m'] < 30.0)
+                | (dataframe['cti_20_1h'] < -0.0)
+                | (dataframe['cti_20_4h'] < 0.75)
                 | (dataframe['ema_200_1d'] > dataframe['ema_200_1d'].shift(1152))
             )
         )
