37dcdc995e47b2ab0cba8111d03fdc69c4940c91
iterativ <vangelis.dimou@tutanota.com>
2023-03-19 09:56:29 +0200

    X2: grinding: fine tune the buy trigger.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -64,7 +64,7 @@ class NostalgiaForInfinityX2(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v12.0.301"
+        return "v12.0.302"
 
     # ROI table:
     minimal_roi = {
@@ -1278,13 +1278,11 @@ class NostalgiaForInfinityX2(IStrategy):
                                 or (slice_profit_entry < -0.01)
                             )
                             and (
-                                (last_candle['rsi_14'] < 50.0)
+                                (last_candle['rsi_14'] < 36.0)
                                 and (last_candle['close_max_12'] < (last_candle['close'] * 1.1))
                                 and (last_candle['close_max_24'] < (last_candle['close'] * 1.12))
                                 and (last_candle['close_max_48'] < (last_candle['close'] * 1.16))
-                                and (last_candle['ema_26'] > last_candle['ema_12'])
-                                and ((last_candle['ema_26'] - last_candle['ema_12']) > (last_candle['open'] * 0.005))
-                                and ((previous_candle['ema_26'] - previous_candle['ema_12']) > (last_candle['open'] / 100))
+                                and (last_candle['ha_close'] > last_candle['ha_open'])
                                 and (last_candle['rsi_3_1h'] > 10.0)
                                 and (last_candle['btc_pct_close_max_72_5m'] < 0.04)
                                 and (last_candle['btc_pct_close_max_24_5m'] < 0.03)
