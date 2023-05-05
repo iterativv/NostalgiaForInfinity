60622658b5671499d7cdc180fae417470ca542cc
iterativ <vangelis.dimou@tutanota.com>
2023-05-05 12:11:22 +0300

    X2: buy 42: rework.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX2(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v12.0.496"
+        return "v12.0.497"
 
     # ROI table:
     minimal_roi = {
@@ -8017,6 +8017,13 @@ class NostalgiaForInfinityX2(IStrategy):
                     item_buy_logic.append((dataframe['rsi_3'] > 20.0)
                                           | (dataframe['rsi_3_15m'] > 20.0)
                                           | (dataframe['cti_20_1d'] < 0.8))
+                    item_buy_logic.append((dataframe['cti_20_15m'] < -0.9)
+                                          | (dataframe['rsi_3_15m'] > 8.0)
+                                          | (dataframe['cti_20_4h'] < -0.0)
+                                          | (dataframe['ema_200_1h'] > dataframe['ema_200_1h'].shift(576))
+                                          | (dataframe['ema_200_4h'] > dataframe['ema_200_4h'].shift(1152))
+                                          | (dataframe['ema_200_1d'] > dataframe['ema_200_1d'].shift(1152))
+                                          | ((dataframe['ema_26'] - dataframe['ema_12']) > (dataframe['open'] * 0.03)))
 
                     # Logic
                     item_buy_logic.append(dataframe['ema_26'] > dataframe['ema_12'])
