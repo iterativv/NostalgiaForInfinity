7b129897c475a06564fd9c6af3feff46213e559f
iterativ <vangelis.dimou@tutanota.com>
2023-05-08 19:20:00 +0300

    X2: buy 43: rework.

--- NostalgiaForInfinityX2.py
+++ NostalgiaForInfinityX2.py
@@ -8462,6 +8462,11 @@ class NostalgiaForInfinityX2(IStrategy):
                     item_buy_logic.append((dataframe['rsi_3'] > 20.0)
                                           | (dataframe['rsi_3_15m'] > 20.0)
                                           | (dataframe['cti_20_1d'] < 0.8))
+                    item_buy_logic.append((dataframe['rsi_3'] > 10.0)
+                                          | (dataframe['rsi_3_15m'] > 16.0)
+                                          | (dataframe['ema_200_1h'] > dataframe['ema_200_1h'].shift(576))
+                                          | (dataframe['ema_200_4h'] > dataframe['ema_200_4h'].shift(1152))
+                                          | (dataframe['ema_200_1d'] > dataframe['ema_200_1d'].shift(1152)))
 
                     # Logic
                     item_buy_logic.append(dataframe['close'] < (dataframe['ema_26'] * 0.938))
