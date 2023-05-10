c17125dc4e081cf394abc3c2c291685e114a40ed
iterativ <vangelis.dimou@tutanota.com>
2023-05-10 18:17:50 +0300

    X3: buy 2: rework.

--- experimental/NostalgiaForInfinityX3.py
+++ experimental/NostalgiaForInfinityX3.py
@@ -2964,6 +2964,9 @@ class NostalgiaForInfinityX3(IStrategy):
                                           | (dataframe['ema_200_1h'] > dataframe['ema_200_1h'].shift(576))
                                           | (dataframe['ema_200_4h'] > dataframe['ema_200_4h'].shift(1152))
                                           | (dataframe['ema_200_1d'] > dataframe['ema_200_1d'].shift(1152)))
+                    item_buy_logic.append((dataframe['cti_20_4h'] < 0.8)
+                                          | (dataframe['ema_200_4h'] > dataframe['ema_200_4h'].shift(1152))
+                                          | (dataframe['ema_200_1d'] > dataframe['ema_200_1d'].shift(1152)))
 
                     # Logic
                     item_buy_logic.append(dataframe['bb40_2_delta'].gt(dataframe['close'] * 0.06))
