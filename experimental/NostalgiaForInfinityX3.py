8826fea4eb9c4ba2903970a4e307a6d0e0d69b47
iterativ <vangelis.dimou@tutanota.com>
2023-05-11 15:28:46 +0300

    X3: fine tune the grinding buy settings.

--- experimental/NostalgiaForInfinityX3.py
+++ experimental/NostalgiaForInfinityX3.py
@@ -144,11 +144,11 @@ class NostalgiaForInfinityX3(IStrategy):
     stake_grinding_mode_multiplier_alt_1 = 0.75
     stake_grinding_mode_multiplier_alt_2 = 1.0
     # Grinding stakes
-    grinding_stakes = [0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
+    grinding_stakes = [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
     grinding_stakes_alt_1 = [0.5, 0.5]
     grinding_stakes_alt_2 = [0.75]
     # Current total profit
-    grinding_thresholds = [-0.04, -0.08, -0.1, -0.12, -0.14, -0.16]
+    grinding_thresholds = [-0.03, -0.05, -0.08, -0.1, -0.14, -0.16, -0.18, -0.2]
     grinding_thresholds_alt_1 = [-0.06, -0.12]
     grinding_thresholds_alt_2 = [-0.06]
 
