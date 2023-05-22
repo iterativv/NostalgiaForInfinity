cbaccb280a112c4085971acd3bb8c0543b4cc669
iterativ <vangelis.dimou@tutanota.com>
2023-05-22 19:22:52 +0300

    X3: grinding: new buy condition.

--- experimental/NostalgiaForInfinityX3.py
+++ experimental/NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.48"
+        return "v13.0.49"
 
     # ROI table:
     minimal_roi = {
@@ -1367,6 +1367,17 @@ class NostalgiaForInfinityX3(IStrategy):
                                     and (last_candle['cti_20_1h'] < 0.8)
                                     and (last_candle['rsi_3_1h'] > 10.0)
                                     and (last_candle['rsi_3_4h'] > 20.0)
+                                )
+                                or
+                                (
+                                    (last_candle['rsi_14'] < 50.0)
+                                    and (last_candle['rsi_3'] > 5.0)
+                                    and (last_candle['cti_20'] > -0.5)
+                                    and (previous_candle['cti_20'] < -0.5)
+                                    and (last_candle['rsi_3_15m'] > 10.0)
+                                    and (last_candle['cti_20_1h'] < 0.8)
+                                    and (last_candle['rsi_3_1h'] > 30.0)
+                                    and (last_candle['rsi_3_4h'] > 30.0)
                                 )
                             )
                     ):
