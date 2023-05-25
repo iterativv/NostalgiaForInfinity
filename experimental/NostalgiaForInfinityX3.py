a98a851ddd24598f38fee6080b5fe1e104d33e0e
iterativ <vangelis.dimou@tutanota.com>
2023-05-26 00:01:35 +0300

    X3: grinding: stops to -8%.

--- experimental/NostalgiaForInfinityX3.py
+++ experimental/NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.59"
+        return "v13.0.60"
 
     # ROI table:
     minimal_roi = {
@@ -146,8 +146,8 @@ class NostalgiaForInfinityX3(IStrategy):
     stake_grinding_mode_multiplier_alt_1 = 1.0
     stake_grinding_mode_multiplier_alt_2 = 1.0
     # Grinding stop thresholds
-    grinding_stop_init = -0.05
-    grinding_stop_grinds = -0.05
+    grinding_stop_init = -0.08
+    grinding_stop_grinds = -0.08
     # Grinding take profit threshold
     grinding_profit_threshold = 0.012
     # Grinding stakes
