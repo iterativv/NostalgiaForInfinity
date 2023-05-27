d83213ae44a07afd59f186e6f3bc9fa9acd067af
iterativ <vangelis.dimou@tutanota.com>
2023-05-27 12:46:15 +0300

    X3: buy 22: rework.

--- experimental/NostalgiaForInfinityX3.py
+++ experimental/NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.70"
+        return "v13.0.71"
 
     # ROI table:
     minimal_roi = {
@@ -7703,6 +7703,10 @@ class NostalgiaForInfinityX3(IStrategy):
                                           | (dataframe['ema_200_dec_4_1d'] == False))
                     item_buy_logic.append((dataframe['cti_20_4h'] < 0.5)
                                           | (dataframe['rsi_14_4h'] < 70.0)
+                                          | (dataframe['rsi_14_1d'] < 70.0))
+                    item_buy_logic.append((dataframe['change_pct_1d'] > -0.03)
+                                          | (dataframe['rsi_14_4h'] < 40.0)
+                                          | (dataframe['cti_20_1d'] < 0.5)
                                           | (dataframe['rsi_14_1d'] < 70.0))
 
                     # Logic
