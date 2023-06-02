0bebde54fdbf0dc4719a37ecb1bc17dc758d1956
iterativ <vangelis.dimou@tutanota.com>
2023-06-02 12:33:31 +0300

    X3: grinding: fine tune the sell conditions.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.103"
+        return "v13.0.104"
 
     # ROI table:
     minimal_roi = {
@@ -149,7 +149,7 @@ class NostalgiaForInfinityX3(IStrategy):
     grinding_stop_init = -0.08
     grinding_stop_grinds = -0.08
     # Grinding take profit threshold
-    grinding_profit_threshold = 0.012
+    grinding_profit_threshold = 0.024
     # Grinding stakes
     grinding_stakes = [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
     grinding_stakes_alt_1 = [0.5, 0.5, 0.5]
@@ -1483,10 +1483,6 @@ class NostalgiaForInfinityX3(IStrategy):
                         grind_profit = (exit_rate - buy_order.average) / buy_order.average
                         if (
                                 (grind_profit > self.grinding_profit_threshold)
-                                and
-                                (
-                                    (last_candle['f_bear_14'] == 1)
-                                )
                         ):
                             sell_amount = buy_order.filled * exit_rate
                             if ((current_stake_amount - sell_amount) < (min_stake * 1.5)):
