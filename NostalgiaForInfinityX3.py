bb6c3b363449f4b73cdad4c6eba25cbab8401a80
iterativ <vangelis.dimou@tutanota.com>
2023-07-16 23:27:06 +0300

    X3: grinding: fine tune the buy conditions.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.232"
+        return "v13.0.233"
 
     # ROI table:
     minimal_roi = {
@@ -1501,16 +1501,15 @@ class NostalgiaForInfinityX3(IStrategy):
                                 )
                                 or
                                 (
-                                    (last_candle['rsi_14'] < 60.0)
-                                    and (previous_candle['rsi_3'] > 16.0)
+                                    (last_candle['rsi_14'] < 40.0)
                                     and (last_candle['ewo_50_200'] > 2.4)
                                     and (last_candle['rsi_14'] > previous_candle['rsi_14'])
-                                    and (last_candle['close'] < (last_candle['ema_12'] * 0.988))
-                                    and (last_candle['rsi_3_15m'] > 16.0)
+                                    and (last_candle['close'] < (last_candle['ema_12'] * 0.998))
+                                    and (last_candle['rsi_3_15m'] > 6.0)
                                     and (last_candle['cti_20_1h'] < 0.8)
-                                    and (last_candle['rsi_3_1h'] > 20.0)
-                                    and (last_candle['rsi_3_4h'] > 20.0)
-                                    and (last_candle['rsi_14_4h'] < 70.0)
+                                    and (last_candle['rsi_3_1h'] > 16.0)
+                                    and (last_candle['rsi_3_4h'] > 16.0)
+                                    and (last_candle['rsi_14_4h'] < 66.0)
                                     and (last_candle['ema_200_dec_24'] == False)
                                     and (last_candle['ema_200_dec_24_15m'] == False)
                                     and (last_candle['ema_200_dec_48_1h'] == False)
