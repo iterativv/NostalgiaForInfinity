85cca6b1d0e6e3a65f8c7d7d72dc8c683e18b4b2
iterativ <vangelis.dimou@tutanota.com>
2023-07-17 20:51:10 +0300

    X3: grinding: fine tune the buy conditions.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.238"
+        return "v13.0.239"
 
     # ROI table:
     minimal_roi = {
@@ -1434,6 +1434,7 @@ class NostalgiaForInfinityX3(IStrategy):
                                     and (last_candle['close'] > (last_candle['sar'] * 1.000))
                                     and (previous_candle['close'] < previous_candle['sar'])
                                     and (last_candle['rsi_3_15m'] > 10.0)
+                                    and (last_candle['cti_20_1h'] < 0.85)
                                     and (last_candle['rsi_3_1h'] > 10.0)
                                     and (last_candle['rsi_3_4h'] > 10.0)
                                     and (last_candle['ema_200_dec_24'] == False)
