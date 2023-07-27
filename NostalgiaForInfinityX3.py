1093d80f0b127b6186efab5e881c3df87dcc9063
iterativ <vangelis.dimou@tutanota.com>
2023-07-27 17:30:48 +0300

    X3: add new buy global protection item.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.259"
+        return "v13.0.260"
 
     # ROI table:
     minimal_roi = {
@@ -2486,6 +2486,14 @@ class NostalgiaForInfinityX3(IStrategy):
                 | (dataframe['not_downtrend_4h'])
                 | (dataframe['rsi_3_4h'] > 16.0)
                 | (dataframe['cti_20_1d'] < 0.7)
+            )
+            # 4h red with top wick, 1h downtrend, 1d overbought
+            &
+            (
+                (dataframe['change_pct_4h'] > -0.04)
+                | (dataframe['top_wick_pct_4h'] < 0.04)
+                | (dataframe['is_downtrend_3_1h'] == False)
+                | (dataframe['rsi_14_1d'] < 70.0)
             )
         ]
 
