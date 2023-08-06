4e85edaad4291ea67f61d1206c58b501717ba2eb
Tolga Karataş <tolga.karatas@gmail.com>
2023-08-06 23:14:44 +0300

    Download only necessary pairs data modified:   .github/workflows/scripts/download-necessary-exchange-market-data-for-backtests.sh

--- .github/workflows/scripts/download-necessary-exchange-market-data-for-backtests.sh
+++ .github/workflows/scripts/download-necessary-exchange-market-data-for-backtests.sh
@@ -81,7 +81,16 @@ if [[ $data_necessary_market_type == futures ]]
     else
     EXCHANGE_MARKET_DIRECTORY=$data_necessary_exchange
 fi
-git -C $MAIN_DATA_DIRECTORY sparse-checkout add /$EXCHANGE_MARKET_DIRECTORY/*-$data_necessary_timeframe*.feather
+
+for pair in `freqtrade test-pairlist -c configs/pairlists-$data_necessary_market_type.json -c configs/pairlist-static-$data_necessary_exchange-$data_necessary_market_type-usdt.json -c configs/exampleconfig.json -1 --exchange $data_necessary_exchange -c configs/blacklist-$data_necessary_exchange.json|sed -e 's+/+_+g'`;
+do
+    echo $pair;
+
+git -C $MAIN_DATA_DIRECTORY sparse-checkout add /$EXCHANGE_MARKET_DIRECTORY/$pair*-$data_necessary_timeframe*.feather
+
+ done
+
+
 done
 done
 done
