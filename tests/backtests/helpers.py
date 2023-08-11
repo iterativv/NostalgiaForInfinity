0bf2ef72b6357e7c520ff77618e2ec28ebe49181
Tolga Karataş <tolga.karatas@gmail.com>
2023-08-11 17:19:32 +0300

    renamed configs configs/pairlist-static- -> configs/pairlist-backtest-static- 	modified:   .github/workflows/scripts/download-necessary-exchange-market-data-for-backtests.sh 	renamed:    configs/pairlist-static-binance-futures-usdt.json -> configs/pairlist-backtest-static-binance-futures-usdt.json 	renamed:    configs/pairlist-static-binance-spot-usdt.json -> configs/pairlist-backtest-static-binance-spot-usdt.json 	renamed:    configs/pairlist-static-kucoin-futures-usdt.json -> configs/pairlist-backtest-static-kucoin-futures-usdt.json 	renamed:    configs/pairlist-static-kucoin-spot-usdt.json -> configs/pairlist-backtest-static-kucoin-spot-usdt.json 	renamed:    configs/pairlist-static-okx-futures-usdt.json -> configs/pairlist-backtest-static-okx-futures-usdt.json 	modified:   docker-compose.yml 	modified:   tests/backtests/helpers.py

--- tests/backtests/helpers.py
+++ tests/backtests/helpers.py
@@ -76,7 +76,7 @@ class Backtest:
             )
 
         tmp_path = self.request.getfixturevalue("tmp_path")
-        exchange_config = f"configs/pairlist-static-{exchange}-{trading_mode}-usdt.json"
+        exchange_config = f"configs/pairlist-backtest-static-{exchange}-{trading_mode}-usdt.json"
         json_results_file = tmp_path / "backtest-results.json"
         cmdline = [
             "freqtrade",
