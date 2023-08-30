de9d6d54ea39470736f2a001cb6161e3329b1802
iterativ <vangelis.dimou@tutanota.com>
2023-08-30 03:39:57 +0300

    X3: allow BTC informative to be used for futures.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.460"
+        return "v13.0.461"
 
     # ROI table:
     minimal_roi = {
@@ -1930,7 +1930,10 @@ class NostalgiaForInfinityX3(IStrategy):
             informative_pairs.extend([(pair, info_timeframe) for pair in pairs])
 
         if self.config['stake_currency'] in ['USDT','BUSD','USDC','DAI','TUSD','PAX','USD','EUR','GBP']:
-            btc_info_pair = f"BTC/{self.config['stake_currency']}"
+            if ('trading_mode' in self.config) and (self.config['trading_mode'] in ['futures', 'margin']):
+                btc_info_pair = f"BTC/{self.config['stake_currency']}:{self.config['stake_currency']}"
+            else:
+                btc_info_pair = f"BTC/{self.config['stake_currency']}"
         else:
             if ('trading_mode' in self.config) and (self.config['trading_mode'] in ['futures', 'margin']):
                 btc_info_pair = "BTC/USDT:USDT"
