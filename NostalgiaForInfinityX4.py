b8f471ade0e91c084645617244b4fc90f015bfd0
iterativ <vangelis.dimou@tutanota.com>
2023-09-06 21:12:55 +0300

    X4: fine tune buy protection rules.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.116"
+        return "v14.0.117"
 
     # ROI table:
     minimal_roi = {
@@ -4136,7 +4136,6 @@ class NostalgiaForInfinityX4(IStrategy):
             &
             (
                 (dataframe['not_downtrend_4h'])
-                | (dataframe['rsi_3'] > 26.0)
                 | (dataframe['rsi_3_4h'] > 20.0)
                 | (dataframe['r_480_4h'] > -95.0)
                 | (dataframe['ema_200_dec_48_1h'] == False)
