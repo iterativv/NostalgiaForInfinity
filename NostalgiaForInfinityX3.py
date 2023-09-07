c543a1391bd8f249b48c6aa1d284822222a18a16
iterativ <vangelis.dimou@tutanota.com>
2023-09-07 13:02:31 +0300

    X3: fine tune buy protection rules.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.491"
+        return "v13.0.492"
 
     # ROI table:
     minimal_roi = {
@@ -4189,6 +4189,20 @@ class NostalgiaForInfinityX3(IStrategy):
                 | (dataframe['rsi_3_15m'] > 6.0)
                 | (dataframe['rsi_3_1h'] > 20.0)
                 | (dataframe['rsi_14_4h'] < 46.0)
+            )
+            # current 1d red with top wick, 4h downtrend, 5m & 15m & 1h & 4h downmove, 15m & 1h & 4h still high
+            &
+            (
+                (dataframe['change_pct_1d'] > -0.03)
+                | (dataframe['top_wick_pct_1d'] < 0.03)
+                | (dataframe['not_downtrend_4h'])
+                | (dataframe['rsi_3'] > 30.0)
+                | (dataframe['rsi_3_15m'] > 10.0)
+                | (dataframe['rsi_3_1h'] > 30.0)
+                | (dataframe['rsi_3_4h'] > 30.0)
+                | (dataframe['rsi_14_15m'] < 20.0)
+                | (dataframe['rsi_14_1h'] < 30.0)
+                | (dataframe['rsi_14_4h'] < 30.0)
             )
         )
 
