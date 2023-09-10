1db411723a5c3045154bee46b92753693d9c4e23
iterativ <vangelis.dimou@tutanota.com>
2023-09-10 21:34:35 +0300

    X3: grinding: fine tune the init stop.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -147,7 +147,7 @@ class NostalgiaForInfinityX3(IStrategy):
     stake_grinding_mode_multiplier_alt_1 = 1.0
     stake_grinding_mode_multiplier_alt_2 = 1.0
     # Grinding stop thresholds
-    grinding_stop_init = -0.16
+    grinding_stop_init = -0.18
     grinding_stop_grinds = -0.16
     # Grinding take profit threshold
     grinding_profit_threshold = 0.012
