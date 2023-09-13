44d2d48d8549876afeb7265f1e0c1a7d740d4504
iterativ <vangelis.dimou@tutanota.com>
2023-09-13 12:31:27 +0300

    X4: grinding: mode 1: fine tune.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.159"
+        return "v14.0.160"
 
     # ROI table:
     minimal_roi = {
@@ -163,7 +163,7 @@ class NostalgiaForInfinityX4(IStrategy):
     # Grinding mode 1
     grinding_mode_1_stop_grinds = -0.16
     grinding_mode_1_profit_threshold = 0.018
-    grinding_mode_1_thresholds = [-0.0, -0.05]
+    grinding_mode_1_thresholds = [-0.0, -0.06]
     grinding_mode_1_stakes = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
     grinding_mode_1_sub_thresholds = [-0.03, -0.035, -0.04, -0.045, -0.05, -0.055, -0.06, -0.065, -0.07, -0.075]
     grinding_mode_1_stakes_alt_1 = [0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15]
