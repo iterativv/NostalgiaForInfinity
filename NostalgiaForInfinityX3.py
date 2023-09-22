24ac3208ac734f5a9322b1516bb53115e85020d4
iterativ <vangelis.dimou@tutanota.com>
2023-09-22 22:40:56 +0300

    X3: grinding: mode 1: fine tune the stakes.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX3(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v13.0.605"
+        return "v13.0.606"
 
     # ROI table:
     minimal_roi = {
@@ -166,8 +166,8 @@ class NostalgiaForInfinityX3(IStrategy):
     grinding_mode_1_stop_grinds = -0.16
     grinding_mode_1_profit_threshold = 0.018
     grinding_mode_1_thresholds = [-0.0]
-    grinding_mode_1_stakes = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-    grinding_mode_1_sub_thresholds = [-0.0, -0.03, -0.035, -0.04, -0.045, -0.05, -0.055, -0.06, -0.065, -0.07]
+    grinding_mode_1_stakes = [0.2, 0.2, 0.2, 0.2, 0.2]
+    grinding_mode_1_sub_thresholds = [-0.0, -0.04, -0.045, -0.05, -0.055]
     grinding_mode_1_stakes_alt_1 = [0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15]
     grinding_mode_1_sub_thresholds_alt_1 = [-0.0, -0.03, -0.035, -0.04, -0.045, -0.05, -0.055]
     grinding_mode_1_stakes_alt_2 = [0.2, 0.2, 0.2, 0.2, 0.2]
