cb12a155055d07085b127b19f38e3123342ed1e1
iterativ <vangelis.dimou@tutanota.com>
2023-10-01 12:11:49 +0300

    X4: grinding: mode 1: fine tune the alt thresholds.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.276"
+        return "v14.0.277"
 
     # ROI table:
     minimal_roi = {
@@ -176,10 +176,10 @@ class NostalgiaForInfinityX4(IStrategy):
     grinding_mode_1_thresholds = [-0.0, -0.06]
     grinding_mode_1_stakes = [0.2, 0.2, 0.2, 0.2, 0.2]
     grinding_mode_1_sub_thresholds = [-0.06, -0.065, -0.07, -0.075, -0.08]
-    grinding_mode_1_stakes_alt_1 = [0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15]
-    grinding_mode_1_sub_thresholds_alt_1 = [-0.06, -0.065, -0.07, -0.085, -0.09, -0.095, -0.1]
-    grinding_mode_1_stakes_alt_2 = [0.2, 0.2, 0.2, 0.2, 0.2]
-    grinding_mode_1_sub_thresholds_alt_2 = [-0.06, -0.07, -0.09, -0.09, -0.1]
+    grinding_mode_1_stakes_alt_1 = [0.25, 0.25, 0.25, 0.25]
+    grinding_mode_1_sub_thresholds_alt_1 = [-0.06, -0.065, -0.07, -0.085]
+    grinding_mode_1_stakes_alt_2 = [0.3, 0.3, 0.3, 0.3]
+    grinding_mode_1_sub_thresholds_alt_2 = [-0.06, -0.07, -0.09, -0.1]
 
     stake_rebuy_mode_multiplier = 0.33
     pa_rebuy_mode_max = 2
