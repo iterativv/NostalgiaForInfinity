064805db4ec2f4b9096d6c11a6efa63e624dee1c
iterativ <vangelis.dimou@tutanota.com>
2023-10-17 17:32:27 +0300

    X4: futures: final stop at -7%.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.343"
+        return "v14.0.344"
 
     # ROI table:
     minimal_roi = {
@@ -146,6 +146,7 @@ class NostalgiaForInfinityX4(IStrategy):
     stop_thresholds = [-0.2, -0.2, -0.025, -0.025, 720, 720, 0.016, 0.016, 24.0, 24.0, False, False, True, True]
     # Based on the the first entry (regardless of rebuys)
     stop_threshold = 0.5
+    stop_threshold_futures = 0.07
 
     # Rebuy mode minimum number of free slots
     rebuy_mode_min_free_slots = 2
@@ -1242,6 +1243,14 @@ class NostalgiaForInfinityX4(IStrategy):
                 profit_stake < -(filled_entries[0].cost * self.stop_threshold)
                 # temporary
                 and (trade.open_date_utc.replace(tzinfo=None) >= datetime(2023, 6, 13) or is_backtest)
+        ):
+            return True, f'exit_{mode_name}_stoploss'
+
+        if (
+                self.is_futures_mode is True
+                and profit_stake < -(filled_entries[0].cost * self.stop_threshold_futures)
+                # temporary
+                and (trade.open_date_utc.replace(tzinfo=None) >= datetime(2023, 10, 17) or is_backtest)
         ):
             return True, f'exit_{mode_name}_stoploss'
 
