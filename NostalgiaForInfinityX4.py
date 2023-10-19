8da41ea0d51262f9a4ae8738fb6487ff9f08df17
iterativ <vangelis.dimou@tutanota.com>
2023-10-19 03:07:59 +0300

    X4: fine tune buy protection rules.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -65,7 +65,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.352"
+        return "v14.0.353"
 
     # ROI table:
     minimal_roi = {
@@ -6781,14 +6781,17 @@ class NostalgiaForInfinityX4(IStrategy):
                 | (dataframe['cti_20_1d'] < -0.0)
                 | (dataframe['rsi_14_1d'] < 50.0)
             )
-            # 1h & 1d downtrend, 5m & 15m downmove, 15m stil high, 4h low, 1h & 4h & 1d downtrend
+            # 1h & 1d downtrend, 5m & 15m downmove, 4h low, 1h & 4h & 1d downtrend
             &
             (
                 (dataframe['not_downtrend_1h'])
                 | (dataframe['not_downtrend_1d'])
-                | (dataframe['rsi_3'] > 10.0)
+                | (dataframe['rsi_3'] > 16.0)
+                | (dataframe['close'] > dataframe['bb20_2_low_15m'])
+                | (dataframe['close'] > dataframe['bb20_2_low_1h'])
+                | (dataframe['close'] > dataframe['sup_level_4h'])
+                | (dataframe['close'] > dataframe['sup_level_1d'])
                 | (dataframe['rsi_3_15m'] > 26.0)
-                | (dataframe['rsi_14_15m'] < 30.0)
                 | (dataframe['r_480_4h'] > -85.0)
                 | (dataframe['ema_200_dec_48_1h'] == False)
                 | (dataframe['ema_200_dec_24_4h'] == False)
