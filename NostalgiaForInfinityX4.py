6d57b83d6f64afccfab76729ea3a72b41d82bfe4
iterativ <vangelis.dimou@tutanota.com>
2023-11-08 12:43:44 +0200

    X4: add global buy protection rule.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -66,7 +66,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.417"
+        return "v14.0.418"
 
     # ROI table:
     minimal_roi = {
@@ -7571,6 +7571,19 @@ class NostalgiaForInfinityX4(IStrategy):
                 | (dataframe['cti_20_4h'] < 0.5)
                 | (dataframe['rsi_14_4h'] < 46.0)
                 | (dataframe['cti_20_1d'] < -0.5)
+                | (dataframe['rsi_14_1d'] < 40.0)
+            )
+            # 5m & 15m downmove, 15m & 1h & 4h & 1d still high
+            &
+            (
+                (dataframe['rsi_3'] > 16.0)
+                | (dataframe['rsi_3_15m'] > 20.0)
+                | (dataframe['cti_20_15m'] < -0.0)
+                | (dataframe['rsi_14_15m'] < 40.0)
+                | (dataframe['cti_20_1h'] < 0.5)
+                | (dataframe['rsi_14_1h'] < 46.0)
+                | (dataframe['rsi_14_4h'] < 46.0)
+                | (dataframe['cti_20_1d'] < 0.5)
                 | (dataframe['rsi_14_1d'] < 40.0)
             )
         )
