845e0bbe0e3d98feb457d698406cb5c27bda3be1
iterativ <vangelis.dimou@tutanota.com>
2023-11-09 19:07:42 +0200

    X4: fine tune stops for rapid mode.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -66,7 +66,7 @@ class NostalgiaForInfinityX4(IStrategy):
     INTERFACE_VERSION = 3
 
     def version(self) -> str:
-        return "v14.0.431"
+        return "v14.0.432"
 
     # ROI table:
     minimal_roi = {
@@ -7708,14 +7708,15 @@ class NostalgiaForInfinityX4(IStrategy):
                 | (dataframe['ema_200_dec_24'] == False)
                 | (dataframe['ema_200_dec_24_15m'] == False)
             )
-            # 15m downmove, 15m & 1h & 4h & 1d still high, drop in last 12 days
+            # current 4h top wick, 15m downmove, 15m & 1h & 4h & 1d still high, drop in last 12 days
             &
             (
-                (dataframe['rsi_3_15m'] > 50.0)
+                (dataframe['top_wick_pct_4h'] < 0.02)
+                | (dataframe['rsi_3_15m'] > 50.0)
                 | (dataframe['cti_20_15m'] < -0.0)
                 | (dataframe['rsi_14_15m'] < 40.0)
-                | (dataframe['cti_20_1h'] < 0.5)
-                | (dataframe['rsi_14_1h'] < 50.0)
+                | (dataframe['cti_20_1h'] < 0.7)
+                | (dataframe['rsi_14_1h'] < 60.0)
                 | (dataframe['cti_20_4h'] < -0.5)
                 | (dataframe['rsi_14_4h'] < 46.0)
                 | (dataframe['cti_20_1d'] < -0.5)
