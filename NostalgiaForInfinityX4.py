b886b6691e6875b107c9babe33089d548442f6e1
iterativ <vangelis.dimou@tutanota.com>
2023-11-19 15:15:58 +0200

    X4: formatting.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -2483,12 +2483,11 @@ class NostalgiaForInfinityX4(IStrategy):
       total_profit += exit_stake
     current_stake = trade.amount * exit_rate * (1 - trade.fee_close)
     total_profit += current_stake
-    total_profit_ratio = (total_profit / total_stake
-                          * (self.futures_mode_leverage if self.is_futures_mode else 1.0))
-    current_profit_ratio = (total_profit / current_stake
-                            * (self.futures_mode_leverage if self.is_futures_mode else 1.0))
-    init_profit_ratio = (total_profit / filled_entries[0].cost
-                         * (self.futures_mode_leverage if self.is_futures_mode else 1.0))
+    total_profit_ratio = total_profit / total_stake * (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+    current_profit_ratio = total_profit / current_stake * (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+    init_profit_ratio = (
+      total_profit / filled_entries[0].cost * (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+    )
     return total_profit, total_profit_ratio, current_profit_ratio, init_profit_ratio
 
   def custom_exit(
@@ -2947,8 +2946,10 @@ class NostalgiaForInfinityX4(IStrategy):
         for i in range(grinding_parts):
           if current_stake_amount < stake_amount_threshold:
             if (
-                (profit_current_stake_ratio < (grinding_thresholds[i]
-                                               * (self.futures_mode_leverage if self.is_futures_mode else 1.0)))
+              (
+                profit_current_stake_ratio
+                < (grinding_thresholds[i] * (self.futures_mode_leverage if self.is_futures_mode else 1.0))
+              )
               and (last_candle["protections_global"] == True)
               and (
                 (last_candle["close_max_12"] < (last_candle["close"] * 1.12))
@@ -3387,9 +3388,15 @@ class NostalgiaForInfinityX4(IStrategy):
         if (not partial_sell) and (sub_grind_count < max_sub_grinds):
           if (
             (
-              ((sub_grind_count == 0)
-               and (profit_init_ratio < (self.grinding_mode_1_thresholds[1]
-                                       * (self.futures_mode_leverage if self.is_futures_mode else 1.0))))
+              (
+                (sub_grind_count == 0)
+                and (
+                  profit_init_ratio
+                  < (
+                    self.grinding_mode_1_thresholds[1] * (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+                  )
+                )
+              )
               or (
                 (0 < sub_grind_count < max_sub_grinds)
                 and (slice_profit_entry < grinding_mode_1_sub_thresholds[sub_grind_count])
