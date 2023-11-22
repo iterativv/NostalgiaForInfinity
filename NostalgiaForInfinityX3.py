b4772bf67cf2ec16a324ad1be6b03f487685acaf
iterativ <vangelis.dimou@tutanota.com>
2023-11-22 14:53:48 +0200

    X3: add global buy protection rule.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.0.829"
+    return "v13.0.831"
 
   # ROI table:
   minimal_roi = {
@@ -9122,6 +9122,31 @@ class NostalgiaForInfinityX3(IStrategy):
         | (dataframe["ema_200_dec_48_1h"] == False)
         | (dataframe["ema_200_dec_24_4h"] == False)
         | (dataframe["ema_200_dec_4_1d"] == False)
+      )
+      # 1h & 1d downtrend, 15m downmove, 1d still high, 1h downtrend
+      & (
+        (dataframe["not_downtrend_1h"])
+        | (dataframe["is_downtrend_3_1d"] == False)
+        | (dataframe["rsi_3_15m"] > 16.0)
+        | (dataframe["cti_20_1d"] < -0.0)
+        | (dataframe["rsi_14_1d"] < 46.0)
+        | (dataframe["close"] > dataframe["bb20_2_low_15m"])
+        | (dataframe["close"] > dataframe["bb20_2_low_1h"])
+        | (dataframe["ema_200_dec_48_1h"] == False)
+      )
+      # current 4h green with top wick, current 1d top wick, 15m downmove, 15m & 1h & 4h & 1d still high
+      & (
+        (dataframe["change_pct_4h"] < 0.02)
+        | (dataframe["top_wick_pct_4h"] < 0.02)
+        | (dataframe["top_wick_pct_1d"] < 0.06)
+        | (dataframe["rsi_3_15m"] > 30.0)
+        | (dataframe["rsi_14_15m"] < 30.0)
+        | (dataframe["cti_20_1h"] < -0.7)
+        | (dataframe["rsi_14_1h"] < 36.0)
+        | (dataframe["cti_20_4h"] < 0.5)
+        | (dataframe["rsi_14_4h"] < 50.0)
+        | (dataframe["cti_20_1d"] < -0.5)
+        | (dataframe["rsi_14_1d"] < 40.0)
       )
     )
 
