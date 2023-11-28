2a6adbcba6e35cf25c11ff7d4d185150121695d5
iterativ <vangelis.dimou@tutanota.com>
2023-11-28 17:31:59 +0200

    X3: replace orders access with the safe versions.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.0.863"
+    return "v13.0.864"
 
   # ROI table:
   minimal_roi = {
@@ -2202,11 +2202,11 @@ class NostalgiaForInfinityX3(IStrategy):
     total_stake = 0.0
     total_profit = 0.0
     for entry in filled_entries:
-      entry_stake = entry.filled * entry.average * (1 + trade.fee_open)
+      entry_stake = entry.safe_filled * entry.safe_price * (1 + trade.fee_open)
       total_stake += entry_stake
       total_profit -= entry_stake
     for exit in filled_exits:
-      exit_stake = exit.filled * exit.average * (1 - trade.fee_close)
+      exit_stake = exit.safe_filled * exit.safe_price * (1 - trade.fee_close)
       total_profit += exit_stake
     current_stake = trade.amount * exit_rate * (1 - trade.fee_close)
     total_profit += current_stake
@@ -2600,10 +2600,10 @@ class NostalgiaForInfinityX3(IStrategy):
       )
 
       slice_amount = filled_entries[0].cost
-      slice_profit = (exit_rate - filled_orders[-1].average) / filled_orders[-1].average
-      slice_profit_entry = (exit_rate - filled_entries[-1].average) / filled_entries[-1].average
+      slice_profit = (exit_rate - filled_orders[-1].safe_price) / filled_orders[-1].safe_price
+      slice_profit_entry = (exit_rate - filled_entries[-1].safe_price) / filled_entries[-1].safe_price
       slice_profit_exit = (
-        ((exit_rate - filled_exits[-1].average) / filled_exits[-1].average) if count_of_exits > 0 else 0.0
+        ((exit_rate - filled_exits[-1].safe_price) / filled_exits[-1].safe_price) if count_of_exits > 0 else 0.0
       )
 
       current_stake_amount = trade.amount * current_rate
@@ -2633,10 +2633,10 @@ class NostalgiaForInfinityX3(IStrategy):
         (count_of_entries == 1)
         and (count_of_exits > 0)
         and (filled_orders[-1].ft_order_side == "sell")
-        and (filled_orders[-1].average < filled_orders[0].average)
+        and (filled_orders[-1].safe_price < filled_orders[0].safe_price)
       ):
         sell_amount = (
-          filled_orders[-1].remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+          filled_orders[-1].safe_remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
         )
         if sell_amount > min_stake:
           self.dp.send_msg(
@@ -3011,7 +3011,7 @@ class NostalgiaForInfinityX3(IStrategy):
           count_of_full_exits = 0
           for exit_order in filled_exits:
             if (
-              exit_order.remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+              exit_order.safe_remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
             ) < min_stake:
               count_of_full_exits += 1
           num_buys = 0
@@ -3021,19 +3021,19 @@ class NostalgiaForInfinityX3(IStrategy):
               num_buys += 1
             elif order.ft_order_side == "sell":
               if (
-                order.remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+                order.safe_remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
               ) < min_stake:
                 num_sells += 1
             # patial fills on exits
             if (num_buys == num_sells) and (order.ft_order_side == "sell"):
-              sell_amount = order.remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
-              grind_profit = (exit_rate - order.average) / order.average
+              sell_amount = order.safe_remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+              grind_profit = (exit_rate - order.safe_price) / order.safe_price
               if sell_amount > min_stake:
                 # Test if it's the last exit. Normal exit with partial fill
                 if (trade.stake_amount - sell_amount) > min_stake:
                   if grind_profit > 0.01:
                     self.dp.send_msg(
-                      f"Grinding exit (remaining) [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount} | Coin amount: {order.remaining} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
+                      f"Grinding exit (remaining) [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount} | Coin amount: {order.safe_remaining} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
                     )
                     return -sell_amount
                   else:
@@ -3046,16 +3046,16 @@ class NostalgiaForInfinityX3(IStrategy):
               and (order.ft_order_side == "buy")
             ):
               buy_order = order
-              grind_profit = (exit_rate - buy_order.average) / buy_order.average
+              grind_profit = (exit_rate - buy_order.safe_price) / buy_order.safe_price
               if grind_profit > self.grinding_profit_threshold:
                 sell_amount = (
-                  buy_order.filled * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+                  buy_order.safe_filled * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
                 )
                 if (current_stake_amount - sell_amount) < (min_stake * 1.5):
                   sell_amount = (trade.amount * exit_rate) - (min_stake * 1.5)
                 if sell_amount > min_stake:
                   self.dp.send_msg(
-                    f"Grinding exit [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount}| Coin amount: {buy_order.filled} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
+                    f"Grinding exit [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount}| Coin amount: {buy_order.safe_filled} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
                   )
                   return -sell_amount
               elif (
@@ -3067,13 +3067,13 @@ class NostalgiaForInfinityX3(IStrategy):
                 )
               ):
                 sell_amount = (
-                  buy_order.filled * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0) * 0.999
+                  buy_order.safe_filled * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0) * 0.999
                 )
                 if (current_stake_amount - sell_amount) < (min_stake * 1.5):
                   sell_amount = (trade.amount * exit_rate) - (min_stake * 1.5)
                 if sell_amount > min_stake:
                   self.dp.send_msg(
-                    f"Grinding stop exit [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount}| Coin amount: {buy_order.filled} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
+                    f"Grinding stop exit [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount}| Coin amount: {buy_order.safe_filled} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
                   )
                   return -sell_amount
               break
@@ -3117,11 +3117,11 @@ class NostalgiaForInfinityX3(IStrategy):
         for order in reversed(filled_orders):
           if (order.ft_order_side == "buy") and (order is not filled_orders[0]):
             sub_grind_count += 1
-            total_amount += order.filled
-            total_cost += order.filled * order.average
+            total_amount += order.safe_filled
+            total_cost += order.safe_filled * order.safe_price
           elif order.ft_order_side == "sell":
             if (
-              order.remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+              order.safe_remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
             ) > min_stake:
               partial_sell = True
             break
@@ -3288,13 +3288,13 @@ class NostalgiaForInfinityX3(IStrategy):
         # Sell remaining if partial fill on exit
         if partial_sell:
           order = filled_exits[-1]
-          sell_amount = order.remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
-          grind_profit = (exit_rate - order.average) / order.average
+          sell_amount = order.safe_remaining * exit_rate / (self.futures_mode_leverage if self.is_futures_mode else 1.0)
+          grind_profit = (exit_rate - order.safe_price) / order.safe_price
           if sell_amount > min_stake:
             # Test if it's the last exit. Normal exit with partial fill
             if (trade.stake_amount - sell_amount) > min_stake:
               self.dp.send_msg(
-                f"Grinding exit (remaining) [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount} | Coin amount: {order.remaining} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
+                f"Grinding exit (remaining) [{trade.pair}] | Rate: {exit_rate} | Stake amount: {sell_amount} | Coin amount: {order.safe_remaining} | Profit (stake): {profit_stake} | Profit: {(profit_ratio * 100.0):.2f}% | Grind profit: {(grind_profit * 100.0):.2f}%"
               )
               return -sell_amount
 
