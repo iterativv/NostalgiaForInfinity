60aed628b93fcc0774633c9e38cdbd7ab2816c7b
iterativ <vangelis.dimou@tutanota.com>
2023-12-12 09:47:20 +0200

    X4: grinding: mode 2: fine tune init stops for spot.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -197,7 +197,7 @@ class NostalgiaForInfinityX4(IStrategy):
   grinding_mode_1_sub_thresholds_alt_4 = [-0.06, -0.08, -0.11]
 
   # Grinding mode 2
-  grinding_mode_2_stop_init_grinds_spot = -0.16
+  grinding_mode_2_stop_init_grinds_spot = -0.18
   grinding_mode_2_stop_grinds_spot = -0.16
   grinding_mode_2_stop_init_grinds_futures = -0.50
   grinding_mode_2_stop_grinds_futures = -0.16
