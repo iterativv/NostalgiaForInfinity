c71a5808ee02529e52f08731d5cab87d8944e3ad
iterativ <vangelis.dimou@tutanota.com>
2023-12-21 17:52:13 +0200

    X3: grinding: mode 2: fine tune the stops for futures.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.0.1015"
+    return "v13.0.1016"
 
   # ROI table:
   minimal_roi = {
@@ -194,7 +194,7 @@ class NostalgiaForInfinityX3(IStrategy):
   # Grinding mode 2
   grinding_mode_2_stop_init_grinds_spot = -0.20
   grinding_mode_2_stop_grinds_spot = -0.16
-  grinding_mode_2_stop_init_grinds_futures = -0.50
+  grinding_mode_2_stop_init_grinds_futures = -0.90
   grinding_mode_2_stop_grinds_futures = -0.26
   grinding_mode_2_profit_threshold_spot = 0.018
   grinding_mode_2_profit_threshold_futures = 0.018
