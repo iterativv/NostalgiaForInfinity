6d70618fee468cc1d172d03d09f6173db24fbd5a
iterativ <vangelis.dimou@tutanota.com>
2023-12-23 05:40:52 +0200

    X3: signal 103: rework.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.0.1021"
+    return "v13.0.1022"
 
   # ROI table:
   minimal_roi = {
@@ -23478,7 +23478,7 @@ class NostalgiaForInfinityX3(IStrategy):
           # Logic
           item_buy_logic.append(dataframe["rsi_3"] < 46.0)
           item_buy_logic.append(dataframe["rsi_14"] > 30.0)
-          item_buy_logic.append(dataframe["close"] < (dataframe["sma_16"] * 0.974))
+          item_buy_logic.append(dataframe["close"] < (dataframe["sma_16"] * 0.972))
 
         # Condition #104 - Long mode rapid
         if index == 104:
