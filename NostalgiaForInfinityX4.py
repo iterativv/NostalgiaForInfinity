149be71b5e144eb9951560ed20456f8cdeec4985
iterativ <vangelis.dimou@tutanota.com>
2023-12-25 12:35:24 +0200

    X4: signal 104: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.0.687"
+    return "v14.0.688"
 
   # ROI table:
   minimal_roi = {
@@ -23924,7 +23924,7 @@ class NostalgiaForInfinityX4(IStrategy):
           item_buy_logic.append(dataframe["rsi_14"] > 30.0)
           item_buy_logic.append(dataframe["rsi_14"] < 40.0)
           item_buy_logic.append(dataframe["ha_close"] > dataframe["ha_open"])
-          item_buy_logic.append(dataframe["close"] < (dataframe["sma_16"] * 0.978))
+          item_buy_logic.append(dataframe["close"] < (dataframe["sma_16"] * 0.976))
 
         # Condition #105 - Long mode rapid
         if index == 105:
