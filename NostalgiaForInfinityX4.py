2eebd4260444ab97ef78a464bb9fbefe25339f09
iterativ <vangelis.dimou@tutanota.com>
2024-01-05 19:11:13 +0200

    X4: futures: disable de-risk.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.0.732"
+    return "v14.0.733"
 
   # ROI table:
   minimal_roi = {
@@ -200,7 +200,7 @@ class NostalgiaForInfinityX4(IStrategy):
   # Grinding mode 2
   grinding_mode_2_stop_init_grinds_spot = -0.20
   grinding_mode_2_stop_grinds_spot = -0.16
-  grinding_mode_2_stop_init_grinds_futures = -0.90
+  grinding_mode_2_stop_init_grinds_futures = -10.90
   grinding_mode_2_stop_grinds_futures = -0.26
   grinding_mode_2_profit_threshold_spot = 0.018
   grinding_mode_2_profit_threshold_futures = 0.018
