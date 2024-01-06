70ae10a95309be3b516cfd429964ea45ade7c786
iterativ <vangelis.dimou@tutanota.com>
2024-01-06 12:55:13 +0200

    X4: signal 102: disable.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -291,7 +291,7 @@ class NostalgiaForInfinityX4(IStrategy):
     # "buy_condition_81_enable": True,
     # "buy_condition_82_enable": True,
     "buy_condition_101_enable": True,
-    "buy_condition_102_enable": True,
+    "buy_condition_102_enable": False,
     "buy_condition_103_enable": True,
     "buy_condition_104_enable": True,
     "buy_condition_105_enable": True,
