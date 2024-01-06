70a944f515bd4dd6c05f299af2c82cc1484811cb
iterativ <vangelis.dimou@tutanota.com>
2024-01-06 19:47:01 +0200

    X3: signal 103: disable.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -287,7 +287,7 @@ class NostalgiaForInfinityX3(IStrategy):
     # "buy_condition_82_enable": True,
     "buy_condition_101_enable": True,
     "buy_condition_102_enable": False,
-    "buy_condition_103_enable": True,
+    "buy_condition_103_enable": False,
     "buy_condition_104_enable": True,
     "buy_condition_105_enable": True,
   }
