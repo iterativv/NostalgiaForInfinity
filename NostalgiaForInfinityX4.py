7c5177c782eca22d1eb5b2a0e21f93f21d045850
iterativ <vangelis.dimou@tutanota.com>
2024-01-25 19:05:21 +0200

    X4: signal 103: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.50"
+    return "v14.1.51"
 
   # ROI table:
   minimal_roi = {
@@ -617,7 +617,7 @@ class NostalgiaForInfinityX4(IStrategy):
   entry_103_r_480_1h_max = DecimalParameter(-40.0, -0.0, default=-25.0, decimals=0, space="buy", optimize=False)
   entry_103_r_480_4h_min = DecimalParameter(-100.0, -70.0, default=-90.0, decimals=0, space="buy", optimize=False)
   entry_103_r_480_4h_max = DecimalParameter(-40.0, -0.0, default=-25.0, decimals=0, space="buy", optimize=False)
-  entry_103_rsi_14_min = DecimalParameter(20.0, 60.0, default=22.0, decimals=0, space="buy", optimize=False)
+  entry_103_rsi_14_min = DecimalParameter(20.0, 60.0, default=24.0, decimals=0, space="buy", optimize=False)
   entry_103_sma_offset = DecimalParameter(0.930, 0.972, default=0.960, decimals=3, space="buy", optimize=False)
   entry_103_bb_offset = DecimalParameter(0.940, 1.010, default=0.952, decimals=3, space="buy", optimize=False)
 
