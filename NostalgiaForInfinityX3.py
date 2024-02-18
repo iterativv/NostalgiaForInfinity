1bceb9aa977642ab0882bb48d89e0bebc1bb19b9
iterativ <vangelis.dimou@tutanota.com>
2024-02-18 10:20:13 +0200

    X3: exit_r: rework.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.178"
+    return "v13.1.179"
 
   stoploss = -0.99
 
@@ -3144,7 +3144,7 @@ class NostalgiaForInfinityX3(IStrategy):
     if 0.01 > current_profit >= 0.001:
       if (last_candle["r_480"] > -0.1) and (last_candle["r_14"] >= -1.0) and (last_candle["rsi_14"] > 75.0):
         return True, f"exit_{mode_name}_w_0_1"
-      elif (last_candle["r_14"] >= -1.0) and (last_candle["rsi_14"] > 82.0):
+      elif (last_candle["r_14"] >= -1.0) and (last_candle["rsi_14"] > 84.0):
         return True, f"exit_{mode_name}_w_0_2"
       elif (last_candle["r_14"] >= -1.0) and (last_candle["rsi_14"] < 40.0):
         return True, f"exit_{mode_name}_w_0_3"
@@ -3184,7 +3184,7 @@ class NostalgiaForInfinityX3(IStrategy):
         return True, f"exit_{mode_name}_w_0_8"
       elif (
         (last_candle["r_14"] >= -1.0)
-        and (last_candle["rsi_14"] >= 80.0)
+        and (last_candle["rsi_14"] >= 82.0)
         and (last_candle["rsi_14_15m"] >= 72.0)
         and (last_candle["cti_20_4h"] <= -0.50)
         and (last_candle["cti_20_1d"] >= 0.70)
