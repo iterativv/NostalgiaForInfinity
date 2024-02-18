a9ef7713f99c6f5e8d902f76452fd96322b570eb
iterativ <vangelis.dimou@tutanota.com>
2024-02-18 13:56:45 +0200

    X4: signal 46: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.182"
+    return "v14.1.183"
 
   stoploss = -0.99
 
@@ -27999,6 +27999,17 @@ class NostalgiaForInfinityX4(IStrategy):
             item_buy_logic.append(dataframe["not_downtrend_4h"])
           if self.entry_46_not_downtrend_1d_enabled.value:
             item_buy_logic.append(dataframe["not_downtrend_1d"])
+
+          item_buy_logic.append(
+            (dataframe["not_downtrend_1h"])
+            | (dataframe["r_480_1h"] > -70.0)
+            | (dataframe["r_480_4h"] > -80.0)
+            | (dataframe["close"] > dataframe["sup_level_1h"])
+            | (dataframe["close"] > dataframe["sup_level_4h"])
+            | (dataframe["close"] > dataframe["sup_level_1d"])
+            | (dataframe["ema_200_dec_48_1h"] == False)
+            | (dataframe["ema_200_dec_24_4h"] == False)
+          )
 
           # Logic
           item_buy_logic.append(dataframe["rsi_14"] < self.entry_46_rsi_14_max.value)
