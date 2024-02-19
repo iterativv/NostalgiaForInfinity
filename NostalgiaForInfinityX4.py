8057dbb2c4fb0b06d9598769eaf20602f1286d93
iterativ <vangelis.dimou@tutanota.com>
2024-02-19 21:56:03 +0200

    X4: add startup candle count exception for Bitget.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.191"
+    return "v14.1.192"
 
   stoploss = -0.99
 
@@ -1395,6 +1395,8 @@ class NostalgiaForInfinityX4(IStrategy):
       self.startup_candle_count = 710
     elif self.config["exchange"]["name"] in ["bybit"]:
       self.startup_candle_count = 199
+    elif self.config["exchange"]["name"] in ["bitget"]:
+      self.startup_candle_count = 499
 
     if ("trading_mode" in self.config) and (self.config["trading_mode"] in ["futures", "margin"]):
       self.is_futures_mode = True
