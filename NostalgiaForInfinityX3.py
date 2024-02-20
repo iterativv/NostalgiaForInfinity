aa3ec96b0115ac581390eb3765cd28fea9b753b6
iterativ <vangelis.dimou@tutanota.com>
2024-02-20 13:43:49 +0200

    X3: signal 11: enable.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.198"
+    return "v13.1.199"
 
   stoploss = -0.99
 
@@ -284,7 +284,7 @@ class NostalgiaForInfinityX3(IStrategy):
     "buy_condition_8_enable": True,
     "buy_condition_9_enable": True,
     "buy_condition_10_enable": True,
-    "buy_condition_11_enable": False,
+    "buy_condition_11_enable": True,
     "buy_condition_12_enable": False,
     "buy_condition_21_enable": True,
     "buy_condition_22_enable": True,
