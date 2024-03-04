dd76423475a58ecb77ee35f5476134dff8b6cd10
iterativ <vangelis.dimou@tutanota.com>
2024-03-04 12:27:54 +0200

    X3: calc total profit: if fees paid with the coin, then remove that from the current stake.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.280"
+    return "v13.1.281"
 
   stoploss = -0.99
 
@@ -8076,6 +8076,19 @@ class NostalgiaForInfinityX3(IStrategy):
       total_profit += exit_stake
       total_amount -= exit.safe_filled
     current_stake = total_amount * exit_rate * (1 - trade.fee_close)
+    # if fees paid with the coin
+    if (
+      trade.fee_open_currency is not None
+      and trade.fee_open_cost is not None
+      and trade.safe_base_currency == trade.fee_open_currency
+    ):
+      current_stake -= trade.fee_open_cost
+    if (
+      trade.fee_close_currency is not None
+      and trade.fee_close_cost is not None
+      and trade.safe_base_currency == trade.fee_close_currency
+    ):
+      current_stake -= trade.fee_close_cost
     if self.is_futures_mode:
       if trade.is_short:
         current_stake -= trade.funding_fees
