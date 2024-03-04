b86ea07adc4bc3f569fb0af61e4f198d945c36c1
iterativ <vangelis.dimou@tutanota.com>
2024-03-04 17:13:25 +0200

    X3: signal 13: rework.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.283"
+    return "v13.1.284"
 
   stoploss = -0.99
 
@@ -23498,6 +23498,12 @@ class NostalgiaForInfinityX3(IStrategy):
             | (dataframe["rsi_3"] > 16.0)
             | (dataframe["rsi_3_15m"] > 20.0)
             | (dataframe["rsi_3_1h"] > 26.0)
+          )
+          item_buy_logic.append(
+            (dataframe["rsi_3"] > 12.0)
+            | (dataframe["rsi_3_15m"] > 26.0)
+            | (dataframe["ema_200_dec_24_4h"] == False)
+            | (dataframe["hl_pct_change_6_1d"] < 0.9)
           )
 
           # Logic
