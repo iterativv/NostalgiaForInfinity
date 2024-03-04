c4958d24a42ca6b1631ea8d94f2f1a034e3a1d24
iterativ <vangelis.dimou@tutanota.com>
2024-03-04 18:07:26 +0200

    X4: signal 13: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.284"
+    return "v14.1.285"
 
   stoploss = -0.99
 
@@ -23828,6 +23828,12 @@ class NostalgiaForInfinityX4(IStrategy):
             | (dataframe["rsi_3_15m"] > 26.0)
             | (dataframe["ema_200_dec_24_4h"] == False)
             | (dataframe["hl_pct_change_6_1d"] < 0.9)
+          )
+          item_buy_logic.append(
+            (dataframe["not_downtrend_4h"])
+            | (dataframe["rsi_3_15m"] > 30.0)
+            | (dataframe["rsi_3_4h"] > 30.0)
+            | (dataframe["r_480_4h"] < -30.0)
           )
 
           # Logic
