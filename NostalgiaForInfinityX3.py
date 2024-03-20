a19b9f91e0b405c1d027de7ec423f08373cb20d3
iterativ <vangelis.dimou@tutanota.com>
2024-03-20 15:04:47 +0200

    X3: signal 13: rework.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.387"
+    return "v13.1.388"
 
   stoploss = -0.99
 
@@ -31471,6 +31471,15 @@ class NostalgiaForInfinityX3(IStrategy):
             | (df["rsi_14"] > df["rsi_14"].shift(12))
             | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
             | (df["close"] > (df["high_max_24_1h"] * 0.75))
+          )
+          long_entry_logic.append(
+            (df["not_downtrend_1h"])
+            | (df["not_downtrend_4h"])
+            | (df["rsi_14"] > df["rsi_14"].shift(12))
+            | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+            | (df["close"] > df["sup_level_1h"])
+            | (df["close"] > df["sup_level_4h"])
+            | (df["close"] > df["sup_level_1d"])
           )
 
           # Logic
