76e2ab7f2e261c87286042b2d1f1c4a166c10f01
iterativ <vangelis.dimou@tutanota.com>
2024-03-25 21:15:32 +0200

    Formatting.

--- tools/ho_to_raw_codemod_nfi4.py
+++ tools/ho_to_raw_codemod_nfi4.py
@@ -7,8 +7,9 @@ import os
 
 pattern = re.compile(
   r"(?:CategoricalParameter|DecimalParameter|IntParameter|RealParameter)\(.+?default=(?P<value>[^,\n]+) ([^\)]|\n)*?\)",
-  re.MULTILINE|re.DOTALL|re.IGNORECASE|re.VERBOSE
+  re.MULTILINE | re.DOTALL | re.IGNORECASE | re.VERBOSE,
 )
+
 
 def validate_syntax(src: str):
   ast.parse(src)  # Throw if syntax is not valid
@@ -17,6 +18,7 @@ def validate_syntax(src: str):
 def transform_code(src: str):
   def repl(matchobj):
     return matchobj.group(1)
+
   return pattern.subn(repl, src)
 
 
@@ -31,6 +33,7 @@ def replace_references(source: str, references_to_replace: list[str]):
   for reference_to_replace in references_to_replace:
     modified_source = modified_source.replace(f"{reference_to_replace}.value", reference_to_replace)
   return modified_source
+
 
 def replace_classname(source: str, old_name, new_name: str):
   """
@@ -47,8 +50,8 @@ def replace_classname(source: str, old_name, new_name: str):
   new_code = f"class {new_name}(IStrategy):"
   print(f"Replacing strategy class name '{old_name}' => '{new_name}' ...")
   return source.replace(old_code, new_code)
-    
-    
+
+
 def replace_all_references(strategy: IStrategy, source: str):
   params = strategy.detect_all_parameters()
   buy_params = [param[0] for param in params["buy"]]
@@ -56,7 +59,7 @@ def replace_all_references(strategy: IStrategy, source: str):
   if len(buy_params) > 0:
     print("Replacing references of 'buy' hyperopting params...")
     new_source = replace_references(source, buy_params)
-  if len(sell_params) > 0:  
+  if len(sell_params) > 0:
     print("Replacing references of 'sell' hyperopting params...")
     new_source = replace_references(new_source, sell_params)
 
@@ -67,14 +70,15 @@ def replace_all_references(strategy: IStrategy, source: str):
 
   return new_source
 
+
 # Abnormal termination
-def args_error(err_msg = None):
-    if err_msg != None:
-      print(f"ERROR: {err_msg}")
+def args_error(err_msg=None):
+  if err_msg != None:
+    print(f"ERROR: {err_msg}")
 
-    exit(1)
+  exit(1)
 
-    
+
 if __name__ == "__main__":
   import argparse
 
@@ -93,7 +97,7 @@ if __name__ == "__main__":
     metavar="OUTPUT_PATH",
     help="Output of transformed raw NFI file",
     type=str,
-    #default="NostalgiaForInfinityX4_Raw.py",
+    # default="NostalgiaForInfinityX4_Raw.py",
   )
   parser.add_argument(
     "--cname",
@@ -102,39 +106,39 @@ if __name__ == "__main__":
     metavar="OUTPUT_CLASSNAME",
     help="Optional new strategy class name in raw file, if set to no value, this class name is derived from the output file name",
     type=str,
-    const=":D:E:F:A:U:L:T:"
+    const=":D:E:F:A:U:L:T:",
   )
-  
-  args = parser.parse_args()
-  #print('ARGS=', args)
- 
-  if args.output == None:
-    args.output = args.strategy + '_Raw.py'
 
-  if os.path.splitext(args.output)[1] == '':
-    args.output += '.py' #python file
-  
+  args = parser.parse_args()
+  # print('ARGS=', args)
+
+  if args.output == None:
+    args.output = args.strategy + "_Raw.py"
+
+  if os.path.splitext(args.output)[1] == "":
+    args.output += ".py"  # python file
+
   config = Configuration.from_files([])
   config["strategy"] = args.strategy
   strategy = StrategyResolver.load_strategy(config)
-  
+
   source = ""
   with open(strategy.__file__, mode="r", encoding="utf-8") as f:
     source = f.read()
-    
+
   validate_syntax(source)
-  
+
   new_source = replace_all_references(strategy, source)
   print("Transforming all hyperopt values to raw...")
   transformed, t_count = transform_code(new_source)
   print(f"{t_count} hyperopt values transformed.")
-  
+
   if args.cname != None:
     if args.cname == ":D:E:F:A:U:L:T:" or len(args.cname) == 0:
-      #NostalgiaForInfinityX4_Raw.py ==> class NostalgiaForInfinityX4_Raw()(IStrategy):
+      # NostalgiaForInfinityX4_Raw.py ==> class NostalgiaForInfinityX4_Raw()(IStrategy):
       args.cname = os.path.splitext(args.output)[0]
     transformed = replace_classname(transformed, args.strategy, args.cname)
-  
+
   with open(args.output, mode="w", encoding="utf-8") as f:
     f.write(transformed)
   print(f"Path of the transformed file: {args.output}")
