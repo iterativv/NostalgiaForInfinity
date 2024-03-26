c34fd2022bccc8569484c9a8cc4e2fdc56b6bff9
iterativ <vangelis.dimou@tutanota.com>
2024-03-26 15:15:01 +0200

    X3: signal 108: enable.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.422"
+    return "v13.1.423"
 
   stoploss = -0.99
 
@@ -747,7 +747,7 @@ class NostalgiaForInfinityX3(IStrategy):
     "long_entry_condition_105_enable": True,
     "long_entry_condition_106_enable": True,
     "long_entry_condition_107_enable": True,
-    "long_entry_condition_108_enable": False,
+    "long_entry_condition_108_enable": True,
     "long_entry_condition_109_enable": False,
     "long_entry_condition_110_enable": False,
     "long_entry_condition_120_enable": True,
