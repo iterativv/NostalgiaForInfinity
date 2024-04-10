0bf88b578032b1169a4e5c56f5f6fefc968e277f
iterativ <vangelis.dimou@tutanota.com>
2024-04-10 21:45:33 +0300

    X4: signal 46: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.565"
+    return "v14.1.566"
 
   stoploss = -0.99
 
@@ -40409,6 +40409,14 @@ class NostalgiaForInfinityX4(IStrategy):
             | (df["rsi_14_1d"] < 50.0)
             | (df["r_480_4h"] > -65.0)
             | (df["close"] < df["res_hlevel_4h"])
+            | (df["ema_200_dec_4_1d"] == False)
+          )
+          long_entry_logic.append(
+            (df["not_downtrend_1h"])
+            | (df["rsi_14"] < 46.0)
+            | (df["rsi_14_15m"] < 46.0)
+            | (df["close"] > df["sup_level_1h"])
+            | (df["close"] > df["sup_level_4h"])
             | (df["ema_200_dec_4_1d"] == False)
           )
 
