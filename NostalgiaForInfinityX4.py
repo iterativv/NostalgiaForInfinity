c9953c71c24082966b8ab5c544d6c5c7a82f9ab8
iterativ <vangelis.dimou@tutanota.com>
2024-04-13 12:51:20 +0300

    X4: update indicators.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -20011,7 +20011,7 @@ class NostalgiaForInfinityX4(IStrategy):
 
     df.fillna({"zlma_50_1h": 0.0}, inplace=True)
     df.fillna({"cti_20_1d": 0.0}, inplace=True)
-    df.fillna({"r_480_4h": -50.0}, inplace=True)
+    df["r_480_4h"] = df["r_480_4h"].astype(np.float64).replace(to_replace=[np.nan, None], value=(-50.0))
 
     # Global protections
     df["protections_long_global"] = (
