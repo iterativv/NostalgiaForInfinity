90d55c0d395cc76350e08d9106982abac2dc2c4c
iterativ <vangelis.dimou@tutanota.com>
2024-04-28 17:39:24 +0300

    X3: add global buy protection rule.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.632"
+    return "v13.1.633"
 
   stoploss = -0.99
 
@@ -13394,6 +13394,17 @@ class NostalgiaForInfinityX3(IStrategy):
         | (df["not_downtrend_4h"])
         | (df["rsi_14_max_6_1h"] < 70.0)
         | (df["ema_200_dec_24_4h"] == False)
+      )
+      & (
+        (df["change_pct_1d"] > -0.08)
+        | (df["top_wick_pct_1d"] < 0.08)
+        | (df["rsi_14"] > df["rsi_14"].shift(12))
+        | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+        | (df["rsi_3_15m"] > 30.0)
+        | (df["rsi_14_15m"] < 46.0)
+        | (df["close"] > df["sup_level_4h"])
+        | (df["ema_200_dec_48_1h"] == False)
+        | (df["close"] > (df["high_max_12_1d"] * 0.70))
       )
     )
 
