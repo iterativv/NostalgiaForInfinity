d94e67e353e4e13f9faa428f7f33a8384f13ed61
iterativ <vangelis.dimou@tutanota.com>
2024-05-31 11:03:22 +0300

    X3: add global buy protection rule.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.786"
+    return "v13.1.787"
 
   stoploss = -0.99
 
@@ -12132,6 +12132,23 @@ class NostalgiaForInfinityX3(IStrategy):
         | (df["rsi_14_max_6_1d"] < 70.0)
         | (df["hl_pct_change_6_1d"] < 0.9)
         | (((df["close"] - df["low_min_6_1d"]) / df["low_min_6_1d"]) < (df["hl_pct_change_6_1d"] * 0.38))
+      )
+      & (
+        (df["change_pct_1h"] > -0.02)
+        | (df["top_wick_pct_1h"] < 0.02)
+        | (df["rsi_14"] > df["rsi_14"].shift(12))
+        | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+        | (df["rsi_3_15m"] > 30.0)
+        | (df["rsi_14_15m"] < 36.0)
+        | (df["rsi_14_1h"] < 40.0)
+        | (df["cti_20_4h"] < 0.8)
+        | (df["rsi_14_4h"] < 50.0)
+        | (df["rsi_14_max_6_4h"] < 70.0)
+        | (df["rsi_14_1d"] < 50.0)
+        | (df["r_480_1h"] < -35.0)
+        | (df["close"] < df["res_hlevel_1d"])
+        | (df["hl_pct_change_6_1d"] < 0.7)
+        | (((df["close"] - df["low_min_48_1h"]) / df["low_min_48_1h"]) < (df["hl_pct_change_48_1h"] * 0.38))
       )
     )
 
