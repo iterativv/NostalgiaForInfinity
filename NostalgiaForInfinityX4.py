5e9bc60ff3236bc0fccd049335a0074d81a8509d
iterativ <vangelis.dimou@tutanota.com>
2024-06-04 18:14:33 +0300

    X4: add global buy protection rule.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.807"
+    return "v14.1.808"
 
   stoploss = -0.99
 
@@ -12218,31 +12218,17 @@ class NostalgiaForInfinityX4(IStrategy):
         | (((df["close"] - df["low_min_48_1h"]) / df["low_min_48_1h"]) < (df["hl_pct_change_48_1h"] * 0.38))
       )
       & (
-        (df["change_pct_1d"] > -0.06)
+        (df["change_pct_1h"] > -0.02)
         | (df["rsi_14"] > df["rsi_14"].shift(12))
         | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
-        | (df["rsi_3"] > 26.0)
-        | (df["rsi_14_15m"] < 44.0)
-        | (df["rsi_14_1h"] < 46.0)
-        | (df["rsi_14_4h"] < 46.0)
-        | (df["rsi_14_1d"] < 65.0)
-        | (df["rsi_14_max_6_1d"] < 80.0)
-        | (df["close"] > (df["high_max_6_1d"] * 0.70))
-        | (df["hl_pct_change_6_1d"] < 1.2)
-      )
-      & (
-        (df["not_downtrend_4h"])
-        | (df["rsi_14"] > df["rsi_14"].shift(12))
-        | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
-        | (df["rsi_3_15m"] > 20.0)
-        | (df["rsi_3_4h"] > 20.0)
-        | (df["rsi_14_1d"] < 65.0)
-        | (df["rsi_14_max_6_1d"] < 80.0)
-        | (df["close"] > df["sup_level_1h"])
-        | (df["close"] > df["sup_level_4h"])
-        | (df["ema_200_dec_48_1h"] == False)
-        | (df["close"] > (df["high_max_6_1d"] * 0.70))
-        | (df["hl_pct_change_6_1d"] < 0.7)
+        | (df["rsi_3"] > 4.0)
+        | (df["rsi_3_15m"] > 12.0)
+        | (df["rsi_14_1h"] < 50.0)
+        | (df["rsi_14_max_6_1h"] < 70.0)
+        | (df["rsi_14_4h"] < 50.0)
+        | (df["close"] < df["res_hlevel_1h"])
+        | (df["close"] < df["res_hlevel_4h"])
+        | (((df["close"] - df["low_min_48_1h"]) / df["low_min_48_1h"]) < (df["hl_pct_change_48_1h"] * 0.38))
       )
     )
 
@@ -14437,6 +14423,33 @@ class NostalgiaForInfinityX4(IStrategy):
         | (df["close"] > df["sup_level_1h"])
         | (df["close"] > df["sup_level_4h"])
         | (df["ema_200_dec_48_1h"] == False)
+      )
+      & (
+        (df["change_pct_1d"] > -0.06)
+        | (df["rsi_14"] > df["rsi_14"].shift(12))
+        | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+        | (df["rsi_3"] > 26.0)
+        | (df["rsi_14_15m"] < 44.0)
+        | (df["rsi_14_1h"] < 46.0)
+        | (df["rsi_14_4h"] < 46.0)
+        | (df["rsi_14_1d"] < 65.0)
+        | (df["rsi_14_max_6_1d"] < 80.0)
+        | (df["close"] > (df["high_max_6_1d"] * 0.70))
+        | (df["hl_pct_change_6_1d"] < 1.2)
+      )
+      & (
+        (df["not_downtrend_4h"])
+        | (df["rsi_14"] > df["rsi_14"].shift(12))
+        | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+        | (df["rsi_3_15m"] > 20.0)
+        | (df["rsi_3_4h"] > 20.0)
+        | (df["rsi_14_1d"] < 65.0)
+        | (df["rsi_14_max_6_1d"] < 80.0)
+        | (df["close"] > df["sup_level_1h"])
+        | (df["close"] > df["sup_level_4h"])
+        | (df["ema_200_dec_48_1h"] == False)
+        | (df["close"] > (df["high_max_6_1d"] * 0.70))
+        | (df["hl_pct_change_6_1d"] < 0.7)
       )
     )
 
