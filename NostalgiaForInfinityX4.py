cf779d32600141c01ec241f3a9f34c22cb78c793
iterativ <vangelis.dimou@tutanota.com>
2024-06-17 16:56:12 +0300

    X4: signal 13: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.842"
+    return "v14.1.843"
 
   stoploss = -0.99
 
@@ -16122,6 +16122,21 @@ class NostalgiaForInfinityX4(IStrategy):
             | (df["rsi_3_15m"] > 30.0)
             | (df["rsi_3_1h"] > 20.0)
             | (df["close"] > df["sup_level_1h"])
+          )
+          long_entry_logic.append(
+            (df["change_pct_1h"] > -0.01)
+            | (df["not_downtrend_1h"])
+            | (df["is_downtrend_3_1h"] == False)
+            | (df["rsi_14"] > df["rsi_14"].shift(12))
+            | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+            | (df["rsi_3_15m"] > 12.0)
+            | (df["rsi_3_1h"] > 20.0)
+            | (df["close"] > df["sup_level_1h"])
+            | (df["close"] > df["sup_level_4h"])
+            | (df["close"] > df["sup_level_1d"])
+            | (df["sma_200_dec_48_1h"] == False)
+            | (df["ema_200_dec_24_4h"] == False)
+            | (df["ema_200_dec_4_1d"] == False)
           )
 
           # Logic
