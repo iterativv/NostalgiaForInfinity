204ffe0618fcfe5c96653698544da3dd817f21de
iterativ <vangelis.dimou@tutanota.com>
2024-06-21 10:24:12 +0300

    X4: signal 13: rework.

--- NostalgiaForInfinityX4.py
+++ NostalgiaForInfinityX4.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX4(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v14.1.857"
+    return "v14.1.858"
 
   stoploss = -0.99
 
@@ -16285,6 +16285,20 @@ class NostalgiaForInfinityX4(IStrategy):
             | (df["sma_200_dec_48_1h"] == False)
             | (df["ema_200_dec_24_4h"] == False)
             | (df["ema_200_dec_4_1d"] == False)
+          )
+          long_entry_logic.append(
+            (df["change_pct_4h"] > -0.01)
+            | (df["change_pct_1h"] > -0.01)
+            | (df["rsi_14_15m"] > df["rsi_14_15m"].shift(12))
+            | (df["rsi_3_15m"] > 36.0)
+            | (df["rsi_14_1h"] < 40.0)
+            | (df["rsi_14_4h"] < 50.0)
+            | (df["rsi_14_1d"] < 70.0)
+            | (df["r_480_1h"] < -35.0)
+            | (df["close"] > df["sup_level_1h"])
+            | (df["close"] < df["res_hlevel_1d"])
+            | (df["hl_pct_change_6_1d"] < 0.9)
+            | (((df["close"] - df["low_min_48_1h"]) / df["low_min_48_1h"]) < (df["hl_pct_change_48_1h"] * 0.38))
           )
 
           # Logic
