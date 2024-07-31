b6b6870fd7cf4260d17a6b95d7cc8285025c5555
iterativ <vangelis.dimou@tutanota.com>
2024-07-31 20:02:15 +0300

    X3: add global buy protection rule.

--- NostalgiaForInfinityX3.py
+++ NostalgiaForInfinityX3.py
@@ -68,7 +68,7 @@ class NostalgiaForInfinityX3(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v13.1.963"
+    return "v13.1.964"
 
   stoploss = -0.99
 
@@ -15955,6 +15955,21 @@ class NostalgiaForInfinityX3(IStrategy):
         | (df["rsi_14_4h"] > 70.0)
         | (df["close"] < df["res_hlevel_1h"])
         | (df["close"] < df["res_hlevel_4h"])
+        | (df["ema_200_dec_48_1h"] == True)
+        | (df["ema_200_dec_24_4h"] == True)
+      )
+      & (
+        (df["change_pct_1d"] < 0.06)
+        | (df["change_pct_1h"] < 0.01)
+        | (df["rsi_3"] < 80.0)
+        | (df["rsi_3_15m"] < 70.0)
+        | (df["rsi_14_15m"] > 75.0)
+        | (df["rsi_14_1h"] > 70.0)
+        | (df["r_480_1h"] < -15.0)
+        | (df["r_480_4h"] < -15.0)
+        | (df["close"] < df["res_hlevel_1h"])
+        | (df["close"] < df["res_hlevel_4h"])
+        | (df["close"] < df["res_hlevel_1d"])
         | (df["ema_200_dec_48_1h"] == True)
         | (df["ema_200_dec_24_4h"] == True)
       )
