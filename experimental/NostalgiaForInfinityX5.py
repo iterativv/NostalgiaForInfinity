8b3b637e26b521cff3e5f9f7c86cc61b6e6b3641
iterativ <vangelis.dimou@tutanota.com>
2024-09-11 12:36:27 +0300

    X5: signal 41: rework.

--- experimental/NostalgiaForInfinityX5.py
+++ experimental/NostalgiaForInfinityX5.py
@@ -66,7 +66,7 @@ class NostalgiaForInfinityX5(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v15.0.79"
+    return "v15.0.80"
 
   stoploss = -0.99
 
@@ -3594,6 +3594,10 @@ class NostalgiaForInfinityX5(IStrategy):
           # 15m down move, 1h strong down move, 1d overbought
           long_entry_logic.append(
             (df["MFI_14_15m"] > 20.0) | (df["RSI_3_change_pct_1h"] > -80.0) | (df["ROC_9_1d"] < 50.0)
+          )
+          # 1h not low enough, 4h high, 1d strong downtrend
+          long_entry_logic.append(
+            (df["STOCHRSIk_14_14_3_3_1h"] < 30.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0) | (df["ROC_9_1d"] > -60.0)
           )
           # 1h down move, 4h still high, 1d downtrend
           long_entry_logic.append(
