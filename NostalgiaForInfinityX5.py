aec90bf3f82cc971a9d22d17f4a4032d86ef629f
iterativ <vangelis.dimou@tutanota.com>
2024-10-14 13:32:23 +0300

    X5: signal 501: rework.

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -6171,6 +6171,10 @@ class NostalgiaForInfinityX5(IStrategy):
           short_entry_logic.append(
             (df["RSI_3_1h"] < 90.0) | (df["RSI_3_4h"] < 90.0) | (df["STOCHRSIk_14_14_3_3_1d"] > 70.0)
           )
+          # 1h up move, 1h still not high enough, 1d low
+          short_entry_logic.append(
+            (df["RSI_3_1h"] < 90.0) | (df["STOCHRSIk_14_14_3_3_1h"] > 70.0) | (df["STOCHRSIk_14_14_3_3_1d"] > 30.0)
+          )
           # 4h up move, 15m still low, 1h not high enough
           short_entry_logic.append(
             (df["RSI_3_4h"] < 95.0) | (df["STOCHRSIk_14_14_3_3_15m"] > 50.0) | (df["AROOND_14_1h"] < 25.0)
