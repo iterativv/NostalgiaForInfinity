f29b6a365d8a4b5e39410e0795addea495037ae0
iterativ <vangelis.dimou@tutanota.com>
2024-11-07 21:48:05 +0200

    X5: add global protection rule.

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -3213,6 +3213,18 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["AROONU_14_4h"] < 25.0)
         | (df["ROC_9_4h"] > -20.0)
       )
+      # 1d P&D, 15m down move, 15m still not low enough, 1h & 4h & 1d still high
+      & (
+        (df["change_pct_1d"] > -20.0)
+        | (df["change_pct_1d"].shift(288) < 20.0)
+        | (df["RSI_3_15m"] > 40.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 20.0)
+        | (df["AROONU_14_1h"] < 50.0)
+        | (df["STOCHRSIk_14_14_3_3_1h"] < 70.0)
+        | (df["AROONU_14_4h"] < 25.0)
+        | (df["AROONU_14_1d"] < 75.0)
+        | (df["STOCHRSIk_14_14_3_3_1d"] < 50.0)
+      )
       # 1d red, 15m & 1h & 4h down move, 1h & 4h still not low enough, 1d still high
       & (
         (df["change_pct_1d"] > -30.0)
@@ -3242,7 +3254,7 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1h"] < 40.0)
         | (df["AROONU_14_4h"] < 50.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 30.0)
-        | (df["AROONU_14_1d"] < 50.0)
+        | (df["AROONU_14_1d"] < 75.0)
         | (df["STOCHRSIk_14_14_3_3_1d"] < 50.0)
       )
     )
