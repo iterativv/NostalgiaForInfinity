094d252e64520089d5c5b58cafeaab6f50f9f3ce
iterativ <vangelis.dimou@tutanota.com>
2024-11-08 19:28:46 +0200

    X5: signal 6: add protection rule.

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -5580,6 +5580,15 @@ class NostalgiaForInfinityX5(IStrategy):
           long_entry_logic.append((df["RSI_3_15m"] > 15.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0))
           # 15m down move, 4h high
           long_entry_logic.append((df["RSI_3_15m"] > 15.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0))
+          # 15m down move, 15m & 1h still high, 4h still not low enough, 1d still high
+          long_entry_logic.append(
+            (df["RSI_3_15m"] > 15.0)
+            | (df["RSI_14_15m"] < 35.0)
+            | (df["AROONU_14_15m"] < 50.0)
+            | (df["STOCHRSIk_14_14_3_3_1h"] < 40.0)
+            | (df["STOCHRSIk_14_14_3_3_4h"] < 20.0)
+            | (df["AROONU_14_1d"] < 50.0)
+          )
           # 15m & 1h down move, 4h still high
           long_entry_logic.append(
             (df["RSI_3_15m"] > 20.0) | (df["RSI_3_1h"] > 15.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 40.0)
