7b4e9194941abcb9ef93d2b2080df0372827308e
iterativ <vangelis.dimou@tutanota.com>
2024-11-13 09:49:44 +0200

    X5: add global protection rule.

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -3467,14 +3467,28 @@ class NostalgiaForInfinityX5(IStrategy):
 
     df["global_protections_short_pump"] = (
       # 15m & 1h & 4h & 1d up move, 1h still not high enough, 1d still low, 4h & 1d uptrend
-      (df["RSI_3_15m"] < 80.0)
-      | (df["RSI_3_1h"] < 85.0)
-      | (df["RSI_3_4h"] < 90.0)
-      | (df["RSI_3_1d"] < 95.0)
-      | (df["STOCHRSIk_14_14_3_3_1h"] > 60.0)
-      | (df["AROOND_14_1d"] < 50.0)
-      | (df["ROC_9_4h"] < 100.0)
-      | (df["ROC_9_1d"] < 100.0)
+      (
+        (df["RSI_3_15m"] < 80.0)
+        | (df["RSI_3_1h"] < 85.0)
+        | (df["RSI_3_4h"] < 90.0)
+        | (df["RSI_3_1d"] < 95.0)
+        | (df["STOCHRSIk_14_14_3_3_1h"] > 60.0)
+        | (df["AROOND_14_1d"] < 50.0)
+        | (df["ROC_9_4h"] < 100.0)
+        | (df["ROC_9_1d"] < 100.0)
+      )
+      # 1h & 4h & 1d up move, 15m still not high enough, 1h & 4h & 1d still not high enough, 1d uptrend
+      & (
+        (df["RSI_3_1h"] < 80.0)
+        | (df["RSI_3_4h"] < 60.0)
+        | (df["RSI_3_1d"] < 90.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] > 80.0)
+        | (df["WILLR_14_1h"] > -20.0)
+        | (df["WILLR_14_4h"] > -25.0)
+        | (df["STOCHRSIk_14_14_3_3_4h"] > 20.0)
+        | (df["AROOND_14_1d"] < 50.0)
+        | (df["ROC_9_1d"] < 20.0)
+      )
     )
 
     df["global_protections_short_dump"] = (
