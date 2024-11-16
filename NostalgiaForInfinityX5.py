88cb0c26713ea501ebf179eb7c311f9ee54b5c21
iterativ <vangelis.dimou@tutanota.com>
2024-11-16 10:07:10 +0200

    X5: formatting.

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -21,8 +21,8 @@ log = logging.getLogger(__name__)
 warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 #############################################################################################################
-##                 NostalgiaForInfinityX5 by iterativ                                                       ##
-##            https://github.com/iterativv/NostalgiaForInfinity                                             ##
+##                 NostalgiaForInfinityX5 by iterativ                                                      ##
+##            https://github.com/iterativv/NostalgiaForInfinity                                            ##
 ##                                                                                                         ##
 ##    Strategy for Freqtrade https://github.com/freqtrade/freqtrade                                        ##
 ##                                                                                                         ##
@@ -33,16 +33,16 @@ warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 ##   A pairlist with 40 to 80 pairs. Volume pairlist works well.                                           ##
 ##   Prefer stable coin (USDT, BUSDT etc) pairs, instead of BTC or ETH pairs.                              ##
 ##   Highly recommended to blacklist leveraged tokens (*BULL, *BEAR, *UP, *DOWN etc).                      ##
-##   Ensure that you don't override any variables in you config.json. Especially                            ##
+##   Ensure that you don't override any variables in you config.json. Especially                           ##
 ##   the timeframe (must be 5m).                                                                           ##
 ##     use_exit_signal must set to true (or not set at all).                                               ##
-##     exit_profit_only must set to false (or not set at all).                                              ##
+##     exit_profit_only must set to false (or not set at all).                                             ##
 ##     ignore_roi_if_entry_signal must set to true (or not set at all).                                    ##
 ##                                                                                                         ##
 #############################################################################################################
 ##               DONATIONS                                                                                 ##
 ##                                                                                                         ##
-##   BTC: bc1qvflsvddkmxh7eqhc4jyu5z5k6xcw3ay8jl49sk                                                        ##
+##   BTC: bc1qvflsvddkmxh7eqhc4jyu5z5k6xcw3ay8jl49sk                                                       ##
 ##   ETH (ERC20): 0x83D3cFb8001BDC5d2211cBeBB8cB3461E5f7Ec91                                               ##
 ##   BEP20/BSC (USDT, ETH, BNB, ...): 0x86A0B21a20b39d16424B7c8003E4A7e12d78ABEe                           ##
 ##   TRC20/TRON (USDT, TRON, ...): TTAa9MX6zMLXNgWMhg7tkNormVHWCoq8Xk                                      ##
@@ -53,12 +53,12 @@ warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 ##  Kucoin: https://www.kucoin.com/r/af/QBSSS5J2 (20% lifetime discount on trading fees)                   ##
 ##  Gate.io: https://www.gate.io/referral/invite/UAARUlhf_2130_103 (20% lifetime discount on trading fees) ##
 ##  OKX: https://www.okx.com/join/11749725931 (20% discount on trading fees)                               ##
-##  MEXC: https://promote.mexc.com/a/nfi  (10% discount on trading fees)                                    ##
-##  ByBit: https://partner.bybit.com/b/nfi                                                                  ##
-##  Bitget: https://bonus.bitget.com/nfi (lifetime 20% rebate all & 10% discount on spot fees)              ##
+##  MEXC: https://promote.mexc.com/a/nfi  (10% discount on trading fees)                                   ##
+##  ByBit: https://partner.bybit.com/b/nfi                                                                 ##
+##  Bitget: https://bonus.bitget.com/nfi (lifetime 20% rebate all & 10% discount on spot fees)             ##
 ##  HTX: https://www.htx.com/invite/en-us/1f?invite_code=ubpt2223                                          ##
 ##         (Welcome Bonus worth 241 USDT upon completion of a deposit and trade)                           ##
-##  Bitvavo: https://account.bitvavo.com/create?a=D22103A4BC (no fees for the first € 1000)                 ##
+##  Bitvavo: https://account.bitvavo.com/create?a=D22103A4BC (no fees for the first € 1000)                ##
 #############################################################################################################
 
 
