f55c9d7d3931a3ea582a5235c85457670b90344b
iterativ <vangelis.dimou@tutanota.com>
2024-11-17 09:03:27 +0200

    X5: add global protection rule (Short).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -3773,30 +3773,18 @@ class NostalgiaForInfinityX5(IStrategy):
     )
 
     df["global_protections_short_dump"] = (
-      (
-        # 15m still not low enough, 4h & 1d down move, 1d downtrend
-        (df["STOCHRSIk_14_14_3_3_15m"] > 95.0)
-        | (df["AROOND_14_15m"] < 25.0)
-        | (df["RSI_3_4h"] < 90.0)
-        | (df["RSI_3_1d"] < 90.0)
-        | (df["ROC_9_1d"] < 30.0)
-      )
-      # 15m & 4h still not low enough, 1h & 4h & 1d down move
-      & (
-        (df["STOCHRSIk_14_14_3_3_15m"] > 90.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] > 70.0)
-        | (df["RSI_3_1h"] < 85.0)
-        | (df["RSI_3_4h"] < 70.0)
-        | (df["RSI_3_1d"] < 70.0)
-      )
-      # 1d green with top wick, 4h down move, 4h still high, 4h overbought
-      & (
-        (df["change_pct_1d"] > -20.0)
-        | (df["bot_wick_pct_1d"] < 20.0)
-        | (df["RSI_3_4h"] < 50.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] > 60.0)
-        | (df["ROC_9_4h"] > -50.0)
-      )
+      # 15m up move, 15m still low, 1h & 4h & 1d still not high
+      (df["RSI_3_15m"] < 85.0)
+      | (df["AROOND_14_15m"] < 50.0)
+      | (df["RSI_14_1h"] > 70.0)
+      | (df["WILLR_14_1h"] > -50.0)
+      | (df["STOCHRSIk_14_14_3_3_1h"] > 80.0)
+      | (df["AROOND_14_1h"] < 75.0)
+      | (df["RSI_14_4h"] > 70.0)
+      | (df["WILLR_14_4h"] > -50.0)
+      | (df["AROOND_14_4h"] < 25.0)
+      | (df["STOCHRSIk_14_14_3_3_4h"] > 30.0)
+      | (df["RSI_14_1d"] > 70.0)
     )
 
     df["protections_short_rebuy"] = True
