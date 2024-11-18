62032f6a3ec63a7b7ed0482f835c1d1608fe209b
iterativ <vangelis.dimou@tutanota.com>
2024-11-18 23:34:40 +0200

    X5: add global protection rule (Long).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -66,7 +66,7 @@ class NostalgiaForInfinityX5(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v15.1.227"
+    return "v15.1.228"
 
   stoploss = -0.99
 
@@ -3665,6 +3665,21 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1h"] < 10.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0)
         | (df["ROC_9_1d"] > -30.0)
+      )
+      # 15m & 1h & 4h down move, 15m still not low enough, 1h & 4h still not low enough & downtrend
+      & (
+        (df["RSI_3_15m"] > 40.0)
+        | (df["RSI_3_1h"] > 35.0)
+        | (df["RSI_3_4h"] > 35.0)
+        | (df["CMF_20_15m"] > -0.25)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 10.0)
+        | (df["RSI_14_1h"] < 30.0)
+        | (df["MFI_14_1h"] < 10.0)
+        | (df["ROC_9_1h"] > -10.0)
+        | (df["RSI_14_4h"] < 40.0)
+        | (df["MFI_14_4h"] < 20.0)
+        | (df["ROC_9_4h"] > -15.0)
       )
       # 15m & 1h & 4h & 1d down move, 15m high, 1h still not low enough, 1d still high
       & (
