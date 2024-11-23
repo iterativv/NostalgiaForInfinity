2b7b92964aabec704b95aa51fbb4f70b85cf0387
iterativ <vangelis.dimou@tutanota.com>
2024-11-23 12:25:28 +0200

    X5: add global protection rule (Long).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -3882,6 +3882,19 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1d"] < 10.0)
         | (df["ROC_9_1d"] > -30.0)
       )
+      # 15m & 1h & 4h down move, 15m & 1h & 4h still not low enough, 1d downtrend
+      & (
+        (df["RSI_3_15m"] > 15.0)
+        | (df["RSI_3_1h"] > 15.0)
+        | (df["RSI_3_4h"] > 35.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 30.0)
+        | (df["UO_7_14_28_1h"] < 40.0)
+        | (df["RSI_14_4h"] < 35.0)
+        | (df["WILLR_14_4h"] < -90.0)
+        | (df["CCI_20_4h"] < -100.0)
+        | (df["ROC_2_1d"] > -30.0)
+      )
       # 15m & 1h & 4h & 1d down move, 15m still high, 1h & 4h still not low enough, 4h & 1d downtrend
       & (
         (df["RSI_3_15m"] > 15.0)
