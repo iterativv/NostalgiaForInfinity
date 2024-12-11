36f30ee35d6fee4350ccf9af51acc313eafa1ec3
iterativ <vangelis.dimou@tutanota.com>
2024-12-11 11:57:55 +0200

    X5: fine tune global protection rule (Long).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -4496,12 +4496,11 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["UO_7_14_28_4h"] < 40.0)
         | (df["RSI_14_1d"] < 40.0)
       )
-      # 15m & 1h & 4h & 1d down move, 15m & 1h & 4h still not low enough, 1d still high
+      # 15m & 1h & 4h down move, 15m & 1h & 4h still not low enough, 1d still high
       & (
         (df["RSI_3_15m"] > 10.0)
         | (df["RSI_3_1h"] > 20.0)
         | (df["RSI_3_4h"] > 35.0)
-        | (df["RSI_3_1d"] > 60.0)
         | (df["RSI_14_15m"] < 15.0)
         | (df["CCI_20_15m"] < -250.0)
         | (df["RSI_14_1h"] < 30.0)
@@ -4510,7 +4509,6 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["WILLR_14_4h"] < -80.0)
         | (df["CCI_20_4h"] < -200.0)
         | (df["STOCHk_14_3_3_4h"] < 20.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] < 20.0)
         | (df["RSI_14_1d"] < 50.0)
         | (df["STOCHk_14_3_3_1d"] < 60.0)
       )
