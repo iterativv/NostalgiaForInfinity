37a35ca7eb7ac943c1bd48dc17d00cacb253cc2b
iterativ <vangelis.dimou@tutanota.com>
2025-01-06 16:22:12 +0200

    X5: remove global protection rule (Long).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX5(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v15.1.316"
+    return "v15.1.317"
 
   stoploss = -0.99
 
@@ -4044,17 +4044,6 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["RSI_14_1d"] < 50.0)
         | (df["AROONU_14_1d"] < 75.0)
       )
-      # 15m down move, 15m still not low enough, 1h & 4h high, 1d overbought
-      & (
-        (df["RSI_3_15m"] > 35.0)
-        | (df["AROONU_14_15m"] < 25.0)
-        | (df["STOCHRSIk_14_14_3_3_1h"] < 90.0)
-        | (df["AROONU_14_4h"] < 75.0)
-        | (df["WILLR_14_4h"] < -20.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] < 70.0)
-        | (df["RSI_14_1d"] < 75.0)
-        | (df["ROC_9_1d"] < 50.0)
-      )
       # 15m & 1 & 4h down move, 15m & 1h & 4h still not low enough, 1d still high
       & (
         (df["RSI_3_15m"] > 35.0)
@@ -4453,29 +4442,6 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1d"] < 90.0)
         | (df["ROC_9_1d"] < 30.0)
       )
-      # 15m & 1h down move, 15m & 1h still high, 4h high, 1d overbought
-      & (
-        (df["RSI_3_15m"] > 40.0)
-        | (df["RSI_3_1h"] > 40.0)
-        | (df["AROONU_14_15m"] < 50.0)
-        | (df["STOCHRSIk_14_14_3_3_15m"] < 20.0)
-        | (df["RSI_14_1h"] < 50.0)
-        | (df["AROONU_14_4h"] < 75.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0)
-        | (df["ROC_9_1d"] < 50.0)
-      )
-      # 15m & 4h down move, 15m & 1h stil high, 4h high
-      & (
-        (df["RSI_3_15m"] > 40.0)
-        | (df["RSI_3_4h"] > 60.0)
-        | (df["AROONU_14_15m"] < 50.0)
-        | (df["STOCHRSIk_14_14_3_3_15m"] < 50.0)
-        | (df["RSI_14_1h"] < 50.0)
-        | (df["AROONU_14_1h"] < 25.0)
-        | (df["RSI_14_4h"] < 50.0)
-        | (df["AROONU_14_4h"] < 75.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] < 70.0)
-      )
       # 15m down move, 15m still not low enough, 1h & 4h high, 4h overbought
       & (
         (df["RSI_3_15m"] > 40.0)
@@ -4515,16 +4481,6 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["RSI_14_4h"] < 30.0)
         | (df["ROC_9_4h"] > -15.0)
         | (df["ROC_9_1d"] < 50.0)
-      )
-      # 15m down move, 15m & 1h & 1d high
-      & (
-        (df["RSI_3_15m"] > 40.0)
-        | (df["AROONU_14_15m"] < 75.0)
-        | (df["WILLR_14_1h"] < -10.0)
-        | (df["AROONU_14_1h"] < 75.0)
-        | (df["STOCHRSIk_14_14_3_3_1h"] < 90.0)
-        | (df["AROONU_14_1d"] < 75.0)
-        | (df["STOCHRSIk_14_14_3_3_1d"] < 80.0)
       )
       # 15m & 1h down move, 15m & 1h still not low enough, 4h high & overbought
       & (
