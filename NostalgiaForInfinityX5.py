900c13a71b251c9fed00d7d1d226f585bf4ffb9e
iterativ <vangelis.dimou@tutanota.com>
2025-01-13 18:20:57 +0200

    X5: add global protection rule (Long).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX5(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v15.1.325"
+    return "v15.1.326"
 
   stoploss = -0.99
 
@@ -6689,6 +6689,25 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["RSI_14_4h"] < 35.0)
         | (df["MFI_14_4h"] < 40.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 20.0)
+        | (df["MFI_14_1d"] < 50.0)
+      )
+      # 15m & 1h & 4h & 1d down move, 15m & 1h still not low enough, 4h & 1d still high
+      & (
+        (df["RSI_3_15m"] > 30.0)
+        | (df["RSI_3_1h"] > 15.0)
+        | (df["RSI_3_4h"] > 35.0)
+        | (df["RSI_3_1d"] > 50.0)
+        | (df["RSI_14_15m"] < 25.0)
+        | (df["CCI_20_15m"] < -150.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 20.0)
+        | (df["RSI_14_1h"] < 30.0)
+        | (df["AROONU_14_1h"] < 50.0)
+        | (df["CCI_20_1h"] < -150.0)
+        | (df["RSI_14_4h"] < 30.0)
+        | (df["AROONU_14_4h"] < 75.0)
+        | (df["CCI_20_4h"] < -100.0)
+        | (df["STOCHRSIk_14_14_3_3_4h"] < 50.0)
+        | (df["RSI_14_1d"] < 40.0)
         | (df["MFI_14_1d"] < 50.0)
       )
       # 15m & 4h & 1d down move, 15m & 1h & 4h still not low enough, 1d still high
