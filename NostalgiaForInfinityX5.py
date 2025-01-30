05064f6102119eacf950d8975d275b7da78c39a4
iterativ <vangelis.dimou@tutanota.com>
2025-01-30 18:27:05 +0200

    X5: add global protection rule (Long).

--- NostalgiaForInfinityX5.py
+++ NostalgiaForInfinityX5.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX5(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v15.1.356"
+    return "v15.1.357"
 
   stoploss = -0.99
 
@@ -5369,6 +5369,20 @@ class NostalgiaForInfinityX5(IStrategy):
         | (df["AROONU_14_4h"] < 25.0)
         | (df["RSI_14_1d"] < 60.0)
         | (df["ROC_9_1d"] < 30.0)
+      )
+      # 15m down move, 15m & 1h still not low nough, 4h still high & overbought
+      & (
+        (df["RSI_3_15m"] > 65.0)
+        | (df["RSI_14_15m"] < 40.0)
+        | (df["CCI_20_15m"] < -100.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 20.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["CCI_20_1h"] < -100.0)
+        | (df["RSI_14_4h"] < 60.0)
+        | (df["AROONU_14_4h"] < 50.0)
+        | (df["CCI_20_4h"] < -0.0)
+        | (df["STOCHRSIk_14_14_3_3_4h"] < 50.0)
+        | (df["ROC_9_4h"] < 200.0)
       )
       # 15m down move, 15m & 1h high, 4h high & overbought
       & (
