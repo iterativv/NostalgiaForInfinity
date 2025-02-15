10c150a184b296e399880d50c2a97fbbefd9febe
iterativ <vangelis.dimou@tutanota.com>
2025-02-15 20:03:25 +0200

    X6: grinding: fine tune the entries.

--- experimental/NostalgiaForInfinityX6.py
+++ experimental/NostalgiaForInfinityX6.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.0.2"
+    return "v16.0.3"
 
   stoploss = -0.99
 
@@ -32625,7 +32625,9 @@ class NostalgiaForInfinityX6(IStrategy):
         or (
           (last_candle["RSI_14"] < 46.0)
           and (last_candle["RSI_3"] > 10.0)
-          and (last_candle["RSI_3_15m"] > 10.0)
+          and (last_candle["RSI_3_15m"] > 15.0)
+          and (last_candle["RSI_3_1h"] > 15.0)
+          and (last_candle["RSI_3_4h"] > 15.0)
           and (last_candle["AROONU_14"] < 25.0)
           and (last_candle["ROC_2_1h"] > -5.0)
           and (last_candle["ROC_2_4h"] > -5.0)
@@ -32646,9 +32648,9 @@ class NostalgiaForInfinityX6(IStrategy):
         or (
           (last_candle["RSI_14"] < 36.0)
           and (last_candle["RSI_3"] > 5.0)
-          and (last_candle["RSI_3_15m"] > 10.0)
-          and (last_candle["RSI_3_1h"] > 10.0)
-          and (last_candle["RSI_3_4h"] > 10.0)
+          and (last_candle["RSI_3_15m"] > 15.0)
+          and (last_candle["RSI_3_1h"] > 15.0)
+          and (last_candle["RSI_3_4h"] > 15.0)
           and (last_candle["ROC_2_1h"] > -5.0)
           and (last_candle["ROC_2_4h"] > -5.0)
           and (last_candle["ROC_9_1h"] > -10.0)
@@ -32694,7 +32696,7 @@ class NostalgiaForInfinityX6(IStrategy):
           and (last_candle["close"] > (last_candle["close_max_48"] * 0.90))
           and (last_candle["close"] > (last_candle["high_max_6_1h"] * 0.85))
           and (last_candle["close"] > (last_candle["high_max_12_1h"] * 0.80))
-          and (last_candle["close"] < (last_candle["EMA_26"] * 0.940))
+          and (last_candle["close"] < (last_candle["EMA_26"] * 0.945))
           and (last_candle["close"] < (last_candle["BBL_20_2.0"] * 0.999))
         )
       )
