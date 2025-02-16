3ff5304bd1f0ca12e476539ac62160380e6057e8
iterativ <vangelis.dimou@tutanota.com>
2025-02-16 19:24:37 +0200

    X6: grinding: fine tune the thresholds.

--- experimental/NostalgiaForInfinityX6.py
+++ experimental/NostalgiaForInfinityX6.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.0.3"
+    return "v16.0.4"
 
   stoploss = -0.99
 
@@ -379,10 +379,10 @@ class NostalgiaForInfinityX6(IStrategy):
   grinding_v2_grind_1_thresholds_spot = [-0.06, -0.08, -0.10, -0.12]
   grinding_v2_grind_1_stakes_futures = [0.25, 0.30, 0.35, 0.40]
   grinding_v2_grind_1_thresholds_futures = [-0.06, -0.08, -0.10, -0.12]
-  grinding_v2_grind_1_profit_threshold_spot = 0.02
-  grinding_v2_grind_1_profit_threshold_futures = 0.02
-  grinding_v2_grind_1_derisk_spot = -0.03
-  grinding_v2_grind_1_derisk_futures = -0.03
+  grinding_v2_grind_1_profit_threshold_spot = 0.01
+  grinding_v2_grind_1_profit_threshold_futures = 0.01
+  grinding_v2_grind_1_derisk_spot = -0.10
+  grinding_v2_grind_1_derisk_futures = -0.10
 
   grinding_v2_grind_2_stakes_spot = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18]
   grinding_v2_grind_2_thresholds_spot = [-0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.12, -0.13]
@@ -390,8 +390,8 @@ class NostalgiaForInfinityX6(IStrategy):
   grinding_v2_grind_2_thresholds_futures = [-0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.12, -0.13]
   grinding_v2_grind_2_profit_threshold_spot = 0.02
   grinding_v2_grind_2_profit_threshold_futures = 0.02
-  grinding_v2_grind_2_derisk_spot = -0.03
-  grinding_v2_grind_2_derisk_futures = -0.03
+  grinding_v2_grind_2_derisk_spot = -0.10
+  grinding_v2_grind_2_derisk_futures = -0.10
 
   grinding_v2_buyback_1_stake_spot = 0.20
   grinding_v2_buyback_1_stake_futures = 0.20
@@ -399,8 +399,8 @@ class NostalgiaForInfinityX6(IStrategy):
   grinding_v2_buyback_1_distance_ratio_futures = -0.06
   grinding_v2_buyback_1_profit_threshold_spot = 0.10
   grinding_v2_buyback_1_profit_threshold_futures = 0.10
-  grinding_v2_buyback_1_derisk_spot = -0.02
-  grinding_v2_buyback_1_derisk_futures = -0.02
+  grinding_v2_buyback_1_derisk_spot = -0.10
+  grinding_v2_buyback_1_derisk_futures = -0.10
 
   grinding_v2_buyback_2_stake_spot = 0.25
   grinding_v2_buyback_2_stake_futures = 0.25
@@ -408,8 +408,8 @@ class NostalgiaForInfinityX6(IStrategy):
   grinding_v2_buyback_2_distance_ratio_futures = -0.12
   grinding_v2_buyback_2_profit_threshold_spot = 0.10
   grinding_v2_buyback_2_profit_threshold_futures = 0.10
-  grinding_v2_buyback_2_derisk_spot = -0.02
-  grinding_v2_buyback_2_derisk_futures = -0.02
+  grinding_v2_buyback_2_derisk_spot = -0.10
+  grinding_v2_buyback_2_derisk_futures = -0.10
 
   grinding_v2_buyback_3_stake_spot = 0.30
   grinding_v2_buyback_3_stake_futures = 0.30
@@ -417,8 +417,8 @@ class NostalgiaForInfinityX6(IStrategy):
   grinding_v2_buyback_3_distance_ratio_futures = -0.16
   grinding_v2_buyback_3_profit_threshold_spot = 0.10
   grinding_v2_buyback_3_profit_threshold_futures = 0.10
-  grinding_v2_buyback_3_derisk_spot = -0.02
-  grinding_v2_buyback_3_derisk_futures = -0.02
+  grinding_v2_buyback_3_derisk_spot = -0.10
+  grinding_v2_buyback_3_derisk_futures = -0.10
 
   # Rebuy mode
   rebuy_mode_stake_multiplier = 0.2
