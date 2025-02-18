1c105106e7c091610eb474df1affd7e51d46c112
iterativ <vangelis.dimou@tutanota.com>
2025-02-18 21:12:57 +0200

    X6: add global protection rule (Long).

--- experimental/NostalgiaForInfinityX6.py
+++ experimental/NostalgiaForInfinityX6.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.0.6"
+    return "v16.0.7"
 
   stoploss = -0.99
 
@@ -3757,6 +3757,26 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["RSI_14_1d"] < 70.0)
         | (df["AROONU_14_1d"] < 75.0)
         | (df["ROC_9_1d"] < 25.0)
+      )
+      # 15m & 1h & 4h & 1d down move, 15m & 1h & 4h still not low enough, 1d still high
+      & (
+        (df["RSI_3_15m"] > 20.0)
+        | (df["RSI_3_1h"] > 50.0)
+        | (df["RSI_3_4h"] > 50.0)
+        | (df["RSI_3_1d"] > 50.0)
+        | (df["RSI_14_15m"] < 20.0)
+        | (df["CCI_20_15m"] < -300.0)
+        | (df["RSI_14_1h"] < 30.0)
+        | (df["CCI_20_1h"] < -200.0)
+        | (df["RSI_14_4h"] < 30.0)
+        | (df["CCI_20_4h"] < -100.0)
+        | (df["STOCHRSIk_14_14_3_3_4h"] < 10.0)
+        | (df["RSI_14_1d"] < 40.0)
+        | (df["AROONU_14_1d"] < 50.0)
+        | (df["STOCHRSIk_14_14_3_3_1d"] < 20.0)
+        | (df["ROC_9_1d"] < 50.0)
+        | (df["close"] > (df["high_max_6_1d"] * 0.55))
+        | (df["close"] < (df["low_min_12_1d"] * 1.20))
       )
       # 15m & 1h & 4h down move, 15m & 1h & 4h still not low enough, 1d still high & overbought
       & (
