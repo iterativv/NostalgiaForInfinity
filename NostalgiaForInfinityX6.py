2732751f03982539917e1cd4c5a5e57d70e3b919
iterativ <vangelis.dimou@tutanota.com>
2025-03-09 10:40:53 +0200

    X6: grinding: fine tune the grind entries.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.1.6"
+    return "v16.1.7"
 
   stoploss = -0.99
 
@@ -33068,7 +33068,7 @@ class NostalgiaForInfinityX6(IStrategy):
         and (last_candle["ROC_9_1h"] > -10.0)
         and (last_candle["ROC_9_4h"] > -10.0)
         # and (last_candle["ROC_9_1d"] > -10.0)
-        # and (last_candle["AROONU_14"] < 25.0)
+        and (last_candle["AROONU_14"] < 25.0)
         # and (last_candle["AROONU_14_15m"] < 25.0)
         # and (last_candle["STOCHRSIk_14_14_3_3"] < 20.0)
         # and (last_candle["STOCHRSIk_14_14_3_3_15m"] < 20.0)
@@ -33121,6 +33121,7 @@ class NostalgiaForInfinityX6(IStrategy):
         and (last_candle["STOCHRSIk_14_14_3_3"] < 20.0)
         and (last_candle["WILLR_84_1h"] < -70.0)
         # and (last_candle["STOCHRSIk_14_14_3_3_1h"] < 40.0)
+        and (last_candle["close"] < (last_candle["low_min_24_4h"] * 1.30))
         and (last_candle["BBB_20_2.0_1h"] > 12.0)
         and (last_candle["close_max_48"] >= (last_candle["close"] * 1.10))
       )
@@ -33128,14 +33129,15 @@ class NostalgiaForInfinityX6(IStrategy):
         (last_candle["RSI_3"] < 30.0)
         and (last_candle["RSI_3"] > 5.0)
         and (last_candle["RSI_3_15m"] > 5.0)
-        # and (last_candle["RSI_3_1h"] > 10.0)
-        # and (last_candle["RSI_3_4h"] > 10.0)
+        and (last_candle["RSI_3_1h"] > 10.0)
+        and (last_candle["RSI_3_4h"] > 10.0)
         # and (last_candle["ROC_2_1h"] > -5.0)
         # and (last_candle["ROC_2_4h"] > -5.0)
         # and (last_candle["ROC_9_1h"] > -5.0)
         # and (last_candle["ROC_9_4h"] > -5.0)
         and (last_candle["ROC_9_1d"] > -30.0)
         # and (last_candle["STOCHRSIk_14_14_3_3"] < 20.0)
+        # and (last_candle["close"] < (last_candle["low_min_24_4h"] * 1.50))
         and (last_candle["EMA_26"] > last_candle["EMA_12"])
         and ((last_candle["EMA_26"] - last_candle["EMA_12"]) > (last_candle["open"] * 0.034))
         and ((previous_candle["EMA_26"] - previous_candle["EMA_12"]) > (last_candle["open"] / 100.0))
