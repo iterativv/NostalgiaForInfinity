3229e9c6ea4567addac70678d0dd29b1de68289f
iterativ <vangelis.dimou@tutanota.com>
2025-03-29 18:37:00 +0200

    X6: signal 6: fine tune.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.3.26"
+    return "v16.3.27"
 
   stoploss = -0.99
 
@@ -11300,16 +11300,16 @@ class NostalgiaForInfinityX6(IStrategy):
           long_entry_logic.append(
             (df["RSI_3"] > 10.0) | (df["AROONU_14_15m"] < 30.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 70.0)
           )
-          # 5m down move, 15m still not low enough, 4h high
-          long_entry_logic.append(
-            (df["RSI_3"] > 10.0) | (df["AROONU_14_15m"] < 30.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0)
-          )
           # 5m down move, 1h still high, 4h high
           long_entry_logic.append(
             (df["RSI_3"] > 10.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0) | (df["AROONU_14_4h"] < 95.0)
           )
           # 5m down move, 15m still not low enough, 4h high
           long_entry_logic.append((df["RSI_3"] > 15.0) | (df["AROONU_14_15m"] < 30.0) | (df["AROONU_14_4h"] < 90.0))
+          # 5m down move, 15m still not low enough, 4h high
+          long_entry_logic.append(
+            (df["RSI_3"] > 15.0) | (df["AROONU_14_15m"] < 30.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0)
+          )
           # 5m down move, 15m & 4h high
           long_entry_logic.append(
             (df["RSI_3"] > 20.0) | (df["STOCHRSIk_14_14_3_3_15m"] < 75.0) | (df["AROONU_14_4h"] < 90.0)
