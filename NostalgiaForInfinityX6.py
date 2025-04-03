ff7594122de1993efeff539306ccf9bbb17f5720
iterativ <vangelis.dimou@tutanota.com>
2025-04-03 21:08:57 +0300

    X6: signal 102: fine tune.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -67,7 +67,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.3.36"
+    return "v16.3.37"
 
   stoploss = -0.99
 
@@ -13009,6 +13009,10 @@ class NostalgiaForInfinityX6(IStrategy):
           )
           # 5m & 15m down move, 4h high
           long_entry_logic.append((df["RSI_3"] > 5.0) | (df["RSI_3_15m"] > 20.0) | (df["AROONU_14_4h"] < 70.0))
+          # 5m & 15m down move, 16m still not low enough
+          long_entry_logic.append(
+            (df["RSI_3"] > 5.0) | (df["RSI_3_15m"] > 30.0) | (df["STOCHRSIk_14_14_3_3_15m"] < 30.0)
+          )
           # 5m down move, 1h high
           long_entry_logic.append((df["RSI_3"] > 5.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 80.0))
           # 5m down move, 1h & 4h high
