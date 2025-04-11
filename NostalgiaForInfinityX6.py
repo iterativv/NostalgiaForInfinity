6459a5db5abf5f06b173f5b8ea627c55e2b27a69
iterativ <vangelis.dimou@tutanota.com>
2025-04-11 12:48:06 +0300

    X6: grindind: remove the exception for Bybit futures min order stake.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -10037,10 +10037,10 @@ class NostalgiaForInfinityX6(IStrategy):
   # Correct Min Stake
   # ---------------------------------------------------------------------------------------------
   def correct_min_stake(self, min_stake: float) -> float:
-    if self.config["exchange"]["name"] in ["bybit"]:
-      if self.is_futures_mode:
-        if min_stake < 5.0:
-          min_stake = 5.0
+    # if self.config["exchange"]["name"] in ["bybit"]:
+    #   if self.is_futures_mode:
+    #     if min_stake < 5.0:
+    #       min_stake = 5.0
     return min_stake
 
   def is_backtest_mode(self) -> bool:
