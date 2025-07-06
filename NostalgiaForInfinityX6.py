ae92524f146bfd7abc3fe6e9ca6a2c488c0359c2
iterativ <vangelis.dimou@tutanota.com>
2025-07-06 16:29:46 +0300

    X6: signal 161: refactor.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.154"
+    return "v16.5.155"
 
   stoploss = -0.99
 
@@ -9291,19 +9291,12 @@ class NostalgiaForInfinityX6(IStrategy):
           long_entry_logic.append(df["RSI_14"] < 50.0)
           long_entry_logic.append(df["AROONU_14_15m"] < 90.0)
           long_entry_logic.append(df["STOCHRSIk_14_14_3_3_15m"] < 90.0)
-          if isinstance(df["SMA_200"].iloc[-1], np.float64):
-            long_entry_logic.append(df["SMA_21"].shift(1) < df["SMA_200"].shift(1))
-            long_entry_logic.append(df["SMA_21"] > df["SMA_200"])
-          else:
-            long_entry_logic.append(pd.Series([False]))
-          if isinstance(df["EMA_200_1h"].iloc[-1], np.float64):
-            long_entry_logic.append(df["close"] > df["EMA_200_1h"])
-          else:
-            long_entry_logic.append(pd.Series([False]))
-          if isinstance(df["EMA_200_4h"].iloc[-1], np.float64):
-            long_entry_logic.append(df["close"] > df["EMA_200_4h"])
-          else:
-            long_entry_logic.append(pd.Series([False]))
+          long_entry_logic.append(
+            (df["SMA_21"].shift(1) < df["SMA_200"].shift(1).fillna(np.nan)) & df["SMA_200"].shift(1).notna()
+          )
+          long_entry_logic.append((df["SMA_21"] > df["SMA_200"].fillna(np.nan)) & df["SMA_200"].notna())
+          long_entry_logic.append((df["close"] > df["EMA_200_1h"].fillna(np.nan)) & df["EMA_200_1h"].notna())
+          long_entry_logic.append((df["close"] > df["EMA_200_4h"].fillna(np.nan)) & df["EMA_200_4h"].notna())
           long_entry_logic.append(df["BBB_20_2.0"] > 1.5)
           long_entry_logic.append(df["BBB_20_2.0_1h"] > 6.0)
 
