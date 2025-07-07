93e3f93d6a5f20c1edb4d7d54943204a5f7fb4f2
iterativ <vangelis.dimou@tutanota.com>
2025-07-07 12:13:37 +0300

    X6: grinding v2: add entry condition for grinds.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.156"
+    return "v16.5.157"
 
   stoploss = -0.99
 
@@ -30388,6 +30388,7 @@ class NostalgiaForInfinityX6(IStrategy):
         and (last_candle["RSI_14_1h"] < 50.0)
         and (last_candle["RSI_14_4h"] < 50.0)
       )
+      or ((slice_profit < -0.06) and (last_candle["RSI_3"] > 20.0) and (last_candle["RSI_3_15m"] > 30.0))
     ):
       return True
 
@@ -52963,6 +52964,7 @@ class NostalgiaForInfinityX6(IStrategy):
         and (last_candle["RSI_14_1h"] > 50.0)
         and (last_candle["RSI_14_4h"] > 50.0)
       )
+      or ((slice_profit > 0.06) and (last_candle["RSI_3"] < 80.0) and (last_candle["RSI_3_15m"] < 70.0))
     ):
       return True
 
