5773e4d36e546bea4ab4b5a34a929f837ea5714c
iterativ <vangelis.dimou@tutanota.com>
2025-07-07 17:55:51 +0300

    X6: fix incorrect min stake reporting on Bybit futures.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.159"
+    return "v16.5.160"
 
   stoploss = -0.99
 
@@ -3894,10 +3894,10 @@ class NostalgiaForInfinityX6(IStrategy):
   # Correct Min Stake
   # ---------------------------------------------------------------------------------------------
   def correct_min_stake(self, min_stake: float) -> float:
-    # if self.config["exchange"]["name"] in ["bybit"]:
-    #   if self.is_futures_mode:
-    #     if min_stake < 5.0:
-    #       min_stake = 5.0
+    if self.config["exchange"]["name"] in ["bybit"]:
+      if self.is_futures_mode:
+        if min_stake < 5.0:
+          min_stake = 5.0
     return min_stake
 
   def is_backtest_mode(self) -> bool:
