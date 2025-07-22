797c9923dd19efa34fbac9075e8563f8617311fa
iterativ <vangelis.dimou@tutanota.com>
2025-07-22 12:57:31 +0300

    X6: signal 46: add global protections.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -7304,6 +7304,7 @@ class NostalgiaForInfinityX6(IStrategy):
         if long_entry_condition_index == 46:
           # Protections
           long_entry_logic.append(df["num_empty_288"] <= allowed_empty_candles_288)
+          long_entry_logic.append(df["protections_long_global"] == True)
 
           # 5m & 1h down move
           long_entry_logic.append((df["RSI_3_15m"] > 3.0) | (df["RSI_3_1h"] > 5.0))
