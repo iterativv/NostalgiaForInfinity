11a6ff437b11533d94423795be1c3ec63751cfcb
iterativ <vangelis.dimou@tutanota.com>
2025-07-22 11:54:28 +0300

    X6: add global protection (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.235"
+    return "v16.5.236"
 
   stoploss = -0.99
 
@@ -3358,7 +3358,27 @@ class NostalgiaForInfinityX6(IStrategy):
     df["RSI_14_1h"] = df["RSI_14_1h"].astype(np.float64).replace(to_replace=[np.nan, None], value=(50.0))
 
     # Global protections Long
-    df["protections_long_global"] = True
+    df["protections_long_global"] = (
+      # 1h & 4h down move, 15m & 1h & 4h downtrend, 1h still high
+      (
+        (df["RSI_3_1h"] > 25.0)
+        | (df["RSI_3_4h"] > 25.0)
+        | (df["CMF_20_15m"] > -0.5)
+        | (df["CMF_20_1h"] > -0.3)
+        | (df["CMF_20_4h"] > -0.3)
+        | (df["AROONU_14_1h"] < 50.0)
+      )
+      # 5m & 15m & 1h & 4h down move, 15m downtrend, 4h high, 1d overbought
+      & (
+        (df["RSI_3"] > 5.0)
+        | (df["RSI_3_15m"] > 10.0)
+        | (df["RSI_3_1h"] > 40.0)
+        | (df["RSI_3_4h"] > 55.0)
+        | (df["CMF_20_15m"] > -0.25)
+        | (df["AROONU_14_4h"] < 60.0)
+        | (df["ROC_9_1d"] < 80.0)
+      )
+    )
 
     df["global_protections_long_pump"] = True
 
