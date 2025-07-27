acd143b0edfc209de6a730f8a8d27e4b534e7a7c
iterativ <vangelis.dimou@tutanota.com>
2025-07-27 19:52:38 +0300

    X6: grinding v2: fine tune the grind queue 4 stakes/thresholds.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.281"
+    return "v16.5.282"
 
   stoploss = -0.99
 
@@ -424,10 +424,10 @@ class NostalgiaForInfinityX6(IStrategy):
   grinding_v2_grind_3_derisk_futures = -0.35
 
   grinding_v2_grind_4_enable = True
-  grinding_v2_grind_4_stakes_spot = [0.05, 0.07, 0.10, 0.14, 0.18]
-  grinding_v2_grind_4_thresholds_spot = [-0.06, -0.07, -0.09, -0.12, -0.16]
-  grinding_v2_grind_4_stakes_futures = [0.05, 0.07, 0.10, 0.14, 0.18]
-  grinding_v2_grind_4_thresholds_futures = [-0.06, -0.07, -0.09, -0.12, -0.16]
+  grinding_v2_grind_4_stakes_spot = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11]
+  grinding_v2_grind_4_thresholds_spot = [-0.06, -0.07, -0.09, -0.12, -0.16, -0.21, -0.27]
+  grinding_v2_grind_4_stakes_futures = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11]
+  grinding_v2_grind_4_thresholds_futures = [-0.06, -0.07, -0.09, -0.12, -0.16, -0.21, -0.27]
   grinding_v2_grind_4_profit_threshold_spot = 0.10
   grinding_v2_grind_4_profit_threshold_futures = 0.10
   grinding_v2_grind_4_use_derisk = False
