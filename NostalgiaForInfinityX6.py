a8d94f118fa48d9f937800cb90a988d1e86a526c
iterativ <vangelis.dimou@tutanota.com>
2025-08-04 11:58:58 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.6.2"
+    return "v16.6.3"
 
   stoploss = -0.99
 
@@ -3745,6 +3745,16 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_4h"] < 100.0)
         | (df["ROC_9_1d"] < 250.0)
       )
+      # 15m down move, 15m still high, 1h & 4h high, 1h & 1d overbought
+      & (
+        (df["RSI_3_15m"] > 15.0)
+        | (df["RSI_14_15m"] < 40.0)
+        | (df["RSI_14_1h"] < 70.0)
+        | (df["RSI_14_4h"] < 70.0)
+        | (df["AROONU_14_1h"] < 80.0)
+        | (df["ROC_9_1h"] < 40.0)
+        | (df["ROC_9_1d"] < 100.0)
+      )
       # 15m & 1h down move, 15m still not low enough, 1h & 4h still high, 1d overbought
       & (
         (df["RSI_3_15m"] > 20.0)
@@ -4291,6 +4301,17 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["CMF_20_1h"] > -0.25)
         | (df["AROONU_14_4h"] < 50.0)
       )
+      # 1d top wick, 15m down move, 15m stil high, 1h & 4h high, 4h overbought
+      & (
+        (df["top_wick_pct_1d"] < 25.0)
+        | (df["RSI_3_15m"] > 35.0)
+        | (df["RSI_14_15m"] < 40.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["RSI_14_4h"] < 80.0)
+        | (df["AROONU_14_1h"] < 80.0)
+        | (df["AROONU_14_4h"] < 80.0)
+        | (df["ROC_9_4h"] < 50.0)
+      )
       # pump, drop but not yet near the previous lows, 15m & 1h & 4h & 1d down move, 1d overbought
       & (
         (((df["high_max_6_1d"] - df["low_min_6_1d"]) / df["low_min_6_1d"]) < 1.5)
@@ -4311,6 +4332,18 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["RSI_14_1h"] < 30.0)
         | (df["RSI_14_4h"] < 40.0)
         | (df["ROC_9_1d"] < 20.0)
+      )
+      # drop in last 12 hours, 14m & 1h & 4h down move, 15m still not low enough, 1h still high, 4h high & overbought
+      & (
+        (df["close"] > (df["high_max_12_1h"] * 0.50))
+        | (df["RSI_3_15m"] > 20.0)
+        | (df["RSI_3_1h"] > 35.0)
+        | (df["RSI_3_4h"] > 55.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 40.0)
+        | (df["RSI_14_4h"] < 50.0)
+        | (df["AROONU_14_4h"] < 80.0)
+        | (df["ROC_9_4h"] < 25.0)
       )
       # drop in last 12 hours, 1h & 4h down move, 1h & 4h downtrend
       & (
