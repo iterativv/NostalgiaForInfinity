30a684c7d63d94657a0749cb11694dbe2d8c85bf
iterativ <vangelis.dimou@tutanota.com>
2025-08-30 19:21:00 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.6.189"
+    return "v16.6.190"
 
   stoploss = -0.99
 
@@ -4811,6 +4811,17 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_4h"] < 50.0)
         | (df["ROC_9_1d"] > -40.0)
       )
+      # 15m & 1h & 1d down move, 4h & 1d downtrend, 1h & 4h high, 1d downtrend
+      & (
+        (df["RSI_3_15m"] > 25.0)
+        | (df["RSI_3_1h"] > 35.0)
+        | (df["RSI_3_1d"] > 35.0)
+        | (df["CMF_20_4h"] > -0.1)
+        | (df["CMF_20_1d"] > -0.1)
+        | (df["AROONU_14_1h"] < 75.0)
+        | (df["STOCHRSIk_14_14_3_3_4h"] < 70.0)
+        | (df["ROC_2_1d"] > -20.0)
+      )
       # 15m & 1h down move, 15m still not low enough, 1h still high, 4h high, 4h & 1d overbought
       & (
         (df["RSI_3_15m"] > 25.0)
@@ -4822,16 +4833,19 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["ROC_9_4h"] < 80.0)
         | (df["ROC_9_1d"] < 100.0)
       )
-      # 15m & 1h & 1d down move, 4h & 1d downtrend, 1h & 4h high, 1d downtrend
+      # 15m & 1h down move, 15m still not low enough, 1h & 4h high, 1h & 4h overbought
       & (
         (df["RSI_3_15m"] > 25.0)
         | (df["RSI_3_1h"] > 35.0)
-        | (df["RSI_3_1d"] > 35.0)
-        | (df["CMF_20_4h"] > -0.1)
-        | (df["CMF_20_1d"] > -0.1)
-        | (df["AROONU_14_1h"] < 75.0)
-        | (df["STOCHRSIk_14_14_3_3_4h"] < 70.0)
-        | (df["ROC_2_1d"] > -20.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["RSI_14_4h"] < 80.0)
+        | (df["AROONU_14_1h"] < 50.0)
+        | (df["AROONU_14_4h"] < 90.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 30.0)
+        | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0)
+        | (df["ROC_9_1h"] < 20.0)
+        | (df["ROC_9_4h"] < 60.0)
       )
       # 15m & 1h & 1d down move, 15m & 1h & 4h still high, 15m & 1d downtrend
       & (
