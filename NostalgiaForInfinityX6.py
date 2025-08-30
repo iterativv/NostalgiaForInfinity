3d8338d5780f11849edb9b299a42b177d92e0130
iterativ <vangelis.dimou@tutanota.com>
2025-08-30 21:29:35 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.6.190"
+    return "v16.6.191"
 
   stoploss = -0.99
 
@@ -3493,6 +3493,19 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_15m"] < 60.0)
         | (df["AROONU_14_4h"] < 70.0)
         | (df["STOCHRSIk_14_14_3_3_15m"] < 50.0)
+      )
+      # 5m & 15m & 1h down move, 15m & 1h still high, 4h high, 15m still not low enough, 5h overbought
+      & (
+        (df["RSI_3"] > 10.0)
+        | (df["RSI_3_15m"] > 25.0)
+        | (df["RSI_3_1h"] > 55.0)
+        | (df["RSI_14_15m"] < 40.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["RSI_14_4h"] < 70.0)
+        | (df["AROONU_14_1h"] < 20.0)
+        | (df["AROONU_14_4h"] < 80.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 20.0)
+        | (df["ROC_9_4h"] < 80.0)
       )
       # 5m & 15m & 4h down move, 15m & 1h still not low enough, 4h still high, 15m & 4h high
       & (
