f08a5db4f5db2438a24b28acb3cf6a33b31314c0
iterativ <vangelis.dimou@tutanota.com>
2025-09-01 10:37:48 +0300

    X6: grinding v2: fine tune the grind thresholds.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.6.200"
+    return "v16.7.0"
 
   stoploss = -0.99
 
@@ -392,9 +392,9 @@ class NostalgiaForInfinityX6(IStrategy):
 
   grinding_v2_grind_1_enable = True
   grinding_v2_grind_1_stakes_spot = [0.20, 0.21, 0.22, 0.23]
-  grinding_v2_grind_1_thresholds_spot = [-0.12, -0.14, -0.16, -0.18]
+  grinding_v2_grind_1_thresholds_spot = [-0.06, -0.07, -0.08, -0.09]
   grinding_v2_grind_1_stakes_futures = [0.20, 0.21, 0.22, 0.23]
-  grinding_v2_grind_1_thresholds_futures = [-0.12, -0.14, -0.16, -0.18]
+  grinding_v2_grind_1_thresholds_futures = [-0.06, -0.07, -0.08, -0.09]
   grinding_v2_grind_1_profit_threshold_spot = 0.028
   grinding_v2_grind_1_profit_threshold_futures = 0.028
   grinding_v2_grind_1_use_derisk = True
@@ -403,9 +403,9 @@ class NostalgiaForInfinityX6(IStrategy):
 
   grinding_v2_grind_2_enable = True
   grinding_v2_grind_2_stakes_spot = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18]
-  grinding_v2_grind_2_thresholds_spot = [-0.10, -0.11, -0.12, -0.13, -0.14, -0.15, -0.16, -0.17, -0.18]
+  grinding_v2_grind_2_thresholds_spot = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_2_stakes_futures = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18]
-  grinding_v2_grind_2_thresholds_futures = [-0.10, -0.11, -0.12, -0.13, -0.14, -0.15, -0.16, -0.17, -0.18]
+  grinding_v2_grind_2_thresholds_futures = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_2_profit_threshold_spot = 0.05
   grinding_v2_grind_2_profit_threshold_futures = 0.05
   grinding_v2_grind_2_use_derisk = True
@@ -414,9 +414,9 @@ class NostalgiaForInfinityX6(IStrategy):
 
   grinding_v2_grind_3_enable = True
   grinding_v2_grind_3_stakes_spot = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18]
-  grinding_v2_grind_3_thresholds_spot = [-0.10, -0.11, -0.12, -0.13, -0.14, -0.15, -0.16, -0.17, -0.18]
+  grinding_v2_grind_3_thresholds_spot = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_3_stakes_futures = [0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18]
-  grinding_v2_grind_3_thresholds_futures = [-0.10, -0.11, -0.12, -0.13, -0.14, -0.15, -0.16, -0.17, -0.18]
+  grinding_v2_grind_3_thresholds_futures = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_3_profit_threshold_spot = 0.05
   grinding_v2_grind_3_profit_threshold_futures = 0.05
   grinding_v2_grind_3_use_derisk = True
@@ -425,9 +425,9 @@ class NostalgiaForInfinityX6(IStrategy):
 
   grinding_v2_grind_4_enable = True
   grinding_v2_grind_4_stakes_spot = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11]
-  grinding_v2_grind_4_thresholds_spot = [-0.06, -0.07, -0.09, -0.12, -0.16, -0.21, -0.27]
+  grinding_v2_grind_4_thresholds_spot = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_4_stakes_futures = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11]
-  grinding_v2_grind_4_thresholds_futures = [-0.06, -0.07, -0.09, -0.12, -0.16, -0.21, -0.27]
+  grinding_v2_grind_4_thresholds_futures = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_4_profit_threshold_spot = 0.10
   grinding_v2_grind_4_profit_threshold_futures = 0.10
   grinding_v2_grind_4_use_derisk = True
@@ -436,9 +436,9 @@ class NostalgiaForInfinityX6(IStrategy):
 
   grinding_v2_grind_5_enable = True
   grinding_v2_grind_5_stakes_spot = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11]
-  grinding_v2_grind_5_thresholds_spot = [-0.10, -0.11, -0.12, -0.13, -0.14, -0.15, -0.16]
+  grinding_v2_grind_5_thresholds_spot = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_5_stakes_futures = [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11]
-  grinding_v2_grind_5_thresholds_futures = [-0.10, -0.11, -0.12, -0.13, -0.14, -0.15, -0.16]
+  grinding_v2_grind_5_thresholds_futures = [-0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.10, -0.11]
   grinding_v2_grind_5_profit_threshold_spot = 0.10
   grinding_v2_grind_5_profit_threshold_futures = 0.10
   grinding_v2_grind_5_use_derisk = True
