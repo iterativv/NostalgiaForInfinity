4240b79f4df38714f230c2b4ff090f7b09e9026d
iterativ <vangelis.dimou@tutanota.com>
2025-09-08 00:09:15 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.7.45"
+    return "v16.7.46"
 
   stoploss = -0.99
 
@@ -2477,9 +2477,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     # Add exchange information
     exchange_name = getattr(self, "config", {}).get("exchange", {}).get("name", "Unknown").capitalize()
-    msg += (
-      f"🏦 **Exchange:** `{exchange_name}`\n"
-    )
+    msg += f"🏦 **Exchange:** `{exchange_name}`\n"
 
     # Common fields
     msg += (
@@ -3945,6 +3943,18 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_1h"] < 80.0)
         | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 50.0)
+      )
+      # 15m & 1h down move, 15m still not low enough, 1h & 4h high, 1h & 1d overbought
+      & (
+        (df["RSI_3_15m"] > 10.0)
+        | (df["RSI_3_1h"] > 20.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["RSI_14_4h"] < 50.0)
+        | (df["AROONU_14_1h"] < 50.0)
+        | (df["AROONU_14_4h"] < 100.0)
+        | (df["ROC_9_4h"] < 10.0)
+        | (df["ROC_9_1d"] < 20.0)
       )
       # 15m & 1h & 4h down move, 1h & 4h not low enouhg, 15m downtrend, 4h high, 1d overbought
       & (
