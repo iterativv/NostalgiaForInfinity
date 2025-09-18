5a438e65bd44339498ea2439ae7efb5358d7c531
iterativ <vangelis.dimou@tutanota.com>
2025-09-18 21:32:09 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.7.111"
+    return "v16.7.112"
 
   stoploss = -0.99
 
@@ -6095,7 +6095,7 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["RSI_14_1h"] < 50.0)
         | (df["RSI_14_4h"] < 50.0)
         | (df["AROONU_14_15m"] < 70.0)
-        | (df["AROONU_14_4h"] < 100.0)
+        | (df["AROONU_14_4h"] < 90.0)
         | (df["STOCHRSIk_14_14_3_3_15m"] < 50.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0)
       )
