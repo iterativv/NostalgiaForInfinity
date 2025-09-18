8c4efcd314fbe4ad7f7ec4307111e38c64d5ae6a
iterativ <vangelis.dimou@tutanota.com>
2025-09-18 09:09:35 +0300

    X6: signal 2: fine tune.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.7.106"
+    return "v16.7.107"
 
   stoploss = -0.99
 
@@ -8707,6 +8707,16 @@ class NostalgiaForInfinityX6(IStrategy):
               | (df["CMF_20_15m"] > -0.25)
               | (df["CMF_20_1h"] > -0.25)
             )
+            # 15m & 1h & 4h down move, 15m still not low enough, 1h & 4h still high, 1d overbought
+            & (
+              (df["RSI_3_15m"] > 10.0)
+              | (df["RSI_3_1h"] > 25.0)
+              | (df["RSI_3_4h"] > 50.0)
+              | (df["RSI_14_15m"] < 30.0)
+              | (df["RSI_14_1h"] < 40.0)
+              | (df["RSI_14_4h"] < 40.0)
+              | (df["ROC_9_1d"] < 30.0)
+            )
             # 15m & 1h & 4h down move, 1h & 4h still high
             & (
               (df["RSI_3_15m"] > 10.0)
@@ -8775,6 +8785,15 @@ class NostalgiaForInfinityX6(IStrategy):
               | (df["RSI_14_4h"] < 30.0)
               | (df["AROONU_14_1h"] < 70.0)
             )
+            # 15m & 1h down move, 1h & 4h still high, 1h & 4h high
+            & (
+              (df["RSI_3_15m"] > 10.0)
+              | (df["RSI_3_1h"] > 60.0)
+              | (df["RSI_14_1h"] < 40.0)
+              | (df["RSI_14_4h"] < 40.0)
+              | (df["AROONU_14_1h"] < 80.0)
+              | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0)
+            )
             # 15m & 1h down move, 1h still high, 4h high & overbought
             & (
               (df["RSI_3_15m"] > 15.0)
@@ -8801,16 +8820,6 @@ class NostalgiaForInfinityX6(IStrategy):
               | (df["RSI_3_1d"] > 40.0)
               | (df["RSI_14_1h"] < 30.0)
               | (df["RSI_14_4h"] < 35.0)
-            )
-            # 15m & 1h & 4h down move, 15m still not low enough, 1h & 4h still high, 1d overbought
-            & (
-              (df["RSI_3_15m"] > 10.0)
-              | (df["RSI_3_1h"] > 25.0)
-              | (df["RSI_3_4h"] > 50.0)
-              | (df["RSI_14_15m"] < 30.0)
-              | (df["RSI_14_1h"] < 40.0)
-              | (df["RSI_14_4h"] < 40.0)
-              | (df["ROC_9_1d"] < 30.0)
             )
             # 15m & 1h down move, 15m still not low enough, 1h & 4h still high, 1h high
             & (
