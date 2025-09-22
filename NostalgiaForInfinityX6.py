86f10249147aa46fda2d38b9c5e8d702275c7b55
iterativ <vangelis.dimou@tutanota.com>
2025-09-22 15:23:35 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.7.134"
+    return "v16.7.135"
 
   stoploss = -0.99
 
@@ -5315,17 +5315,6 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_1h"] < 85.0)
         | (df["AROONU_14_4h"] < 85.0)
       )
-      # 15m & 1h down move, 15m still not low enough, 1h stil high, 4h high & overbought
-      & (
-        (df["RSI_3_15m"] > 25.0)
-        | (df["RSI_3_1h"] > 25.0)
-        | (df["RSI_14_15m"] < 30.0)
-        | (df["RSI_14_1h"] < 40.0)
-        | (df["RSI_14_4h"] < 70.0)
-        | (df["AROONU_14_1h"] < 40.0)
-        | (df["AROONU_14_4h"] < 85.0)
-        | (df["ROC_9_4h"] < 70.0)
-      )
       # 15m & 1h & 1d down move, 1h & 4h stilkl high, 1d downtrend, 1h high, 1h & 4h still high, 1d downtrend
       & (
         (df["RSI_3_15m"] > 25.0)
@@ -5338,6 +5327,28 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1h"] < 40.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 50.0)
         | (df["ROC_9_1d"] > -15.0)
+      )
+      # 15m & 1h down move, 15m still not low enough, 1h stil high, 4h high & overbought
+      & (
+        (df["RSI_3_15m"] > 25.0)
+        | (df["RSI_3_1h"] > 25.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 40.0)
+        | (df["RSI_14_4h"] < 70.0)
+        | (df["AROONU_14_1h"] < 40.0)
+        | (df["AROONU_14_4h"] < 85.0)
+        | (df["ROC_9_4h"] < 70.0)
+      )
+      # 15m & 1h down move, 15m still not low enough, 1h still high, 4h high, 4h overbought
+      & (
+        (df["RSI_3_15m"] > 25.0)
+        | (df["RSI_3_1h"] > 25.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 40.0)
+        | (df["RSI_14_4h"] < 80.0)
+        | (df["AROONU_14_1h"] < 50.0)
+        | (df["AROONU_14_4h"] < 100.0)
+        | (df["ROC_9_4h"] < 30.0)
       )
       # 15m & 1h & 4h & 1d down move, 15m & 1h still not low enough, 4h stil high, 15m still high, 1d overbought
       & (
