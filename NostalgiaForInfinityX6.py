6bbba6317009d067c529481f5d1533b41cf19ef8
iterativ <vangelis.dimou@tutanota.com>
2025-10-16 09:04:22 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.207"
+    return "v16.8.208"
 
   stoploss = -0.99
 
@@ -7057,20 +7057,6 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["ROC_9_1h"] > -20.0)
         | (df["ROC_9_4h"] > -30.0)
       )
-      # 15m & 1h & 4h & 1d down move, 15m & 1h & 4h still not low enough, 15m still high, 1h & 4h & 1d downtrend
-      & (
-        (df["RSI_3_15m"] > 30.0)
-        | (df["RSI_3_1h"] > 30.0)
-        | (df["RSI_3_4h"] > 30.0)
-        | (df["RSI_3_1d"] > 30.0)
-        | (df["RSI_14_15m"] < 25.0)
-        | (df["RSI_14_1h"] < 25.0)
-        | (df["RSI_14_4h"] < 25.0)
-        | (df["AROONU_14_15m"] < 40.0)
-        | (df["ROC_9_1h"] > -15.0)
-        | (df["ROC_9_4h"] > -15.0)
-        | (df["ROC_9_1d"] > -15.0)
-      )
       # 15m & 1h & 4h down move, 15m & 1h & 4h still not low enough, 1h downtrend
       & (
         (df["RSI_3_15m"] > 30.0)
@@ -7084,6 +7070,32 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1h"] < 30.0)
         | (df["STOCHRSIk_14_14_3_3_4h"] < 30.0)
         | (df["ROC_9_1h"] > -15.0)
+      )
+      # 15m & 1h & 4h down move, 15m & 1h still not low enough, 4h still high, 15m & 1h & 4h downtrend
+      & (
+        (df["RSI_3_15m"] > 30.0)
+        | (df["RSI_3_1h"] > 30.0)
+        | (df["RSI_3_4h"] > 30.0)
+        | (df["RSI_14_15m"] < 20.0)
+        | (df["RSI_14_1h"] < 20.0)
+        | (df["RSI_14_4h"] < 50.0)
+        | (df["CMF_20_15m"] > -0.35)
+        | (df["ROC_9_1h"] > -10.0)
+        | (df["ROC_9_4h"] > -15.0)
+      )
+      # 15m & 1h & 4h & 1d down move, 15m & 1h & 4h still not low enough, 15m still high, 1h & 4h & 1d downtrend
+      & (
+        (df["RSI_3_15m"] > 30.0)
+        | (df["RSI_3_1h"] > 30.0)
+        | (df["RSI_3_4h"] > 30.0)
+        | (df["RSI_3_1d"] > 30.0)
+        | (df["RSI_14_15m"] < 25.0)
+        | (df["RSI_14_1h"] < 25.0)
+        | (df["RSI_14_4h"] < 25.0)
+        | (df["AROONU_14_15m"] < 40.0)
+        | (df["ROC_9_1h"] > -15.0)
+        | (df["ROC_9_4h"] > -15.0)
+        | (df["ROC_9_1d"] > -15.0)
       )
       # 15m & 1h & 4h down move, 15m & 1h & 4h still not low enough, 1h & 4h downtrend, 4h downtrend
       & (
