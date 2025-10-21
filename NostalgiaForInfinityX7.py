b9c75ed0609b7e221727231f0be85cc6371b20e3
iterativ <vangelis.dimou@tutanota.com>
2025-10-22 01:30:22 +0300

    X7: add global protection entry (Long).

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.0.3"
+    return "v17.0.4"
 
   stoploss = -0.99
 
@@ -5782,6 +5782,13 @@ class NostalgiaForInfinityX7(IStrategy):
         | (df["AROONU_14_1h"] < 90.0)
         | (df["AROONU_14_4h"] < 100.0)
         | (df["ROC_9_1h"] < 15.0)
+      )
+      # 15m & 4h down move, 15m high, 4h downtrend
+      & (
+        (df["RSI_3_15m"] > 15.0)
+        | (df["RSI_3_4h"] > 3.0)
+        | (df["STOCHRSIk_14_14_3_3_15m"] < 70.0)
+        | (df["ROC_9_4h"] > -15.0)
       )
       # 15m & 4h down move, 15m & 1h & 4h still not low enough, 15m & 4h downtrend, 1h & 4h downtrend
       & (
