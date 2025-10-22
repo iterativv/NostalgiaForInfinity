ffb4753f392d47d0d48ef64150e3f8f04ff7f51b
iterativ <vangelis.dimou@tutanota.com>
2025-10-22 10:20:45 +0300

    X7: add global protection entry (Long).

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.0.4"
+    return "v17.0.5"
 
   stoploss = -0.99
 
@@ -4392,6 +4392,17 @@ class NostalgiaForInfinityX7(IStrategy):
         | (df["RSI_14_4h"] < 40.0)
         | (df["CMF_20_15m"] > -0.40)
         | (df["AROONU_14_1h"] < 50.0)
+      )
+      # 15m & 4h down move, 15m & 1h still not low enough, 4h still high, 1h & 4h still not low enough, 1d overbought
+      & (
+        (df["RSI_3_15m"] > 5.0)
+        | (df["RSI_3_4h"] > 20.0)
+        | (df["RSI_14_15m"] < 10.0)
+        | (df["RSI_14_1h"] < 30.0)
+        | (df["RSI_14_4h"] < 40.0)
+        | (df["AROONU_14_1h"] < 20.0)
+        | (df["AROONU_14_4h"] < 20.0)
+        | (df["ROC_9_1d"] < 20.0)
       )
       # 15m down move, 15m still not low enough, 1h & 4h high, 1h overbought
       & (
@@ -9344,7 +9355,7 @@ class NostalgiaForInfinityX7(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0)
         | (df["ROC_9_4h"] > -35.0)
       )
-      # 1d P^D, 15m & 4h down move, 15m & 4h still high
+      # 1d P&D, 15m & 4h down move, 15m & 4h still high
       & (
         (df["change_pct_1d"] > -20.0)
         | (df["change_pct_1d"].shift(288) < 20.0)
