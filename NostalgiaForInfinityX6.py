5687e8de5b733635331b1309eda4113e485ded56
iterativ <vangelis.dimou@tutanota.com>
2025-10-23 13:09:01 +0300

    X6: add global protection entry (Long).

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.280"
+    return "v16.8.281"
 
   stoploss = -0.99
 
@@ -5825,6 +5825,14 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_4h"] < 40.0)
         | (df["ROC_9_1d"] > -15.0)
       )
+      # 15m & 1h & 4h down move, 1h still not low enough, 1d overbought
+      & (
+        (df["RSI_3_15m"] > 20.0)
+        | (df["RSI_3_1h"] > 20.0)
+        | (df["RSI_3_4h"] > 30.0)
+        | (df["AROONU_14_1h"] < 25.0)
+        | (df["ROC_9_1d"] < 200.0)
+      )
       # 15m & 1h & 4h & 1d down move, 1h & 4h still not low enough, 1d still high, 1d downtrend, 1d overbought
       & (
         (df["RSI_3_15m"] > 20.0)
@@ -6171,6 +6179,15 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_15m"] < 20.0)
         | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0)
         | (df["ROC_9_4h"] > -20.0)
+      )
+      # 15m & 1h & 4h down move, 15m downtrend, 15m still high, 1h overbought
+      & (
+        (df["RSI_3_15m"] > 20.0)
+        | (df["RSI_3_1h"] > 40.0)
+        | (df["RSI_3_4h"] > 40.0)
+        | (df["CMF_20_15m"] > -0.20)
+        | (df["AROONU_14_15m"] < 50.0)
+        | (df["ROC_9_1d"] < 100.0)
       )
       # 15m & 1h & 4h down move, 15m & 1h & 4h still high, 4h downtrend
       & (
