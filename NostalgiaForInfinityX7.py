923a24d7512ce88df56fbaf032aaf1a702340bb9
iterativ <vangelis.dimou@tutanota.com>
2025-10-27 22:41:52 +0200

    X7: system v3_1: fine tune the rebuy thresholds.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.1.14"
+    return "v17.1.15"
 
   stoploss = -0.99
 
@@ -507,8 +507,8 @@ class NostalgiaForInfinityX7(IStrategy):
   system_v3_1_stake_multiplier = 0.35
   system_v3_1_rebuy_stakes_spot = [1.0, 1.0]
   system_v3_1_rebuy_stakes_futures = [1.0, 1.0]
-  system_v3_1_rebuy_thresholds_spot = [-0.08, -0.10, -0.12, -0.14]
-  system_v3_1_rebuy_thresholds_futures = [-0.08, -0.10, -0.12, -0.14]
+  system_v3_1_rebuy_thresholds_spot = [-0.09, -0.12, -0.16, -0.20]
+  system_v3_1_rebuy_thresholds_futures = [-0.09, -0.12, -0.16, -0.20]
 
   system_v3_derisk_level_1_enable = True
   system_v3_derisk_level_1_spot = [-0.04, -0.06]
