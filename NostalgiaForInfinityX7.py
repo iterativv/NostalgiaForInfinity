f5afc9806b332a31b59af1432b92c1386eaf76d7
iterativ <vangelis.dimou@tutanota.com>
2025-12-18 23:02:39 +0200

    X7: signal 3: fine tune.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.2.577"
+    return "v17.2.578"
 
   stoploss = -0.99
 
@@ -12811,10 +12811,10 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_1d"] > 10.0) | (df["ROC_9_4h"] > -20.0) | (df["ROC_9_1d"] > -60.0))
             # 1d down move, 4h & 1d downtrend
             & ((df["RSI_3_1d"] > 10.0) | (df["ROC_9_4h"] > -30.0) | (df["ROC_9_1d"] > -50.0))
-            # 1d down move, 15m high, 1h high
-            & ((df["RSI_3_1d"] > 15.0) | (df["AROONU_14_15m"] < 70.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 80.0))
             # 1d down move, 1h high, 1d downtrend
             & ((df["RSI_3_1d"] > 15.0) | (df["AROONU_14_1h"] < 85.0) | (df["ROC_9_1d"] > -30.0))
+            # 1d down move, 15m high, 1h high
+            & ((df["RSI_3_1d"] > 20.0) | (df["AROONU_14_15m"] < 60.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 80.0))
             # 1d down move, 15m high, 1h high
             & ((df["RSI_3_1d"] > 20.0) | (df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 100.0))
             # 1d down move, 4h high, 4h downtrend
