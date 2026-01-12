c8d80e3159d77f10fed03a6d4eff693a8c76b8a7
iterativ <vangelis.dimou@tutanota.com>
2026-01-12 14:17:10 +0200

    X7: signal 501: fine tune.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.141"
+    return "v17.3.142"
 
   stoploss = -0.99
 
@@ -19949,6 +19949,10 @@ class NostalgiaForInfinityX7(IStrategy):
           # 15m up move, 15m still not high enough, 1h still low
           short_entry_logic.append(
             (df["RSI_3_15m"] < 70.0) | (df["STOCHRSIk_14_14_3_3_15m"] > 90.0) | (df["STOCHRSIk_14_14_3_3_1h"] > 50.0)
+          )
+          # 1h & 4h up move, 1h still not high enough
+          short_entry_logic.append(
+            (df["RSI_3_1h"] < 95.0) | (df["RSI_3_4h"] < 95.0) | (df["STOCHRSIk_14_14_3_3_1h"] > 70.0)
           )
           # 1h up move, 4h low
           short_entry_logic.append((df["RSI_3_1h"] < 95.0) | (df["STOCHRSIk_14_14_3_3_4h"] > 30.0))
