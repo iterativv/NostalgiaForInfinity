a5015ff66d7f9a1975b29243149788960e45df96
iterativ <vangelis.dimou@tutanota.com>
2026-01-15 09:40:29 +0200

    X6: signal 162: fine tune.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.528"
+    return "v16.8.529"
 
   stoploss = -0.99
 
@@ -17953,12 +17953,20 @@ class NostalgiaForInfinityX6(IStrategy):
             & ((df["RSI_3_1h"] > 30.0) | (df["RSI_3_4h"] > 35.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 70.0))
             # 1h down move, 1h high, 4h overbought
             & ((df["RSI_3_1h"] > 30.0) | (df["AROONU_14_1h"] < 60.0) | (df["ROC_9_4h"] < 50.0))
+            # 1h down move, 15m still high, 1d overbought
+            & ((df["RSI_3_1h"] > 45.0) | (df["AROONU_14_15m"] < 50.0) | (df["ROC_9_1d"] < 50.0))
+            # 1h down move, 4h & 1d overbought
+            & ((df["RSI_3_1h"] > 45.0) | (df["ROC_9_4h"] < 10.0) | (df["ROC_9_1d"] < 50.0))
             # 1h down move,  4h high, 1d overbought
             & ((df["RSI_3_1h"] > 50.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0) | (df["ROC_9_1d"] < 150.0))
             # 4h & 1d down move
             & ((df["RSI_3_4h"] > 5.0) | (df["RSI_3_1d"] > 10.0))
             # 4h & 1d down move, 4h still not low enough
             & ((df["RSI_3_4h"] > 20.0) | (df["RSI_3_1d"] > 20.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 30.0))
+            # 4h down move, 15m still high, 1d overbought
+            & ((df["RSI_3_4h"] > 40.0) | (df["AROONU_14_15m"] < 50.0) | (df["ROC_9_1d"] < 50.0))
+            # 4h down move, 4h & 1d overbought
+            & ((df["RSI_3_4h"] > 40.0) | (df["ROC_9_4h"] < 10.0) | (df["ROC_9_1d"] < 50.0))
             # 1d down move, 1h high
             & ((df["RSI_3_1d"] > 3.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 70.0))
             # 1d down move, 15m still high
