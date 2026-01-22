7d9371c96f16e934a3005785bd24a6cfb2c20d70
iterativ <vangelis.dimou@tutanota.com>
2026-01-22 10:38:36 +0200

    X7: signal 1: fine tune.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.320"
+    return "v17.3.321"
 
   stoploss = -0.99
 
@@ -12135,6 +12135,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_1h"] > 10.0) | (df["RSI_14_4h"] < 75.0))
             # 1h down move, 15m downtrend, 1d high
             & ((df["RSI_3_1h"] > 10.0) | (df["CMF_20_15m"] > -0.40) | (df["AROONU_14_1d"] < 70.0))
+            # 1h down move, 1h high, 4h overbought
+            & ((df["RSI_3_1h"] > 10.0) | (df["AROONU_14_1h"] < 70.0) | (df["ROC_9_4h"] < 10.0))
             # 1h down move, 4h overbought
             & ((df["RSI_3_1h"] > 10.0) | (df["ROC_9_4h"] < 50.0))
             # 1h down move, 1h high, 1d overbought
