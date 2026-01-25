a680ce0981c118cd28dfa38ec1a8111217d75aac
iterativ <vangelis.dimou@tutanota.com>
2026-01-25 11:44:06 +0200

    X6: signal 103: fine tune.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.577"
+    return "v16.8.578"
 
   stoploss = -0.99
 
@@ -16894,14 +16894,16 @@ class NostalgiaForInfinityX6(IStrategy):
           )
           # 15m still high, 1d high
           long_entry_logic.append((df["AROONU_14_15m"] < 50.0) | (df["STOCHRSIk_14_14_3_3_1d"] < 90.0))
-          # 4h high, 4h & 1d overbought
-          long_entry_logic.append((df["AROONU_14_4h"] < 80.0) | (df["ROC_9_4h"] < 80.0) | (df["ROC_9_1d"] < 100.0))
+          # 15m & 1h
+          long_entry_logic.append((df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 90.0))
           # 15m & 1h & 4h high
           long_entry_logic.append(
             (df["AROONU_14_15m"] < 70.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 70.0) | (df["AROONU_14_4h"] < 90.0)
           )
-          # 15m & 1h
-          long_entry_logic.append((df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 90.0))
+          # 4h high, 4h & 1d overbought
+          long_entry_logic.append((df["AROONU_14_4h"] < 80.0) | (df["ROC_9_4h"] < 80.0) | (df["ROC_9_1d"] < 100.0))
+          # 1d high, 4h & 1d overbought
+          long_entry_logic.append((df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 50.0) | (df["ROC_9_1d"] < 100.0))
           # 15m still high, 1h high
           long_entry_logic.append(
             (df["STOCHRSIk_14_14_3_3_15m"] < 40.0)
