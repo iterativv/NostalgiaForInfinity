8f73f9053963bb99ae4004ccec5e6fa826c3d49d
iterativ <vangelis.dimou@tutanota.com>
2026-01-30 09:18:23 +0200

    X6: signal 163: add protection.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.601"
+    return "v16.8.602"
 
   stoploss = -0.99
 
@@ -18440,6 +18440,8 @@ class NostalgiaForInfinityX6(IStrategy):
             & ((df["RSI_3_4h"] > 20.0) | (df["AROONU_14_15m"] < 40.0) | (df["ROC_9_1d"] < 20.0))
             # 4h & 1d down move, 1h & 4h low
             & ((df["RSI_3_4h"] > 25.0) | (df["RSI_3_1d"] > 25.0) | (df["CMF_20_1h"] > -0.3) | (df["CMF_20_4h"] > -0.4))
+            # 4h down move, 1d high & overbought
+            & ((df["RSI_3_4h"] > 25.0) | (df["STOCHRSIk_14_14_3_3_1d"] < 80.0) | (df["ROC_9_1d"] < 25.0))
             # 4h down move, 4h & 1d high
             & ((df["RSI_3_4h"] > 35.0) | (df["AROONU_14_4h"] < 70.0) | (df["AROONU_14_1d"] < 100.0))
             # 4h down move, 15m still high, 1d overbought
