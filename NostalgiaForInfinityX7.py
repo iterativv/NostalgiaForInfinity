72d7e104c956b643a79ab846c44d40eeaadffbd6
iterativ <vangelis.dimou@tutanota.com>
2026-02-03 16:08:30 +0200

    X7: signal 161: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.628"
+    return "v17.3.629"
 
   stoploss = -0.99
 
@@ -20425,6 +20425,8 @@ class NostalgiaForInfinityX7(IStrategy):
           long_entry_logic.append(
             (df["RSI_3_4h"] > 50.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0) | (df["AROONU_14_4h"] < 70.0)
           )
+          # 4h down move, 1d high & overbought
+          long_entry_logic.append((df["RSI_3_4h"] > 50.0) | (df["AROONU_14_1d"] < 90.0) | (df["ROC_9_1d"] < 20.0))
           # 4h down move, 15m & 4h high
           long_entry_logic.append((df["RSI_3_4h"] > 60.0) | (df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_4h"] < 70.0))
           # 4h down move, 15m high, 4h still high
