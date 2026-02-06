6726a093090145824bf31c601b18c11336ceeda7
iterativ <vangelis.dimou@tutanota.com>
2026-02-06 12:48:09 +0200

    X7: signal 63: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.735"
+    return "v17.3.736"
 
   stoploss = -0.99
 
@@ -17704,6 +17704,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_1h"] > 55.0) | (df["AROONU_14_1h"] < 90.0) | (df["ROC_9_1h"] < 10.0))
             # 4h down move, 1h still not low enough, 4h downtrend
             & ((df["RSI_3_4h"] > 3.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 20.0) | (df["ROC_9_4h"] > -30.0))
+            # 4h down move, 1d high, 4h downtrend
+            & ((df["RSI_3_4h"] > 5.0) | (df["AROONU_14_1d"] < 80.0) | (df["ROC_9_4h"] > -10.0))
             # 4h down move, 1h still high
             & ((df["RSI_3_4h"] > 5.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 50.0))
             # 4h down move, 4h & 1d downtrend
