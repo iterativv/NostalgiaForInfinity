4e642eff73e3d62acf6bc6f66dcd671ba92008fe
iterativ <vangelis.dimou@tutanota.com>
2026-02-15 10:06:12 +0200

    X6: signal 102: add protection.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.681"
+    return "v16.8.682"
 
   stoploss = -0.99
 
@@ -16764,6 +16764,8 @@ class NostalgiaForInfinityX6(IStrategy):
           long_entry_logic.append(
             (df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 80.0) | (df["AROONU_14_4h"] < 90.0)
           )
+          # 15m & 1h high, 4h overbought
+          long_entry_logic.append((df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 90.0) | (df["ROC_9_4h"] < 20.0))
           # 15m & 1d high, 1d overbought
           long_entry_logic.append(
             (df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1d"] < 90.0) | (df["ROC_9_1d"] < 100.0)
@@ -16782,6 +16784,10 @@ class NostalgiaForInfinityX6(IStrategy):
           long_entry_logic.append((df["AROONU_14_1h"] < 50.0) | (df["AROONU_14_4h"] < 90.0) | (df["ROC_9_4h"] < 80.0))
           # 1h & 4h high, 1h overbought
           long_entry_logic.append((df["AROONU_14_1h"] < 80.0) | (df["AROONU_14_4h"] < 100.0) | (df["ROC_9_1h"] < 40.0))
+          # 1h & 1d high, 1d overbought
+          long_entry_logic.append((df["AROONU_14_1h"] < 80.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_1d"] < 30.0))
+          # 1h high, 4h & 1d overbought
+          long_entry_logic.append((df["AROONU_14_1h"] < 90.0) | (df["ROC_9_4h"] < 20.0) | (df["ROC_9_1d"] < 40.0))
           # 4h & 1d high, 4h overbought
           long_entry_logic.append((df["AROONU_14_4h"] < 80.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 30.0))
           # 4h & 1d high, 1d overbought
@@ -16791,7 +16797,7 @@ class NostalgiaForInfinityX6(IStrategy):
             (df["AROONU_14_4h"] < 90.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 85.0) | (df["ROC_9_4h"] < 50.0)
           )
           # 1d high, 4h & 1d overbought
-          long_entry_logic.append((df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 50.0) | (df["ROC_9_1d"] < 100.0))
+          long_entry_logic.append((df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 20.0) | (df["ROC_9_1d"] < 40.0))
           # 4h high, 4h & 1d overbought
           long_entry_logic.append(
             (df["STOCHRSIk_14_14_3_3_4h"] < 70.0) | (df["ROC_9_4h"] < 30.0) | (df["ROC_9_1d"] < 30.0)
