2e84b98c75e98fede441dda1abce1c6d4167eb86
iterativ <vangelis.dimou@tutanota.com>
2026-02-27 17:14:35 +0200

    X6: signal 101: add protection.

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.701"
+    return "v16.8.702"
 
   stoploss = -0.99
 
@@ -16183,12 +16183,12 @@ class NostalgiaForInfinityX6(IStrategy):
           long_entry_logic.append(
             (df["RSI_3_15m"] > 15.0) | (df["AROONU_14_15m"] < 25.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 80.0)
           )
+          # 15m down move, 4h high & overbought
+          long_entry_logic.append((df["RSI_3_15m"] > 15.0) | (df["AROONU_14_4h"] < 80.0) | (df["ROC_9_4h"] < 40.0))
           # 15m down move, 1h & 4h high
           long_entry_logic.append(
             (df["RSI_3_15m"] > 15.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 70.0) | (df["AROONU_14_4h"] < 85.0)
           )
-          # 15m down move, 4h high & overbought
-          long_entry_logic.append((df["RSI_3_15m"] > 15.0) | (df["AROONU_14_4h"] < 80.0) | (df["ROC_9_4h"] < 40.0))
           # 15m & 1h down move, 4h high
           long_entry_logic.append(
             (df["RSI_3_15m"] > 20.0) | (df["RSI_3_1h"] > 30.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0)
@@ -16318,6 +16318,10 @@ class NostalgiaForInfinityX6(IStrategy):
           # 1d down move, 1h high & overbought
           long_entry_logic.append(
             (df["RSI_3_1d"] > 35.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 90.0) | (df["ROC_9_1h"] < 10.0)
+          )
+          # 1d down move, 4h high, 1d overbought
+          long_entry_logic.append(
+            (df["RSI_3_1d"] > 45.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0) | (df["ROC_9_1d"] < 10.0)
           )
           # 4h still high, 4h moving lower, 4h overbought
           long_entry_logic.append(
