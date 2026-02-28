231f8cb27fc8e93cd9b8a18d15189199cbd353b4
iterativ <vangelis.dimou@tutanota.com>
2026-02-28 10:48:04 +0200

    X7: signal 142: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.915"
+    return "v17.3.916"
 
   stoploss = -0.99
 
@@ -20437,26 +20437,32 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_4h"] < 90.0) | (df["AROONU_14_1d"] < 100.0))
             # 15m high, 1d high & overbought
             & ((df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_1d"] < 20.0))
+            # 15m high, 1h & 4h overbought
+            & ((df["AROONU_14_15m"] < 70.0) | (df["ROC_9_1h"] < 10.0) | (df["ROC_9_4h"] < 10.0))
             # 15m high, 4h overbought, 1d downtrend
             & ((df["AROONU_14_15m"] < 80.0) | (df["ROC_9_4h"] < 20.0) | (df["ROC_9_1d"] > -30.0))
-            # 15m & 1h high, 1d overbought
-            & ((df["AROONU_14_15m"] < 100.0) | (df["AROONU_14_1h"] < 100.0) | (df["ROC_9_1d"] < 50.0))
+            # 15m & 1h high
+            & ((df["AROONU_14_15m"] < 90.0) | (df["AROONU_14_1h"] < 100.0))
+            # 15m & 4h high
+            & ((df["AROONU_14_15m"] < 90.0) | (df["AROONU_14_4h"] < 100.0))
             # 1h & 1d high, 1d overbought
             & ((df["AROONU_14_1h"] < 100.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_1d"] < 10.0))
+            # 1h & 4h high, 1h overbought
+            & ((df["AROONU_14_1h"] < 100.0) | (df["AROONU_14_4h"] < 100.0) | (df["ROC_9_1h"] < 10.0))
             # 1h & 4h high, 4h overbought
-            & ((df["AROONU_14_1h"] < 100.0) | (df["AROONU_14_4h"] < 100.0) | (df["ROC_9_4h"] < 50.0))
+            & ((df["AROONU_14_1h"] < 100.0) | (df["AROONU_14_4h"] < 100.0) | (df["ROC_9_4h"] < 10.0))
             # 4h & 1d high, 4h overbought
             & ((df["AROONU_14_4h"] < 100.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 40.0))
             # 4h & 1d high, 1d overbought
             & ((df["AROONU_14_4h"] < 100.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_1d"] < 60.0))
             # 4h high, 1h & 4h overbought
             & ((df["AROONU_14_4h"] < 100.0) | (df["ROC_9_1h"] < 20.0) | (df["ROC_9_4h"] < 40.0))
-            # 4h high, 4h overbought, 1d downtrend
-            & ((df["STOCHRSIk_14_14_3_3_4h"] < 90.0) | (df["ROC_9_4h"] < 20.0) | (df["ROC_9_1d"] > -20.0))
             # 1d high, 4h & 1d overbought
             & ((df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 60.0) | (df["ROC_9_1d"] < 100.0))
             # 1h high, 1h & 1d overbought
             & ((df["STOCHRSIk_14_14_3_3_1h"] < 80.0) | (df["ROC_9_1h"] < 20.0) | (df["ROC_9_1d"] < 100.0))
+            # 4h high, 4h overbought, 1d downtrend
+            & ((df["STOCHRSIk_14_14_3_3_4h"] < 90.0) | (df["ROC_9_4h"] < 20.0) | (df["ROC_9_1d"] > -20.0))
             # 1d high, 1h & 4h overbought
             & ((df["STOCHRSIk_14_14_3_3_1d"] < 80.0) | (df["ROC_9_1h"] < 20.0) | (df["ROC_9_4h"] < 20.0))
           )
