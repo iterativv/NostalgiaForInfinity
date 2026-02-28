e892eb7c19f051736ea2bd7bd709179cc1e3b491
iterativ <vangelis.dimou@tutanota.com>
2026-02-28 15:28:20 +0200

    X7: signal 3: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.919"
+    return "v17.3.920"
 
   stoploss = -0.99
 
@@ -13445,6 +13445,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["AROONU_14_15m"] < 60.0) | (df["AROONU_14_4h"] < 70.0) | (df["ROC_9_1d"] < 10.0))
             # 15m high, 15m & 4h overbought
             & ((df["AROONU_14_15m"] < 60.0) | (df["ROC_9_15m"] < 10.0) | (df["ROC_9_4h"] < 20.0))
+            # 15m high, 4h downtrend
+            & ((df["AROONU_14_15m"] < 60.0) | (df["ROC_9_4h"] > -30.0))
             # 15m & 1h & 4h high
             & ((df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 100.0) | (df["AROONU_14_4h"] < 100.0))
             # 15m & 4h & 1d high
@@ -13471,6 +13473,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["AROONU_14_1d"] < 80.0) | (df["ROC_9_4h"] > -20.0) | (df["ROC_9_1d"] > -20.0))
             # 1d high, 1h & 4h downtrend
             & ((df["AROONU_14_1d"] < 85.0) | (df["ROC_9_1h"] > -30.0) | (df["ROC_9_4h"] > -50.0))
+            # 1d high, 4h downtrend
+            & ((df["AROONU_14_1d"] < 85.0) | (df["ROC_9_4h"] > -30.0))
             # 1d high, 4h downtrend, 1d overbought
             & ((df["AROONU_14_1d"] < 85.0) | (df["ROC_9_4h"] > -30.0) | (df["ROC_9_1d"] < 70.0))
             # 1d high, 4h & 1d overbought
@@ -13483,6 +13487,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["STOCHRSIk_14_14_3_3_15m"] < 90.0) | (df["STOCHRSIk_14_14_3_3_1d"] < 90.0))
             # 1h high, 4h & 1d downtrend
             & ((df["STOCHRSIk_14_14_3_3_1h"] < 60.0) | (df["ROC_9_4h"] > -30.0) | (df["ROC_9_1d"] > -30.0))
+            # 1h high, 4h downtrend
+            & ((df["STOCHRSIk_14_14_3_3_1h"] < 70.0) | (df["ROC_9_4h"] > -30.0))
             # 1h high, 1d downtrend
             & ((df["STOCHRSIk_14_14_3_3_1h"] < 70.0) | (df["ROC_9_1d"] > -20.0))
             # 4h high, 4h overbought, 1d downtrend
