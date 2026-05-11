02c39c493d56b3c07d0b351f6174aad79e86c6fe
iterativ <vangelis.dimou@tutanota.com>
2026-05-11 20:52:12 +0300

    X7: system_v3_2: fine tune the grind entries.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.68"
+    return "v17.4.69"
 
   stoploss = -0.99
 
@@ -47447,8 +47447,8 @@ class NostalgiaForInfinityX7(IStrategy):
         and (last_candle["RSI_3_15m"] > 10.0)
         and (last_candle["RSI_3_1h"] > 15.0)
         and (last_candle["RSI_3_4h"] > 15.0)
-        and (last_candle["RSI_14"] < 46.0)
-        and (last_candle["AROONU_14"] < 25.0)
+        and (last_candle["RSI_14"] < 45.0)
+        and (last_candle["AROONU_14"] < 20.0)
         and (last_candle["close"] > (last_candle["close_max_48"] * 0.90))
         and (last_candle["close"] < (last_candle["low_min_24_4h"] * 1.60))
         and (last_candle["close"] < (last_candle["EMA_16"] * 0.980))
