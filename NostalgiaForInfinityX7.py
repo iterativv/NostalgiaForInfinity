5890f16f752dc2d603068b487817f1332035e799
iterativ <vangelis.dimou@tutanota.com>
2026-06-04 22:07:35 +0300

    X7: signal 142: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -21513,6 +21513,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & (rsi_3_15m_gt_3 | (stochrsi_k_1h < 60.0) | (roc_9_1d_lt_20))
             # 15m down move, 4h high
             & (rsi_3_15m_gt_3 | (stochrsi_k_4h_lt_80))
+            # 15m & 1d down move, 1d still high
+            & ((rsi_3_15m_gt_5) | (rsi_3_1h_gt_5) | (aroonu_14_1d < 40.0))
             # 15m & 1h down move, 15m still not low enough
             & ((rsi_3_15m_gt_5) | (rsi_3_1h_gt_5) | (stochrsi_k_15m < 30.0))
             # 15m & 1h down move, 4h still high
@@ -21599,6 +21601,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((rsi_3_15m_gt_15) | rsi_3_4h_gt_20 | roc_9_1d_lt_50)
             # 15m & 4h down move, 1h high
             & ((rsi_3_15m_gt_15) | (rsi_3_4h_gt_35) | (aroonu_14_1h_lt_90))
+            # 15m & 4h down move, 4h overbought
+            & ((rsi_3_15m_gt_15) | (rsi_3_4h > 45.0) | (roc_9_4h_lt_20))
             # 15m & 1d down move, 4h high
             & ((rsi_3_15m_gt_15) | (rsi_3_1d_gt_20) | (stochrsi_k_4h_lt_80))
             # 15m down move, 15m downtrend, 1h high
