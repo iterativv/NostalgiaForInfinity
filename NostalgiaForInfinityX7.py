c5277fa728ff28f3974d94460ab3fb96adcfb481
iterativ <vangelis.dimou@tutanota.com>
2026-06-06 21:28:27 +0300

    X7: signal 144: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.154"
+    return "v17.4.155"
 
   stoploss = -0.99
 
@@ -22186,6 +22186,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((rsi_3_15m_gt_3) | (rsi_3_1h_gt_10) | (rsi_3_4h_gt_20) | (aroonu_14_15m < 20.0) | (aroonu_14_4h < 20.0))
             # 15m & 1h down move, 1d high
             & ((rsi_3_15m_gt_5) | (rsi_3_1h_gt_5) | (aroonu_14_1d_lt_70))
+            # 15m & 1h & 4h & 1d down move, 1h still not low enough
+            & ((rsi_3_15m_gt_5) | (rsi_3_1h_gt_10) | (rsi_3_4h_gt_30) | (rsi_3_1d > 30.0) | (aroonu_14_1h < 30.0))
             # 15m & 1h down move, 1d high
             & ((rsi_3_15m_gt_5) | (rsi_3_1h_gt_10) | (aroonu_14_1d < 100.0))
             # 15m & 1h down move, 1d overbought
@@ -22195,17 +22197,17 @@ class NostalgiaForInfinityX7(IStrategy):
             # 15m & 4h down move, 4h still high
             & ((rsi_3_15m_gt_5) | (rsi_3_4h_gt_30) | (stochrsi_k_4h < 40.0))
             # 15m down move, 1d high & overbought
-            & ((rsi_3_15m_gt_5) | aroonu_14_1d_lt_100 | (roc_9_1d_lt_40))
+            & ((rsi_3_15m_gt_5) | (aroonu_14_1d_lt_100) | (roc_9_1d_lt_40))
             # 15m & 4h down move, 4h still not low enough
             & ((rsi_3_15m_gt_10) | (rsi_3_4h_gt_10) | (rsi_14_4h < 20.0))
             # 15m & 4h down move, 4h high
-            & ((rsi_3_15m_gt_10) | (rsi_3_4h > 45.0) | aroonu_14_4h_lt_70)
+            & ((rsi_3_15m_gt_10) | (rsi_3_4h > 45.0) | (aroonu_14_4h_lt_70))
             # 15m down move, 4h high, 1d overbought
-            & ((rsi_3_15m_gt_10) | aroonu_14_4h_lt_80 | roc_9_1d_lt_50)
+            & ((rsi_3_15m_gt_10) | (aroonu_14_4h_lt_80) | (roc_9_1d_lt_50))
             # 15m & 1h down move, 15m still high
             & ((rsi_3_15m_gt_15) | (rsi_3_1h_gt_15) | (stochrsi_k_15m < 50.0))
             # 15m & 1h down move, 1d overbought
-            & ((rsi_3_15m_gt_15) | rsi_3_1h_gt_20 | (roc_9_1d_lt_30))
+            & ((rsi_3_15m_gt_15) | (rsi_3_1h_gt_20) | (roc_9_1d_lt_30))
             # 15m & 1h down move, 4h high
             & ((rsi_3_15m_gt_15) | (rsi_3_1h_gt_35) | aroonu_14_4h_lt_100)
             # 15m & 4h down move, 15m still high
