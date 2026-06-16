da88f667f1bb39baf258cd165e6c17a9c3062fc5
iterativ <vangelis.dimou@tutanota.com>
2026-06-16 15:11:41 +0300

    X7: signal 6: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.240"
+    return "v17.4.241"
 
   stoploss = -0.99
 
@@ -16303,10 +16303,12 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((rsi_3_15m_gt_20) | (rsi_3_4h_gt_20) | (rsi_3_1d_gt_20))
             # 15m & 4h down move, 1d high
             & ((rsi_3_15m_gt_20) | (rsi_3_4h_gt_20) | (stochrsi_k_1d_lt_90))
+            # 15m & 4h down move, 4h still high, 1d downtrend
+            & ((rsi_3_15m_gt_20) | (rsi_3_4h_gt_30) | (stochrsi_k_4h_lt_50) | (roc_9_1d_gt_neg_20))
             # 15m & 4h down move, 1d high
             & ((rsi_3_15m_gt_20) | (rsi_3_4h_gt_45) | (stochrsi_k_1d_lt_90))
             # 15m & 1d down move, 1h high
-            & ((rsi_3_15m_gt_20) | (rsi_3_1d_gt_20) | aroonu_14_1h_lt_70)
+            & ((rsi_3_15m_gt_20) | (rsi_3_1d_gt_20) | (aroonu_14_1h_lt_70))
             # 15m & 1d down move, 15m still high
             & ((rsi_3_15m_gt_20) | (rsi_3_1d_gt_30) | (stochrsi_k_15m_lt_50))
             # 15m down move, 15m still not low enough, 1h high
