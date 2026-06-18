959356abda7a2b39d1cae515c6e14be8b4ab3d56
iterativ <vangelis.dimou@tutanota.com>
2026-06-18 15:28:40 +0300

    X7: signal 120: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.254"
+    return "v17.4.255"
 
   stoploss = -0.99
 
@@ -21973,8 +21973,8 @@ class NostalgiaForInfinityX7(IStrategy):
           long_entry_logic.append(is_pair_long_grind_mode)
           long_entry_logic.append(rsi_3 <= 50.0)
           long_entry_logic.append(rsi_3_15m >= 20.0)
-          long_entry_logic.append(rsi_3_1h >= 10.0)
-          long_entry_logic.append(rsi_3_4h >= 10.0)
+          long_entry_logic.append(rsi_3_1h >= 20.0)
+          long_entry_logic.append(rsi_3_4h >= 20.0)
           long_entry_logic.append(rsi_14_1h_lt_80)
           long_entry_logic.append(rsi_14_4h_lt_80)
           long_entry_logic.append(rsi_14_1d_lt_80)
@@ -21982,6 +21982,9 @@ class NostalgiaForInfinityX7(IStrategy):
           long_entry_logic.append(aroonu_14_1h_lt_100)
           long_entry_logic.append(aroonu_14_4h_lt_100)
           long_entry_logic.append(aroonu_14_1d_lt_100)
+          long_entry_logic.append(stochrsi_k_15m_lt_50)
+          long_entry_logic.append(stochrsi_k_1h_lt_70)
+          long_entry_logic.append(stochrsi_k_4h_lt_70)
           long_entry_logic.append(stochrsi_k_1d_lt_90)
 
           # Logic
