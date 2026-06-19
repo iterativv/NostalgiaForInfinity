e852097c3e4c416ec40f7a68fd29fe0fcc342fa6
iterativ <vangelis.dimou@tutanota.com>
2026-06-19 08:05:50 +0300

    X7: signal 102: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.256"
+    return "v17.4.257"
 
   stoploss = -0.99
 
@@ -21426,9 +21426,9 @@ class NostalgiaForInfinityX7(IStrategy):
             # 4h down move, 15m still high, 1d downtrend
             & ((rsi_3_4h_gt_15) | (stochrsi_k_15m_lt_50) | (roc_9_1d_gt_neg_20))
             # 4h & 1d down move, 15m still not low enough
-            & (rsi_3_4h_gt_20 | (rsi_3_1d_gt_20) | (stochrsi_k_15m_lt_30))
+            & ((rsi_3_4h_gt_20) | (rsi_3_1d_gt_20) | (stochrsi_k_15m_lt_30))
             # 4h down move, 4h still not low enough, 1d downtrend
-            & (rsi_3_4h_gt_20 | (stochrsi_k_4h_lt_30) | (roc_9_1d_gt_neg_20))
+            & ((rsi_3_4h_gt_20) | (stochrsi_k_4h_lt_30) | (roc_9_1d_gt_neg_20))
             # 4h down move, 1d high, 4h downtrend
             & ((rsi_3_4h_gt_30) | (stochrsi_k_1d_lt_90) | (roc_9_4h_gt_neg_20))
             # 4h & 1d down move, 4h still high, 1d high
@@ -21436,7 +21436,9 @@ class NostalgiaForInfinityX7(IStrategy):
             # 4h down move, 4h high, 1d downtrend
             & ((rsi_3_4h_gt_35) | (stochrsi_k_4h_lt_70) | (roc_9_1d_gt_neg_30))
             # 4h & 1d down move, 1d high
-            & ((rsi_3_4h_gt_40) | (rsi_3_1d_gt_60) | aroonu_14_1d_lt_100)
+            & ((rsi_3_4h_gt_40) | (rsi_3_1d_gt_60) | (aroonu_14_1d_lt_100))
+            # 4h down move, 4h high, 1d downtrend
+            & ((rsi_3_4h_gt_45) | (stochrsi_k_4h_lt_70) | (roc_9_1d_gt_neg_30))
             # 4h down move, 4h high, 1d downtrend
             & ((rsi_3_4h_gt_60) | (stochrsi_k_4h_lt_80) | (roc_9_1d_gt_neg_20))
             # 4h down move, 15m high, 4h overbought
