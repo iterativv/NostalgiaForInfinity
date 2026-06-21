4959523f31bf1f8a905a18136a7d31bd6a394662
iterativ <vangelis.dimou@tutanota.com>
2026-06-21 14:14:21 +0300

    X7: signal 42: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -17787,6 +17787,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((rsi_3_15m_gt_25) | (stochrsi_k_4h_lt_80) | (roc_9_1d_gt_neg_40))
             # 15m down move, 4h high, 1d downtrend
             & ((rsi_3_15m_gt_25) | (stochrsi_k_4h_lt_90) | (roc_9_1d_gt_neg_10))
+            # 15m & 4h down move, 4h high, 1d downtrend
+            & ((rsi_3_15m_gt_30) | (rsi_3_4h_gt_35) | (stochrsi_k_4h_lt_60) | (roc_9_1d_gt_neg_30))
             # 15m down move, 15m still high, 4h high
             & ((rsi_3_15m_gt_35) | (aroonu_14_15m_lt_50) | (aroonu_14_4h_lt_80))
             # 1h & 4h down move
@@ -18002,9 +18004,9 @@ class NostalgiaForInfinityX7(IStrategy):
             # 1h down move, 1h still high, 1d high
             & ((rsi_3_1h_gt_35) | (aroonu_14_1h_lt_50) | (aroonu_14_1d_lt_90))
             # 1h down move, 1h & 4h high
-            & ((rsi_3_1h_gt_35) | aroonu_14_1h_lt_70 | aroonu_14_4h_lt_100)
+            & ((rsi_3_1h_gt_35) | (aroonu_14_1h_lt_70) | aroonu_14_4h_lt_100)
             # 1h down move, 1h & 1d high
-            & ((rsi_3_1h_gt_35) | aroonu_14_1h_lt_70 | aroonu_14_1d_lt_100)
+            & ((rsi_3_1h_gt_35) | (aroonu_14_1h_lt_70) | aroonu_14_1d_lt_100)
             # 1h down move, 4h high, 1d downtrend
             & ((rsi_3_1h_gt_35) | (aroonu_14_4h_lt_90) | (roc_9_1d_gt_neg_20))
             # 1h down move, 4h high & overbought
