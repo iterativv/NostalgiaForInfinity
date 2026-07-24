2bc3058ed4f8480ed7498efca49b5195c7b47e9b
iterativ <vangelis.dimou@tutanota.com>
2026-07-24 17:06:14 +0300

    X7: system_v3_2: fine tune the grind entries.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.434"
+    return "v17.4.435"
 
   stoploss = -0.99
 
@@ -46141,7 +46141,7 @@ class NostalgiaForInfinityX7(IStrategy):
       and (
         is_long_grind_entry
         or (
-          (slice_profit_entry < -0.04)
+          (slice_profit_entry < -0.06)
           and (last_candle["RSI_3"] > 5.0)
           and (last_candle["RSI_3_15m"] > 10.0)
           and (last_candle["RSI_14"] < 35.0)
@@ -47035,13 +47035,16 @@ class NostalgiaForInfinityX7(IStrategy):
       and ((last_aroonu_14_15m < 100.0) or (last_aroonu_14_1d < 100.0))
       and ((last_aroonu_14_15m < 100.0) or (last_stochrsi_k_15m < 90.0) or (last_stochrsi_k_1d < 80.0))
       and ((last_aroonu_14_15m < 100.0) or (last_stochrsi_k_1h < 80.0))
+      and ((last_aroonu_14_15m < 100.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_aroonu_14_1h < 100.0) or (last_aroonu_14_4h < 100.0))
-      and ((last_aroonu_14_4h < 100.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_aroonu_14_4h < 70.0) or (last_roc_9_1d < 50.0))
+      and ((last_aroonu_14_4h < 100.0) or (last_stochrsi_k_1h < 90.0))
+      and ((last_aroonu_14_4h < 100.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_aroonu_14_1d < 100.0) or (last_stochrsi_k_15m < 90.0))
       and ((last_stochrsi_k_15m < 80.0) or (last_stochrsi_k_1h < 90.0))
       and ((last_stochrsi_k_15m < 90.0) or (last_aroonu_14_1h < 100.0))
       and ((last_stochrsi_k_15m < 90.0) or (last_stochrsi_k_4h < 90.0))
+      and ((last_stochrsi_k_1h < 90.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_stochrsi_k_1h < 90.0) or (last_stochrsi_k_4h < 90.0))
     ):
       self._grind_entry_tag = "g23"
