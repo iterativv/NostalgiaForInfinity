8f21932a7e595d12909bf24aa0db6f8c298b4d6b
iterativ <vangelis.dimou@tutanota.com>
2026-07-27 11:20:15 +0300

    X7: system_v3_2: fine tune the grind entries.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.457"
+    return "v17.4.458"
 
   stoploss = -0.99
 
@@ -47057,6 +47057,7 @@ class NostalgiaForInfinityX7(IStrategy):
       and ((last_rsi_3_1d > 45.0) or (last_stochrsi_k_1h < 90.0))
       and ((last_rsi_3_1d > 45.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_aroonu_14_15m < 80.0) or (last_stochrsi_k_15m < 80.0))
+      and ((last_aroonu_14_15m < 100.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_roc_9_1d > -20.0) or (last_aroonu_14_4h < 100.0))
       and ((last_roc_9_1d > -20.0) or (last_stochrsi_k_4h < 90.0))
       and ((last_roc_9_1d > -30.0) or (last_stochrsi_k_1h < 90.0))
@@ -47146,6 +47147,7 @@ class NostalgiaForInfinityX7(IStrategy):
       and ((last_aroonu_14_1d < 100.0) or (last_stochrsi_k_4h < 90.0))
       # do not buy an overbought top on the bounce
       and ((last_stochrsi_k < 90.0) or (last_rsi_14 < 70.0))
+      and ((last_stochrsi_k_15m < 90.0) or (last_stochrsi_k_1h < 90.0))
       and ((last_stochrsi_k_15m < 90.0) or (last_stochrsi_k_1d < 90.0))
       and ((last_stochrsi_k_1h < 90.0) or (last_roc_9_4h < 15.0))
       and ((last_stochrsi_k_4h < 90.0) or (last_roc_9_4h < 15.0))
