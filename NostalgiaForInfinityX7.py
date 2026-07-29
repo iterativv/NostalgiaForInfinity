c9fae730d4c8f8be30e3fc003ee380ece99b42fa
iterativ <vangelis.dimou@tutanota.com>
2026-07-29 08:52:44 +0300

    X7: signal 43: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -71,7 +71,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.4.473"
+    return "v17.4.474"
 
   stoploss = -0.99
 
@@ -18734,15 +18734,17 @@ class NostalgiaForInfinityX7(IStrategy):
             # 1h down move, 4h high & overbought
             & ((rsi_3_1h_gt_15) | (aroonu_14_4h_lt_80) | (roc_9_4h_lt_10))
             # 1h & 4h down move, 1h still high
-            & (rsi_3_1h_gt_20 | (rsi_3_4h_gt_30) | (stochrsi_k_1h_lt_50))
+            & ((rsi_3_1h_gt_20) | (rsi_3_4h_gt_30) | (stochrsi_k_1h_lt_50))
+            # 1h & 4h & 1d  down move, 1d overbought
+            & ((rsi_3_1h_gt_20) | (rsi_3_4h_gt_50) | (rsi_3_1d_gt_50) | (roc_9_1d_lt_40))
             # 1h & 4h down move, 4h high
-            & (rsi_3_1h_gt_20 | (rsi_3_4h_gt_50) | (aroonu_14_4h_lt_75))
+            & ((rsi_3_1h_gt_20) | (rsi_3_4h_gt_50) | (aroonu_14_4h_lt_75))
             # 1h down move, 1h high, 4h overbought
-            & (rsi_3_1h_gt_20 | aroonu_14_1h_lt_70 | (roc_9_4h_lt_10))
+            & ((rsi_3_1h_gt_20) | (aroonu_14_1h_lt_70) | (roc_9_4h_lt_10))
             # 1h down move, 1h high & overbought
-            & (rsi_3_1h_gt_20 | aroonu_14_1d_lt_100 | (roc_9_1d_lt_100))
+            & ((rsi_3_1h_gt_20) | (aroonu_14_1d_lt_100) | (roc_9_1d_lt_100))
             # 1h down move, 4h downtrend, 1d overbought
-            & (rsi_3_1h_gt_20 | (roc_9_4h_gt_neg_40) | roc_9_1d_lt_50)
+            & ((rsi_3_1h_gt_20) | (roc_9_4h_gt_neg_40) | (roc_9_1d_lt_50))
             # 1h & 4h down move, 1d high
             & ((rsi_3_1h_gt_25) | (rsi_3_4h_gt_30) | (aroonu_14_1d_lt_80))
             # 1h & 4h down move, 1d overbought
