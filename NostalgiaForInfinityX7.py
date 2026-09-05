7791d4856e86dd4bb3f3040cbb45c017ce4bb25c
iterativ <vangelis.dimou@tutanota.com>
2026-09-05 16:58:09 +0300

    X7: signal 103: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -71,7 +71,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.5.37"
+    return "v17.5.38"
 
   stoploss = -0.99
 
@@ -23680,6 +23680,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((rsi_3_4h_gt_15) | (rsi_3_1d_gt_15) | (aroonu_14_15m_lt_60) | (roc_9_1d_gt_neg_20))
             # 4h & 1d down move, 1d still high, 1d downtrend
             & ((rsi_3_4h_gt_15) | (rsi_3_1d_gt_25) | (aroonu_14_1d_lt_50) | (roc_9_1d_gt_neg_10))
+            # 4h & 1d down move, 15m downtrend, 4h still high
+            & ((rsi_3_4h_gt_20) | (rsi_3_1d_gt_50) | (cmf_20_15m_gt_neg_0_35) | (aroonu_14_4h_lt_50))
             # 4h down move, 15m & 1d high, 1h downtrend
             & ((rsi_3_4h_gt_20) | (aroonu_14_15m_lt_70) | (aroonu_14_1d_lt_100) | (roc_9_1h_gt_neg_20))
             # 4h down move, 4h still high, 1d downtrend
