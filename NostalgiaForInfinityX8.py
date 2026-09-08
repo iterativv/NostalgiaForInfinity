5df5ea70e16b909ad8146e90d3842e417318878c
iterativ <vangelis.dimou@tutanota.com>
2026-09-08 17:21:05 +0300

    X8: signal 3: add protection.

--- NostalgiaForInfinityX8.py
+++ NostalgiaForInfinityX8.py
@@ -7964,6 +7964,8 @@ class NostalgiaForInfinityX8(IStrategy):
             & ((rsi_3_1h_gt_15) | (rsi_3_1d_gt_60) | (roc_9_1d_lt_40))
             # 1h down move, 1h still not low enough, 1d downtrend
             & ((rsi_3_1h_gt_15) | (stochrsi_k_1h_lt_30) | (roc_9_1d_gt_neg_30))
+            # 1h & 4h & 1d down move, 1h stil high, 1d downtrend
+            & ((rsi_3_1h_gt_20) | (rsi_3_4h_gt_25) | (rsi_3_1d_gt_25) | (aroonu_14_1h_lt_40) | (roc_9_1d_gt_neg_20))
             # 1h & 4h down move, 1d high
             & ((rsi_3_1h_gt_20) | (rsi_3_4h_gt_25) | (aroonu_14_1d_lt_80))
             # 1h & 4h down move, 1d overbought
