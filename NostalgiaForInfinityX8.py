e9e165f8fc832d77a50ea115e3d06c7418fa5661
iterativ <vangelis.dimou@tutanota.com>
2026-09-09 17:54:22 +0300

    X8: signal 61: add protection.

--- NostalgiaForInfinityX8.py
+++ NostalgiaForInfinityX8.py
@@ -71,7 +71,7 @@ class NostalgiaForInfinityX8(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v18.0.23"
+    return "v18.0.24"
 
   stoploss = -0.99
 
@@ -4051,7 +4051,9 @@ class NostalgiaForInfinityX8(IStrategy):
     _or_day = df["date"].dt.floor("1D")
     _or_first4h = df["date"].dt.hour < 4
     _or_valid = df["date"].dt.hour >= 4
-    orange_h_col = df["high"].where(_or_first4h).groupby(_or_day).transform("max").where(_or_valid).to_numpy(copy=False)
+    orange_h_col = (
+      df["high"].where(_or_first4h).groupby(_or_day).transform("max").where(_or_valid).to_numpy(copy=False)
+    )
     orange_l_col = df["low"].where(_or_first4h).groupby(_or_day).transform("min").where(_or_valid).to_numpy(copy=False)
 
     # =========================================================================
@@ -13772,6 +13774,8 @@ class NostalgiaForInfinityX8(IStrategy):
             & ((rsi_3_1h_gt_20) | (aroonu_14_1d_lt_80) | (roc_9_1d_lt_200) | (roc_9_1h_gt_neg_20))
             # 1h & 4h down move, 1d overbought
             & ((rsi_3_1h_gt_25) | (rsi_3_4h_gt_40) | (roc_9_1d_lt_100))
+            # 1h & 4h down move, 1h high, 1d overbought
+            & ((rsi_3_1h_gt_25) | (rsi_3_4h_gt_45) | (aroonu_14_1h_lt_70) | (roc_9_1d_lt_100))
             # 1h down move, 1h & 1d high
             & ((rsi_3_1h_gt_25) | (aroonu_14_1h_lt_70) | (aroonu_14_1d_lt_90))
             # 1h down move, 1h still high, 1d overbought
