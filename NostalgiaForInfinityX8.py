bbd8d77c8d5d7416d9da216aec72a55acd9195df
iterativ <vangelis.dimou@tutanota.com>
2026-09-09 16:25:05 +0300

    X8: signal 21: add protection.

--- NostalgiaForInfinityX8.py
+++ NostalgiaForInfinityX8.py
@@ -10202,7 +10202,7 @@ class NostalgiaForInfinityX8(IStrategy):
         if long_entry_condition_index == 21:
           # Protections
           long_entry_logic.append(num_empty_288 <= allowed_empty_candles_288)
-          # long_entry_logic.append(protections_long_global == True)
+          long_entry_logic.append(protections_long_global == True)
 
           long_entry_logic.append(
             # 5m down move, 4h high, 1h overbought
@@ -10305,6 +10305,8 @@ class NostalgiaForInfinityX8(IStrategy):
             & ((rsi_3_1h_gt_25) | (aroonu_14_1d_lt_100) | (roc_9_4h_lt_60))
             # 1h down move, 4h high & overbought
             & ((rsi_3_1h_gt_30) | (aroonu_14_4h_lt_80) | (roc_9_4h_lt_30))
+            # 1h & 4h down move, 4h high & overbought
+            & ((rsi_3_1h_gt_35) | (rsi_3_4h_gt_65) | (aroonu_14_4h_lt_90) | (roc_9_4h_lt_50))
             # 1h down move, 1h high, 1d downtrend
             & ((rsi_3_1h_gt_35) | (aroonu_14_1h_lt_80) | (roc_9_1d_gt_neg_40))
             # 1h down move, 4h high, 1d downtrend
