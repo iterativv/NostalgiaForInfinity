f58cfac15ca154ad89fed62e91d2a5952647c916
iterativ <vangelis.dimou@tutanota.com>
2026-09-10 10:46:47 +0300

    X7: signal 62: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -21665,6 +21665,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((rsi_3_1h_gt_40) | (rsi_3_4h_gt_45) | (aroonu_14_1h_lt_60) | (aroonu_14_4h_lt_80))
             # 1h down move, 4h high & overbought
             & ((rsi_3_1h_gt_40) | (stochrsi_k_4h_lt_70) | (roc_9_4h_lt_30))
+            # 1h & 1d down move, 4h & 1d high, 4h overbought
+            & ((rsi_3_1h_gt_45) | (rsi_3_1d_gt_55) | (stochrsi_k_4h_lt_80) | (stochrsi_k_1d_lt_80) | (roc_9_4h_lt_20))
             # 1h down move, 4h high & overbought
             & ((rsi_3_1h_gt_45) | (aroonu_14_4h_lt_80) | (roc_9_4h_lt_100))
             # 1h down move, 1h overbought
