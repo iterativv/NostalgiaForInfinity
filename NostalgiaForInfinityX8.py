5c1eff724a0fd880a38bfb29266118da6e41d267
iterativ <vangelis.dimou@tutanota.com>
2026-09-26 14:51:36 +0300

    X8: signal 162: add protection.

--- NostalgiaForInfinityX8.py
+++ NostalgiaForInfinityX8.py
@@ -71,7 +71,7 @@ class NostalgiaForInfinityX8(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v18.0.83"
+    return "v18.0.84"
 
   stoploss = -0.99
 
@@ -19466,6 +19466,8 @@ class NostalgiaForInfinityX8(IStrategy):
           long_entry_logic.append(
             # 15m & 1h down move, 4h high
             ((rsi_3_15m_gt_10) | rsi_3_1h_gt_20 | (stochrsi_k_4h_lt_70))
+            # 15m & 1d down move, 1h high & overbought
+            & ((rsi_3_15m_gt_10) | (rsi_3_1h_gt_60) | (stochrsi_k_1h_lt_80) | (roc_9_1h_lt_10))
             # 15m & 4h down move, 1h still high
             & ((rsi_3_15m_gt_10) | (rsi_3_4h_gt_40) | (rsi_14_1h_lt_50))
             # 15m & 1d down move, 4h high
